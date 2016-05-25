119231ad207ce0b0257f11dde2a852c05803d90b
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-25 21:45:10 +0200

    Added rasteriser

---
+++ raytracer/raytracer/math/rasteriser.cpp
@@ -0,0 +1,12 @@
+#include "math/rasteriser.h"
+
+using namespace math;
+
+subspace math::rasteriser::operator [](const position& pos) const
+{
+	vector3d x_axis = this->rect.x_axis / this->horizontal_subdivisions;
+	vector3d y_axis = this->rect.y_axis / this->vertical_subdivisions;
+	point3d origin = this->rect.origin + pos.x * x_axis + pos.y * y_axis;
+
+	return subspace(origin, x_axis, y_axis);
+}
