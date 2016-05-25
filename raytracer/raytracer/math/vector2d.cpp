063b0a7a7eed8317a2795310ae4950835032c672
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-25 17:58:26 +0200

    Added vector2d

---
+++ raytracer/raytracer/math/vector2d.cpp
@@ -0,0 +1,52 @@
+#include "math/vector2d.h"
+
+using namespace math;
+
+vector2d math::operator +(const vector2d& u, const vector2d& v)
+{
+	double x = u.x + v.x;
+	double y = u.y + v.y;
+
+	return vector2d(x, y);
+}
+
+vector2d math::operator -(const vector2d& u, const vector2d& v)
+{
+	double x = u.x - v.x;
+	double y = u.y - v.y;
+
+	return vector2d(x, y);
+}
+
+vector2d math::operator *(const vector2d& v, double factor)
+{
+	double x = v.x * factor;
+	double y = v.y * factor;
+
+	return vector2d(x, y);
+}
+
+vector2d math::operator *(double factor, const vector2d& v)
+{
+	return v * factor;
+}
+
+vector2d math::operator /(const vector2d& v, double factor)
+{
+	return v * (1.0 / factor);
+}
+
+std::ostream& math::operator <<(std::ostream& out, const vector2d& v)
+{
+	return out << "(" << v.x << "," << v.y << ")";
+}
+
+double math::vector2d::dot(const vector2d& v) const
+{
+	double result = 0;
+
+	result += x * v.x;
+	result += y * v.y;
+
+	return result;
+}
---
+++ raytracer/raytracer/math/vector2d.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+
+namespace math
+{
+	struct vector2d final
+	{
+		double x, y;
+
+		vector2d()
+			: x(0), y(0) { }
+
+		vector2d(double x, double y)
+			: x(x), y(y) { }
+
+		vector2d(const vector2d& v) = default;
+
+		vector2d& operator =(const vector2d&) = default;
+
+		double dot(const vector2d&) const;
+	};
+
+	vector2d operator +(const vector2d&, const vector2d&);
+	vector2d operator -(const vector2d&, const vector2d&);
+	vector2d operator *(const vector2d&, double factor);
+	vector2d operator *(double factor, const vector2d&);
+	vector2d operator /(const vector2d&, double factor);
+
+	std::ostream& operator <<(std::ostream& out, const vector2d&);
+}
