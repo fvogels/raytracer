104c0f332a5e46710a0a3ae51d450e540d9d0f25
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-25 17:58:48 +0200

    Added point2d

---
+++ raytracer/raytracer/math/point2d.cpp
@@ -0,0 +1,24 @@
+#include "math/point2d.h"
+
+using namespace math;
+
+point2d math::operator +(const point2d& p, const vector2d& v)
+{
+	double x = p.x + v.x;
+	double y = p.y + v.y;
+
+	return point2d(x, y);
+}
+
+point2d math::operator +(const vector2d& v, const point2d& p)
+{
+	return p + v;
+}
+
+vector2d math::operator -(const point2d& p, const point2d& q)
+{
+	double x = p.x - q.x;
+	double y = p.y - q.y;
+
+	return vector2d(x, y);
+}
---
+++ raytracer/raytracer/math/point2d.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "math/vector2d.h"
+
+namespace math
+{
+	struct point2d final
+	{
+		double x, y;
+
+		point2d()
+			: x(0), y(0) { }
+
+		point2d(double x, double y)
+			: x(x), y(y) { }
+
+		point2d(const point2d& v) = default;
+
+		point2d& operator =(const point2d&) = default;
+	};
+
+	point2d operator +(const point2d&, const vector2d&);
+	point2d operator +(const vector2d&, const point2d&);
+	vector2d operator -(const point2d&, const point2d&);
+}
