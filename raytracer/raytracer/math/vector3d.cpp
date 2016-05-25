4de25faee6f44daf3decacf5f014189edcf498ee
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-25 16:23:28 +0200

    Added vector3d

---
+++ raytracer/raytracer/math/vector3d.cpp
@@ -0,0 +1,45 @@
+#include "vector3d.h"
+
+using namespace math;
+
+vector3d math::operator +(const vector3d& u, const vector3d& v)
+{
+	double x = u.x + v.x;
+	double y = u.y + v.y;
+	double z = u.z + v.z;
+
+	return vector3d(x, y, z);
+}
+
+vector3d math::operator -(const vector3d& u, const vector3d& v)
+{
+	double x = u.x - v.x;
+	double y = u.y - v.y;
+	double z = u.z - v.z;
+
+	return vector3d(x, y, z);
+}
+
+vector3d math::operator *(const vector3d& v, double factor)
+{
+	double x = v.x * factor;
+	double y = v.y * factor;
+	double z = v.z * factor;
+
+	return vector3d(x, y, z);
+}
+
+vector3d math::operator *(double factor, const vector3d& v)
+{
+	return v * factor;
+}
+
+vector3d math::operator /(const vector3d& v, double factor)
+{
+	return v * (1.0 / factor);
+}
+
+std::ostream& math::operator <<(std::ostream& out, const vector3d& v)
+{
+	return out << "(" << v.x << "," << v.y << "," << v.z << ")";
+}
---
+++ raytracer/raytracer/math/vector3d.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+
+namespace math
+{
+	struct vector3d
+	{
+		double x, y, z;
+
+		vector3d()
+			: x(0), y(0), z(0) { }
+
+		vector3d(double x, double y, double z)
+			: x(x), y(y), z(z) { }
+
+		vector3d(const vector3d& v) = default;
+
+		vector3d& operator =(const vector3d&) = default;
+	};
+
+	vector3d operator +(const vector3d&, const vector3d&);
+	vector3d operator -(const vector3d&, const vector3d&);
+	vector3d operator *(const vector3d&, double factor);
+	vector3d operator *(double factor, const vector3d&);
+	vector3d operator /(const vector3d&, double factor);
+
+	std::ostream& operator <<(std::ostream& out, const vector3d&);
+}
