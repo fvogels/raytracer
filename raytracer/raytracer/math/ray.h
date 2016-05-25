a287c303f84ca89cc925f6e03cb9d6f37fad55e2
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-25 16:30:37 +0200

    Added ray

---
+++ raytracer/raytracer/math/ray.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "math/vector3d.h"
+#include "math/point3d.h"
+
+namespace math
+{
+	struct ray
+	{
+		point3d origin;
+		vector3d direction;
+
+		ray(const point3d& origin, const vector3d& direction)
+			: origin(origin), direction(direction) { }
+
+		ray(const ray&) = default;
+
+		point3d at(double t) const { return origin + direction * t; }
+	};
+}
