10e7b9accbec9bf23231e073a6963bdc77965d79
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-27 16:10:34 +0200

    math/ray
    Added constructor

--- raytracer/raytracer/math/ray.h
+++ raytracer/raytracer/math/ray.h
@@ -13,6 +13,9 @@ namespace math
 		ray(const point3d& origin, const vector3d& direction)
 			: origin(origin), direction(direction) { }
 
+		ray(const point3d& origin, const point3d& through)
+			: ray(origin, through - origin) { }
+
 		ray(const ray&) = default;
 
 		point3d at(double t) const { return origin + direction * t; }
