e965fe70b0ba1f6473a80e1e5e2813683d0588aa
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-28 17:31:20 +0200

    tests/rasteriser

---
+++ raytracer/raytracer/tests/rasteriser_tests.cpp
@@ -0,0 +1,12 @@
+#include "math/rasteriser.h"
+#include "Catch.h"
+
+using namespace math;
+
+TEST_CASE("Rasterising [0,1]-[1,0] in 2x2", "[rasteriser]")
+{
+	rectangle2d rect(point2d(0, 1), vector2d(1, 0), vector2d(0, -1));
+	rasteriser r(rect, 2, 2);
+	
+	REQUIRE(r[position(0, 0)] == rectangle2d(point2d(0, 1), vector2d(0.5, 0), vector2d(0, -0.5)));
+}
