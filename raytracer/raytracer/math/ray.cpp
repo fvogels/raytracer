3e4206035ea9761f38b7e288ed9a63c94795cbe3
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-28 11:20:49 +0200

    math/ray
    Rewrote transform to aid in debugging

--- raytracer/raytracer/math/ray.cpp
+++ raytracer/raytracer/math/ray.cpp
@@ -4,5 +4,8 @@ using namespace math;
 
 ray math::ray::transform(const matrix4d& m) const
 {
-	return ray(m * origin, m * direction);
+	auto transformed_origin = m * origin;
+	auto transformed_direction = m * direction;
+
+	return ray(transformed_origin, transformed_direction);
 }
