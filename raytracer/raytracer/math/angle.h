1bf0224027e01669a8d2baa922fdd243cdb346ae
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-30 20:57:31 +0200

    Renamed angle to Angle

--- raytracer/raytracer/math/angle.h
+++ raytracer/raytracer/math/angle.h
@@ -4,37 +4,37 @@
 
 namespace math
 {
-	class angle final
+	class Angle final
 	{
 	public:
 		double radians() const { return m_radians; }
 		double degrees() const { return m_radians * 180 / M_PI; }
 
 	private:
-		angle(double x) : m_radians(x) { }
+		Angle(double x) : m_radians(x) { }
 		
-		friend angle operator "" _rad(long double);
-		friend angle operator "" _deg(long double);
-		friend angle operator "" _°(long double);
+		friend Angle operator "" _rad(long double);
+		friend Angle operator "" _deg(long double);
+		friend Angle operator "" _°(long double);
 
 		double m_radians;
 	};
 
-	inline angle operator""_rad(long double x)
+	inline Angle operator""_rad(long double x)
 	{
-		return angle(x);
+		return Angle(x);
 	}
 
-	inline angle operator""_deg(long double x)
+	inline Angle operator""_deg(long double x)
 	{
-		return angle(x / 180 * M_PI);
+		return Angle(x / 180 * M_PI);
 	}
 
-	inline angle operator""_°(long double x)
+	inline Angle operator""_°(long double x)
 	{
-		return angle(x / 180 * M_PI);
+		return Angle(x / 180 * M_PI);
 	}
 
-	inline double sin(angle a) { return ::sin(a.radians()); }
-	inline double cos(angle a) { return ::cos(a.radians()); }
+	inline double sin(Angle a) { return ::sin(a.radians()); }
+	inline double cos(Angle a) { return ::cos(a.radians()); }
 }
