4ae8317e64678e6138eee5f314728b4c474f2f6b
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-30 16:28:55 +0200

    scripting/combined_reader

---
+++ raytracer/raytracer/tests/combined_reader_tests.cpp
@@ -0,0 +1,51 @@
+#ifdef TEST_BUILD
+
+#include "Catch.h"
+#include "scripting/reader.h"
+#include "scripting/string_reader.h"
+#include "scripting/stream_line_reader.h"
+#include "scripting/combined_reader.h"
+#include <sstream>
+
+using namespace scripting;
+
+struct Loc
+{
+	Loc(unsigned line, unsigned ch)
+		: line(line), ch(ch)
+	{
+		// NOP
+	}
+
+	unsigned line, ch;
+};
+
+TEST_CASE("[CombinedReader] Empty stream", "[CombinedReader]")
+{
+	std::istringstream ss("");
+	CombinedReader<StreamLineReader, StringReader, Loc> reader(ss);
+
+	REQUIRE(reader.end_reached());
+}
+
+TEST_CASE("[CombinedReader] Single line", "[CombinedReader]")
+{
+	std::istringstream ss("abc");
+	CombinedReader<StreamLineReader, StringReader, Loc> reader(ss);
+
+	REQUIRE(!reader.end_reached());
+	REQUIRE(reader.current() == 'a');
+	reader.next();
+	REQUIRE(!reader.end_reached());
+	REQUIRE(reader.current() == 'b');
+	reader.next();
+	REQUIRE(!reader.end_reached());
+	REQUIRE(reader.current() == 'c');
+	reader.next();
+	REQUIRE(!reader.end_reached());
+	REQUIRE(reader.current() == '\n');
+	reader.next();
+	REQUIRE(reader.end_reached());
+}
+
+#endif
