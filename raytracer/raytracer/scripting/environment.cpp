8c2f2c48b385fc4dcca540f84ed3b1c72f98bf54
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-11 11:23:54 +0200

    scripting/environment
    Environment now consists of a linked list of frames

--- raytracer/raytracer/scripting/environment.cpp
+++ raytracer/raytracer/scripting/environment.cpp
@@ -1,25 +1,116 @@
 #include "scripting/Environment.h"
+#include <assert.h>
+#include <map>
+
+namespace scripting
+{
+	class Frame
+	{
+	public:
+		Frame() = default;
+
+		bool is_bound(const Symbol&) const;
+		std::shared_ptr<const Object> lookup(const Symbol&) const;
+		void bind(const Symbol&, std::shared_ptr<const Object>);
+
+	private:
+		std::map<std::string, std::shared_ptr<const Object>> bindings;
+	};
+}
+
+struct scripting::Environment::Impl
+{
+	Impl(std::shared_ptr<scripting::Frame>, std::shared_ptr<scripting::Environment>);
+
+	bool is_bound(const Symbol&) const;
+	std::shared_ptr<const Object> lookup(const Symbol&) const;
+	void bind(const Symbol&, std::shared_ptr<const Object>);
+
+	std::shared_ptr<scripting::Frame> frame;
+	std::shared_ptr<scripting::Environment> parent;
+};
+
 
 using namespace scripting;
 
-bool scripting::Environment::contains(const Symbol& symbol) const
+bool scripting::Environment::Impl::is_bound(const Symbol& symbol) const
 {
-	return m_bindings.find(symbol.name()) != m_bindings.end();
+	return frame->is_bound(symbol) || (parent != nullptr && parent->is_bound(symbol));
+}
+
+std::shared_ptr<const Object> scripting::Environment::Impl::lookup(const Symbol& symbol) const
+{
+	if ( frame->is_bound(symbol) )
+	{
+		return frame->lookup(symbol);
+	}
+	else if (parent != nullptr)
+	{
+		return parent->lookup(symbol);
+	}
+	else
+	{
+		throw std::runtime_error("Unbound symbol");
+	}
+}
+
+void scripting::Environment::Impl::bind(const Symbol& symbol, std::shared_ptr<const Object> object)
+{
+	frame->bind(symbol, object);
+}
+
+bool scripting::Frame::is_bound(const Symbol& symbol) const
+{
+	return bindings.find(symbol.name()) != bindings.end();
+}
+
+std::shared_ptr<const Object> scripting::Frame::lookup(const Symbol& symbol) const
+{
+	assert(is_bound(symbol));
+
+	return bindings.at(symbol.name());
+}
+
+void scripting::Frame::bind(const Symbol& symbol, std::shared_ptr<const Object> object)
+{
+	bindings[symbol.name()] = object;
+}
+
+
+scripting::Environment::Impl::Impl(std::shared_ptr<Frame> frame, std::shared_ptr<Environment> parent)
+	: frame(frame), parent(parent)
+{
+	// NOP
+}
+
+scripting::Environment::Environment()
+	: Environment(nullptr)
+{
+	// NOP
+}
+
+scripting::Environment::Environment(std::shared_ptr<Environment> parent)
+	: m_pimpl(std::make_unique<scripting::Environment::Impl>(std::make_shared<Frame>(), parent))
+{
+	// NOP
+}
+
+scripting::Environment::~Environment()
+{
+	// NOP
+}
+
+bool scripting::Environment::is_bound(const Symbol& symbol) const
+{
+	return m_pimpl->is_bound(symbol);
 }
 
 std::shared_ptr<const Object> scripting::Environment::lookup(const Symbol& symbol) const
 {
-	if (!contains(symbol))
-	{
-		throw std::runtime_error("Unknown symbol");
-	}
-	else
-	{
-		return m_bindings.at(symbol.name());
-	}
+	return m_pimpl->lookup(symbol);
 }
 
 void scripting::Environment::bind(const Symbol& symbol, std::shared_ptr<const Object> value)
 {
-	m_bindings[symbol.name()] = value;
+	return m_pimpl->bind(symbol, value);
 }
--- raytracer/raytracer/scripting/environment.h
+++ raytracer/raytracer/scripting/environment.h
@@ -2,18 +2,25 @@
 
 #include "scripting/values/object.h"
 #include <memory>
-#include <map>
+//#include <map>
 
 namespace scripting
 {
 	class Environment
 	{
 	public:
-		bool contains(const Symbol&) const;
+		Environment();
+		Environment(std::shared_ptr<Environment>);
+		~Environment();
+
+		bool is_bound(const Symbol&) const;
 		std::shared_ptr<const Object> lookup(const Symbol&) const;
 		void bind(const Symbol&, std::shared_ptr<const Object> value);
 
 	private:
-		std::map<std::string, std::shared_ptr<const Object>> m_bindings;
+		struct Impl;
+		std::unique_ptr<Impl> m_pimpl;
+		//std::map<std::string, std::shared_ptr<const Object>> m_bindings;
+		//std::shared_ptr<Environment> m_parent;
 	};
 }
