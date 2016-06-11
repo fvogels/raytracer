c0d9757212ffaa90e15ce50df98eb07417d72923
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-11 19:08:58 +0200

    scripting/values/string
    Made String subclass of NativeValueAdapter

--- raytracer/raytracer/scripting/values/string.cpp
+++ raytracer/raytracer/scripting/values/string.cpp
@@ -4,24 +4,7 @@ using namespace scripting;
 
 
 scripting::String::String(const std::string& string)
-	: m_value(string) { }
-
-bool scripting::String::operator==(const Object& other) const
-{
-	auto that = dynamic_cast<const String*>(&other);
-
-	return that != nullptr && this->m_value == that->m_value;
-}
-
-void scripting::String::write(std::ostream& out) const
-{
-	out << '"' << this->m_value << '"';
-}
-
-std::string scripting::String::value() const
-{
-	return this->m_value;
-}
+	: NativeValueAdapter(string) { }
 
 std::shared_ptr<Object> scripting::String::evaluate(std::shared_ptr<scripting::Environment>)
 {
--- raytracer/raytracer/scripting/values/string.h
+++ raytracer/raytracer/scripting/values/string.h
@@ -1,22 +1,15 @@
 #pragma once
 
 #include "scripting/values/object.h"
+#include "scripting/values/native-value-adapter.h"
 
 namespace scripting
 {
-	class String : public Object
+	class String : public NativeValueAdapter<std::string>
 	{
 	public:
 		String(const std::string&);
 
-		void write(std::ostream&) const override;
-		bool operator ==(const Object&) const override;
-
 		std::shared_ptr<Object> evaluate(std::shared_ptr<scripting::Environment>) override;
-
-		std::string value() const;
-
-	private:
-		std::string m_value;
 	};
 }
