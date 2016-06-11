acfad455bb3766e180e1c8a27ee1cedf29ecfd3e
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-11 18:57:24 +0200

    scripting/values/number
    Made number subclass of NativeValueAdapter

--- raytracer/raytracer/scripting/values/number.cpp
+++ raytracer/raytracer/scripting/values/number.cpp
@@ -4,23 +4,9 @@ using namespace scripting;
 
 
 scripting::Number::Number(double value)
-	: m_value(value) { }
-
-bool scripting::Number::operator==(const Object& other) const
+	: NativeValueAdapter<double>(value)
 {
-	auto that = dynamic_cast<const Number*>(&other);
-
-	return that != nullptr && this->m_value == that->m_value;
-}
-
-void scripting::Number::write(std::ostream& out) const
-{
-	out << this->m_value;
-}
-
-double scripting::Number::value() const
-{
-	return m_value;
+	// NOP
 }
 
 std::shared_ptr<Object> scripting::Number::evaluate(std::shared_ptr<scripting::Environment>)
--- raytracer/raytracer/scripting/values/number.h
+++ raytracer/raytracer/scripting/values/number.h
@@ -1,20 +1,16 @@
 #pragma once
 
 #include "scripting/values/object.h"
+#include "scripting/values/native-value-adapter.h"
 
 namespace scripting
 {
-	class Number : public scripting::Object
+	class Number : public scripting::NativeValueAdapter<double>
 	{
 	public:
 		Number(double value);
 
-		void write(std::ostream&) const override;
-		bool operator ==(const Object&) const override;
-
 		std::shared_ptr<Object> evaluate(std::shared_ptr<scripting::Environment>) override;
-
-		double value() const;
 
 	private:
 		double m_value;
