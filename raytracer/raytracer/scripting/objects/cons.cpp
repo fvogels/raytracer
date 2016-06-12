580c6abb8cded6c434ebe4438442feb5f4f26c79
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-12 13:11:31 +0200

    scripting/values/cons

---
+++ raytracer/raytracer/scripting/objects/cons.cpp
@@ -0,0 +1,108 @@
+#include "scripting/objects/cons.h"
+#include "scripting/objects/nil.h"
+#include "scripting/objects/callable.h"
+#include "scripting/environment.h"
+#include <assert.h>
+
+using namespace scripting;
+
+
+scripting::Cons::Cons(std::shared_ptr<Object> car, std::shared_ptr<Object> cdr)
+	: m_car(car), m_cdr(cdr)
+{
+	// NOP
+}
+
+bool scripting::Cons::operator==(const Object& other) const
+{
+	if (has_value_type<Cons>(other))
+	{
+		auto cons = value_cast<Cons>(other);
+
+		return *m_car == *cons.car() && *m_cdr == *cons.cdr();
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void scripting::Cons::write(std::ostream& out) const
+{
+	out << "(" << m_car;
+
+	std::shared_ptr<const Object> current = m_cdr;
+
+	while (has_value_type<Cons>(current))
+	{
+		auto cons = std::static_pointer_cast<const Cons>(current);
+		out << " " << cons->m_car;
+		current = cons->m_cdr;
+	}
+
+	if (has_value_type<Nil>(current))
+	{
+		out << ")";
+	}
+	else
+	{
+		out << " . " << current << ")";
+	}
+}
+
+std::shared_ptr<Object> scripting::Cons::evaluate(std::shared_ptr<scripting::Environment> environment, std::shared_ptr<scripting::Heap> heap)
+{
+	if (!is_list())
+	{
+		throw std::runtime_error("Evaluation expects well-formed list");
+	}
+	else
+	{
+		auto elts = elements();
+		auto evaluated_head = elts[0]->evaluate(environment, heap);
+
+		std::vector<std::shared_ptr<Object>> argument_expressions(elts.begin() + 1, elts.end());
+
+		return with_value_type<Callable, std::shared_ptr<Object>>(evaluated_head, [environment, heap, &argument_expressions](std::shared_ptr<Callable> callable)
+		{
+			return callable->call(environment, heap, argument_expressions);
+		});
+	}
+}
+
+std::shared_ptr<Object> scripting::Cons::car()
+{
+	return m_car;
+}
+
+std::shared_ptr<Object> scripting::Cons::cdr()
+{
+	return m_cdr;
+}
+
+bool scripting::Cons::is_list() const
+{
+	if (has_value_type<Cons>(m_cdr))
+	{
+		auto cdr = value_cast<Cons>(m_cdr);
+
+		return cdr->is_list();
+	}
+	else return has_value_type<Nil>(m_cdr);
+}
+
+std::vector<std::shared_ptr<Object>> scripting::Cons::elements()
+{
+	assert(is_list());
+
+	std::vector<std::shared_ptr<Object>> result;
+	Cons* current = this;
+
+	while (current != nullptr)
+	{
+		result.push_back(current->m_car);
+		current = dynamic_cast<Cons*>(m_cdr.get());
+	}
+
+	return result;
+}
---
+++ raytracer/raytracer/scripting/objects/cons.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "scripting/objects/object.h"
+#include <vector>
+
+namespace scripting
+{
+	class Cons : public Object
+	{
+	public:
+		Cons(std::shared_ptr<Object>, std::shared_ptr<Object>);
+
+		void write(std::ostream&) const override;
+		bool operator ==(const Object&) const override;
+
+		std::shared_ptr<Object> evaluate(std::shared_ptr<Environment>, std::shared_ptr<scripting::Heap>) override;
+
+		std::shared_ptr<Object> car();
+		std::shared_ptr<Object> cdr();
+
+		bool is_list() const;
+		std::vector<std::shared_ptr<scripting::Object>> elements();
+
+	private:
+		std::shared_ptr<scripting::Object> m_car;
+		std::shared_ptr<scripting::Object> m_cdr;
+	};
+}
