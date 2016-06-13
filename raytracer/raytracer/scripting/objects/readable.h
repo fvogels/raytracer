02ab46f2b77e0355b33ae718a0ac4f4b3d52ba55
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-13 09:39:49 +0200

    scripting/objects/readable

---
+++ raytracer/raytracer/scripting/objects/readable.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "scripting/objects/object.h"
+
+namespace scripting
+{
+	class Readable : public virtual Object
+	{
+	public:
+		virtual std::shared_ptr<Object> read() const = 0;
+	};
+}
