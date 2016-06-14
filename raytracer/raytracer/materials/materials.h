dc59a2b43c08184c9f2c13105fd5582cc9dea41d
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-14 11:29:49 +0200

    materials/materials, primitives/primitives

---
+++ raytracer/raytracer/materials/materials.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "materials/material3d.h"
+#include "materials/uniform-material.h"
+#include "materials/checkered-material.h"
---
+++ raytracer/raytracer/primitives/primitives.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "primitives/primitive.h"
+#include "primitives/sphere.h"
+#include "primitives/plane.h"
+#include "primitives/union.h"
+#include "primitives/transformer.h"
+#include "primitives/decorator.h"
