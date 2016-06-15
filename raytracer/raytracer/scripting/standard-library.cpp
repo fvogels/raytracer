8231666184591f9866141eb3d4fb5da38ad07213
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-15 18:48:44 +0200

    scripting/standard-library
    Renamed BIND to BIND_LIBRARY_FUNCTION

--- raytracer/raytracer/scripting/standard-library.cpp
+++ raytracer/raytracer/scripting/standard-library.cpp
@@ -104,7 +104,7 @@ void scripting::add_standard_library_bindings(Environment* environment)
 {
 #define BIND_CREATE_BY_VALUE(SYMBOL, TYPE, ...) environment->bind(Symbol(SYMBOL), std::make_shared<scripting::library::CreateNativeByValue<TYPE, __VA_ARGS__>>())
 #define BIND_CREATE_BY_POINTER(SYMBOL, TYPE, SUPERTYPE, ...) environment->bind(Symbol(SYMBOL), std::make_shared<scripting::library::CreateNativeByPointer<TYPE, SUPERTYPE, __VA_ARGS__>>())
-#define BIND(SYMBOL_NAME, CLASS) environment->bind(Symbol(SYMBOL_NAME), std::make_shared<scripting::library::CLASS>())
+#define BIND_LIBRARY_FUNCTION(SYMBOL_NAME, CLASS) environment->bind(Symbol(SYMBOL_NAME), std::make_shared<scripting::library::CLASS>())
 
 	environment->bind(Symbol("true"), std::make_shared<scripting::Boolean>(true));
 	environment->bind(Symbol("false"), std::make_shared<scripting::Boolean>(false));
@@ -118,23 +118,23 @@ void scripting::add_standard_library_bindings(Environment* environment)
 	BIND_CREATE_BY_POINTER("decorate", Raytracer::Decorator, Raytracer::Primitive, std::shared_ptr<Raytracer::Material3D>, std::shared_ptr<Raytracer::Primitive>);
 	BIND_CREATE_BY_POINTER("uniform-material", Raytracer::UniformMaterial, Raytracer::Material3D, color);
 
-	BIND("let", Let);
-	BIND("if", If);
-	BIND("+", Addition);
-	BIND("-", Subtraction);
-	BIND("*", Multiplication);
-	BIND("=", Equality);
-	BIND("<", LessThan);
-	BIND(">", GreaterThan);
-	BIND("<=", NotGreaterThan);
-	BIND(">=", NotLessThan);
-	BIND("not", Negation);
-	BIND("x", GetX);
-	BIND("y", GetY);
-	BIND("z", GetZ);
-	BIND("alloc", Allocate);
-	BIND("get", ReadHeap);
-	BIND("set", WriteHeap);
+	BIND_LIBRARY_FUNCTION("let", Let);
+	BIND_LIBRARY_FUNCTION("if", If);
+	BIND_LIBRARY_FUNCTION("+", Addition);
+	BIND_LIBRARY_FUNCTION("-", Subtraction);
+	BIND_LIBRARY_FUNCTION("*", Multiplication);
+	BIND_LIBRARY_FUNCTION("=", Equality);
+	BIND_LIBRARY_FUNCTION("<", LessThan);
+	BIND_LIBRARY_FUNCTION(">", GreaterThan);
+	BIND_LIBRARY_FUNCTION("<=", NotGreaterThan);
+	BIND_LIBRARY_FUNCTION(">=", NotLessThan);
+	BIND_LIBRARY_FUNCTION("not", Negation);
+	BIND_LIBRARY_FUNCTION("x", GetX);
+	BIND_LIBRARY_FUNCTION("y", GetY);
+	BIND_LIBRARY_FUNCTION("z", GetZ);
+	BIND_LIBRARY_FUNCTION("alloc", Allocate);
+	BIND_LIBRARY_FUNCTION("get", ReadHeap);
+	BIND_LIBRARY_FUNCTION("set", WriteHeap);
 
 #undef BIND_CREATE_BY_POINTER
 #undef BIND_CREATE_BY_VALUE
