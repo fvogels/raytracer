6398c7b6f8d0bea1b891ad9c5d6d2a24e519ce51
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-15 17:50:55 +0200

    Added logging configuration to tests/main.cpp

--- raytracer/raytracer/tests/main.cpp
+++ raytracer/raytracer/tests/main.cpp
@@ -1,4 +1,16 @@
 #ifdef TEST_BUILD
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "Catch.h"
+#include "easylogging++.h"
+
+int main(int argc, char* const argv[])
+{
+	el::Configurations defaultConf;
+	defaultConf.setToDefault();
+	defaultConf.set(el::Level::Debug, el::ConfigurationType::Format, "[%level] (%fbase:%line) %msg");
+	el::Loggers::reconfigureLogger("stdlib", defaultConf);
+
+	return Catch::Session().run(argc, argv);
+}
+
 #endif
