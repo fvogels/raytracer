20aec58035d4988fc5a617e03f9c9668424f4e7e
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-17 22:51:44 +0200

    primitives/transformer
    Added factory methods transform and translate

--- raytracer/raytracer/primitives/transformer.cpp
+++ raytracer/raytracer/primitives/transformer.cpp
@@ -1,6 +1,8 @@
 #include "primitives/transformer.h"
 
 using namespace math;
+using namespace raytracer;
+
 
 bool raytracer::Transformer::find_hit(const Ray& ray, Hit* hit) const
 {
@@ -17,4 +19,14 @@ bool raytracer::Transformer::find_hit(const Ray& ray, Hit* hit) const
 	{
 		return false;
 	}
+}
+
+std::shared_ptr<Transformer> raytracer::transform::transform(const math::Transformation& transformation, std::shared_ptr<const Primitive> transformee)
+{
+	return std::make_shared<Transformer>(transformation, transformee);
+}
+
+std::shared_ptr<Transformer> raytracer::transform::translate(const math::Vector3D& v, std::shared_ptr<const Primitive> transformee)
+{
+	return raytracer::transform::transform(math::translation(v), transformee);
 }
