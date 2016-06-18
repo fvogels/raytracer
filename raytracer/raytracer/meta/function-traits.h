7d2ddd07bbac985ddbece778881cc3cf19a77fb0
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-18 21:57:25 +0200

    meta/function-traits

---
+++ raytracer/raytracer/meta/function-traits.h
@@ -0,0 +1,20 @@
+#pragma once
+
+template<class F>
+struct function_traits;
+
+template<class R, class... Ts>
+struct function_traits<R(Ts...)>
+{
+	using return_type = R;
+	using parameter_types = std::tuple<Ts...>;
+
+	static constexpr std::size_t arity = sizeof...(Ts);
+
+	template <std::size_t N>
+	struct parameter_type
+	{
+		static_assert(N < arity, "error: invalid parameter index.");
+		using type = typename std::tuple_element<N, std::tuple<Ts...>>::type;
+	};
+};
