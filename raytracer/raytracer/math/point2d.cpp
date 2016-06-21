7366e0d4b0d16614eeeef83d516626e1e90d25e3
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-22 00:56:55 +0200

    math/point2d
    Added constexpr and noexcept

--- raytracer/raytracer/math/point2d.cpp
+++ raytracer/raytracer/math/point2d.cpp
@@ -1,50 +1,12 @@
 #include "math/point2d.h"
-#include "math/util.h"
+
 
 using namespace math;
 
-Point2D math::operator +(const Point2D& p, const Vector2D& v)
-{
-	double x = p.x + v.x;
-	double y = p.y + v.y;
 
-	return Point2D(x, y);
-}
-
-Point2D math::operator +(const Vector2D& v, const Point2D& p)
-{
-	return p + v;
-}
-
-Vector2D math::operator -(const Point2D& p, const Point2D& q)
-{
-	double x = p.x - q.x;
-	double y = p.y - q.y;
-
-	return Vector2D(x, y);
-}
-
-Point2D math::operator -(const Point2D& p, const Vector2D& v)
-{
-	double x = p.x - v.x;
-	double y = p.y - v.y;
-
-	return Point2D(x, y);
-}
-
-double math::distance(const Point2D& p, const Point2D& q)
+double math::distance(const Point2D& p, const Point2D& q) noexcept
 {
 	return (q - p).norm();
-}
-
-bool math::operator ==(const Point2D& p, const Point2D& q)
-{
-	return p.x == approx(q.x) && p.y == approx(q.y);
-}
-
-bool math::operator !=(const Point2D& p, const Point2D& q)
-{
-	return !(p == q);
 }
 
 std::ostream& math::operator <<(std::ostream& out, const Point2D& p)
--- raytracer/raytracer/math/point2d.h
+++ raytracer/raytracer/math/point2d.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "math/vector2d.h"
+#include "math/util.h"
+
 
 namespace math
 {
@@ -8,26 +10,48 @@ namespace math
 	{
 		double x, y;
 
-		Point2D()
+		constexpr Point2D() noexcept
 			: x(0), y(0) { }
 
-		Point2D(double x, double y)
+		constexpr Point2D(double x, double y) noexcept
 			: x(x), y(y) { }
 
-		Point2D(const Point2D& v) = default;
+		constexpr Point2D(const Point2D& v) noexcept = default;
 
-		Point2D& operator =(const Point2D&) = default;
+		Point2D& operator =(const Point2D&) noexcept = default;
 	};
 
-	Point2D operator +(const Point2D&, const Vector2D&);
-	Point2D operator +(const Vector2D&, const Point2D&);
-	Vector2D operator -(const Point2D&, const Point2D&);
-	Point2D operator -(const Point2D&, const Vector2D&);
+	constexpr Point2D operator +(const Point2D& p, const Vector2D& v) noexcept
+	{
+		return Point2D(p.x + v.x, p.y + v.y);
+	}
 
-	bool operator ==(const Point2D&, const Point2D&);
-	bool operator !=(const Point2D&, const Point2D&);
+	constexpr Point2D operator +(const Vector2D& v, const Point2D& p) noexcept
+	{
+		return p + v;
+	}
 
-	double distance(const Point2D&, const Point2D&);
+	constexpr Vector2D operator -(const Point2D& p, const Point2D& q) noexcept
+	{
+		return Vector2D(p.x - q.x, p.y - q.y);
+	}
+
+	constexpr Point2D operator -(const Point2D& p, const Vector2D& v) noexcept
+	{
+		return Point2D(p.x - v.x, p.y - v.y);
+	}
+
+	constexpr bool operator ==(const Point2D& p, const Point2D& q) noexcept
+	{
+		return p.x == approx(q.x) && p.y == approx(q.y);
+	}
+
+	constexpr bool operator !=(const Point2D& p, const Point2D& q) noexcept
+	{
+		return !(p == q);
+	}
+
+	double distance(const Point2D&, const Point2D&) noexcept;
 
 	std::ostream& operator <<(std::ostream& out, const Point2D&);
 }
