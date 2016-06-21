90d2a4bfe2291c6c4c8eddae96a72073f5642ffc
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-21 23:11:04 +0200

    math/functions/vertical-lines
    Simplified

--- raytracer/raytracer/math/functions/vertical-lines.h
+++ raytracer/raytracer/math/functions/vertical-lines.h
@@ -9,25 +9,23 @@ namespace math
 {
 	namespace functions
 	{
-		template<typename T>
-		class VerticalLines : public Function<typename T::result, const Point2D&>
+		class VerticalLines : public Function<bool, const Point2D&>
 		{
 		public:
-			VerticalLines(double thickness, T converter = T())
-				: m_thickness(thickness), m_converter(converter)
+			VerticalLines(double thickness)
+				: m_thickness(thickness)
 			{
 				// NOP
 			}
 
-			typename T::result operator ()(const Point2D& p) const override
+			typename bool operator ()(const Point2D& p) const override
 			{
 				auto x = p.x * 10;
-				return m_converter(std::abs(x - std::round(x)) < m_thickness / 2);
+				return std::abs(x - std::round(x)) < m_thickness / 2;
 			}
 
 		private:
 			double m_thickness;
-			T m_converter;
 		};
 	}
 }
