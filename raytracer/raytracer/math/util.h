7be66c3666e06f28c4b9642e2945373302529b8d
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-22 00:56:31 +0200

    math/util
    Added noexcept

--- raytracer/raytracer/math/util.h
+++ raytracer/raytracer/math/util.h
@@ -17,22 +17,22 @@ constexpr T abs(T x) noexcept
 	return x < 0 ? -x : x;
 }
 
-constexpr bool operator ==(const approx& x, double y)
+constexpr bool operator ==(const approx& x, double y) noexcept
 {
 	return abs<double>(y - x.value) < x.delta;
 }
 
-constexpr bool operator ==(double x, const approx& y)
+constexpr bool operator ==(double x, const approx& y) noexcept
 {
 	return y == x;
 }
 
-constexpr bool operator !=(const approx& x, double y)
+constexpr bool operator !=(const approx& x, double y) noexcept
 {
 	return !(x == y);
 }
 
-constexpr bool operator !=(double x, const approx& y)
+constexpr bool operator !=(double x, const approx& y) noexcept
 {
 	return !(x == y);
 }
