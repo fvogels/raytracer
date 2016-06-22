767c8f31a8d19af6c5a208880473b9556b92d582
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-22 10:18:41 +0200

    math/point3d
    constexpr and noexcept

--- raytracer/raytracer/math/point3d.cpp
+++ raytracer/raytracer/math/point3d.cpp
@@ -2,51 +2,10 @@
 
 using namespace math;
 
-Point3D math::operator +(const Point3D& p, const Vector3D& v)
-{
-	double x = p.x + v.x;
-	double y = p.y + v.y;
-	double z = p.z + v.z;
 
-	return Point3D(x, y, z);
-}
-
-Point3D math::operator +(const Vector3D& v, const Point3D& p)
-{
-	return p + v;
-}
-
-Vector3D math::operator -(const Point3D& p, const Point3D& q)
-{
-	double x = p.x - q.x;
-	double y = p.y - q.y;
-	double z = p.z - q.z;
-
-	return Vector3D(x, y, z);
-}
-
-Point3D math::operator -(const Point3D& p, const Vector3D& v)
-{
-	double x = p.x - v.x;
-	double y = p.y - v.y;
-	double z = p.z - v.z;
-
-	return Point3D(x, y, z);
-}
-
-double math::distance(const Point3D& p, const Point3D& q)
+double math::distance(const Point3D& p, const Point3D& q) noexcept
 {
 	return (q - p).norm();
-}
-
-bool math::operator ==(const Point3D& p, const Point3D& q)
-{
-	return p.x == q.x && p.y == q.y && p.z == q.z;
-}
-
-bool math::operator !=(const Point3D& p, const Point3D& q)
-{
-	return !(p == q);
 }
 
 std::ostream& math::operator <<(std::ostream& out, const Point3D& p)
--- raytracer/raytracer/math/point3d.h
+++ raytracer/raytracer/math/point3d.h
@@ -8,26 +8,48 @@ namespace math
 	{
 		double x, y, z;
 
-		Point3D()
+		constexpr Point3D() noexcept
 			: x(0), y(0), z(0) { }
 
-		Point3D(double x, double y, double z)
+		constexpr Point3D(double x, double y, double z) noexcept
 			: x(x), y(y), z(z) { }
 
-		Point3D(const Point3D& v) = default;
+		constexpr Point3D(const Point3D& v) noexcept = default;
 
-		Point3D& operator =(const Point3D&) = default;
+		Point3D& operator =(const Point3D&) noexcept = default;
 	};
 
-	Point3D operator +(const Point3D&, const Vector3D&);
-	Point3D operator +(const Vector3D&, const Point3D&);
-	Vector3D operator -(const Point3D&, const Point3D&);
-	Point3D operator -(const Point3D&, const Vector3D&);
+	constexpr Point3D operator +(const Point3D& p, const Vector3D& v) noexcept
+	{
+		return Point3D(p.x + v.x, p.y + v.y, p.z + v.z);
+	}
 
-	bool operator ==(const Point3D&, const Point3D&);
-	bool operator !=(const Point3D&, const Point3D&);
+	constexpr Point3D operator +(const Vector3D& v, const Point3D& p) noexcept
+	{
+		return p + v;
+	}
 
-	double distance(const Point3D&, const Point3D&);
+	constexpr Vector3D operator -(const Point3D& p, const Point3D& q) noexcept
+	{
+		return Vector3D(p.x - q.x, p.y - q.y, p.z - q.z);
+	}
+
+	constexpr Point3D operator -(const Point3D& p, const Vector3D& v) noexcept
+	{
+		return Point3D(p.x - v.x, p.y - v.y, p.z - v.z);
+	}
+
+	constexpr bool operator ==(const Point3D& p, const Point3D& q) noexcept
+	{
+		return p.x == q.x && p.y == q.y && p.z == q.z;
+	}
+
+	constexpr bool operator !=(const Point3D& p, const Point3D& q) noexcept
+	{
+		return !(p == q);
+	}
+
+	double distance(const Point3D&, const Point3D&) noexcept;
 
 	std::ostream& operator <<(std::ostream& out, const Point3D&);
 }
