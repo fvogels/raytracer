e796da72867443f1e6f40b47ca2c1522f7bb04eb
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-25 09:46:26 +0200

    math/matrix4d
    Added coordinate_system factory method

--- raytracer/raytracer/math/matrix4d.cpp
+++ raytracer/raytracer/math/matrix4d.cpp
@@ -1,4 +1,5 @@
 #include "math/matrix4d.h"
+#include <assert.h>
 
 using namespace math;
 
@@ -25,6 +26,56 @@ Matrix4D math::operator *(const Matrix4D& a, const Matrix4D& b)
 		AUX(4, 4),
 	};
 
+#undef AUX
+}
+
+void math::Matrix4D::set_row(int row, const Vector3D& v)
+{
+	assert(0 <= row);
+	assert(row <= 3);
+
+#define AUX(row) x ## row ## 1 = v.x; x ## row ## 2 = v.y; x ## row ## 3 = v.z; x ## row ## 4 = 0
+	if (row == 0)
+	{
+		AUX(1);
+	}
+	else if (row == 1)
+	{
+		AUX(2);
+	}
+	else if (row == 2)
+	{
+		AUX(3);
+	}
+	else
+	{
+		AUX(4);
+	}
+#undef AUX
+}
+
+void math::Matrix4D::set_column(int column, const Point3D& v)
+{
+	assert(0 <= column);
+	assert(column <= 3);
+
+#define AUX(col) x1 ## col = v.x; x2 ## col = v.y; x3 ## col = v.z; x4 ## col = 1
+	if (column == 0)
+	{
+		AUX(1);
+	}
+	else if (column == 1)
+	{
+		AUX(2);
+	}
+	else if (column == 2)
+	{
+		AUX(3);
+	}
+	else
+	{
+		AUX(4);
+	}
 #undef AUX
 }
 
@@ -115,6 +166,16 @@ Matrix4D math::rotate_z_matrix(const Angle& angle)
 		c, -s , 0, 0,
 		s, c, 0, 0,
 		0, 0, 1, 0,
+		0, 0, 0, 1
+	};
+}
+
+Matrix4D math::coordinate_system(const Point3D& origin, const Vector3D& x_axis, const Vector3D& y_axis, const Vector3D& z_axis)
+{
+	return Matrix4D{
+		x_axis.x, x_axis.y , x_axis.z, origin.x,
+		y_axis.x, y_axis.y , y_axis.z, origin.y,
+		z_axis.x, z_axis.y , z_axis.z, origin.z,
 		0, 0, 0, 1
 	};
 }
--- raytracer/raytracer/math/matrix4d.h
+++ raytracer/raytracer/math/matrix4d.h
@@ -13,6 +13,9 @@ namespace math
 			x21, x22, x23, x24,
 			x31, x32, x33, x34,
 			x41, x42, x43, x44;
+
+		void set_row(int, const math::Vector3D&);
+		void set_column(int, const math::Point3D&);
 	};
 
 	Matrix4D operator *(const Matrix4D&, const Matrix4D&);
@@ -20,6 +23,7 @@ namespace math
 	Point3D operator *(const Matrix4D&, const Point3D&);
 
 	Matrix4D identity_matrix();
+	Matrix4D coordinate_system(const Point3D&, const Vector3D&, const Vector3D&, const Vector3D&);
 	Matrix4D translation_matrix(const Vector3D&);
 	Matrix4D scale_matrix(double sx, double sy, double sz);
 	Matrix4D rotate_x_matrix(const Angle&);
