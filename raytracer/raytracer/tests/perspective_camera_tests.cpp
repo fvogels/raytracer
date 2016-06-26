f8cacd7e77765717bef9bc90fb79d5b509d11818
Frederic Vogels <frederic.vogels@gmail.com>
2016-06-26 12:03:42 +0200

    tests/perspective_camera
    Rewrote macros

--- raytracer/raytracer/tests/perspective_camera_tests.cpp
+++ raytracer/raytracer/tests/perspective_camera_tests.cpp
@@ -37,19 +37,24 @@ namespace
 #define XY(...)  __VA_ARGS__
 #define XYZ(...) __VA_ARGS__
 
-#define TEST(P, EXPECTED_ORIGIN, EXPECTED_DIRECTION)	                        TEST_EYE_LOOKAT(P, XYZ(0,0,0), XYZ(0,0,1), EXPECTED_ORIGIN, EXPECTED_DIRECTION)
+#define TEST_SIMPLE(P, EXPECTED_DIRECTION) \
+    TEST(P, XYZ(0,0,0), XYZ(0,0,1), XYZ(0,1,0), 1, 1, XYZ(0,0,0), EXPECTED_DIRECTION)
 
-#define TEST_EYE_LOOKAT(P, EYE, LOOK_AT, EXPECTED_ORIGIN, EXPECTED_DIRECTION)   TEST_CASE("[PerspectiveCamera] Eye = " #EYE ", Lookat = " #LOOK_AT ", At " #P, "[PerspectiveCamera]") \
-                                                                                { \
-                                                                                    auto camera = raytracer::cameras::perspective(Point3D(EYE), Point3D(LOOK_AT), Vector3D(0, 1, 0), 1, 1); \
-                                                                                    auto ray = camera->create_ray(Point2D(P)); \
-                                                                                    \
-                                                                                    INFO( "Actual ray: " + show(ray) ); \
-                                                                                    assert_equals(ray.origin, Point3D(EXPECTED_ORIGIN)); \
-                                                                                    assert_equals(ray.direction, Vector3D(EXPECTED_DIRECTION)); \
-                                                                                }
+#define TEST_EYE_LOOKAT(P, EYE, LOOK_AT, EXPECTED_ORIGIN, EXPECTED_DIRECTION) \
+    TEST(P, EYE, LOOK_AT, XYZ(0,1,0), 1, 1, EXPECTED_ORIGIN, EXPECTED_DIRECTION)
 
-TEST(XY(0.5, 0.5), XYZ(0, 0, 0), XYZ(0, 0, 1))
+#define TEST(P, EYE, LOOK_AT, UP, DISTANCE, ASPECT_RATIO, EXPECTED_ORIGIN, EXPECTED_DIRECTION) \
+    TEST_CASE("[PerspectiveCamera] Eye = " #EYE ", Lookat = " #LOOK_AT ", Up = " #UP ", Distance = " #DISTANCE ", Aspect Ratio = " #ASPECT_RATIO ", At " #P, "[PerspectiveCamera]") \
+    { \
+        auto camera = raytracer::cameras::perspective(Point3D(EYE), Point3D(LOOK_AT), Vector3D(UP), DISTANCE, ASPECT_RATIO); \
+        auto ray = camera->create_ray(Point2D(P)); \
+        \
+        INFO( "Actual ray: " + show(ray) ); \
+        assert_equals(ray.origin, Point3D(EXPECTED_ORIGIN)); \
+        assert_equals(ray.direction, Vector3D(EXPECTED_DIRECTION)); \
+    }
+
+TEST_SIMPLE(XY(0.5, 0.5), XYZ(0, 0, 1))
 
 TEST_EYE_LOOKAT(XY(0.5, 0.5), XYZ(1, 0, 0), XYZ(1, 0, 1), XYZ(1, 0, 0), XYZ(0, 0, 1))
 TEST_EYE_LOOKAT(XY(0.5, 0.5), XYZ(-1, 0, 0), XYZ(-1, 0, 1), XYZ(-1, 0, 0), XYZ(0, 0, 1))
