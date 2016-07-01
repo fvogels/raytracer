b4962204335ce6967e3f57b94ef5f94f444ec226
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-01 12:50:26 +0200

    util/misc
    sort
    smallest_greater_than_zero

--- raytracer/raytracer/util/misc.h
+++ raytracer/raytracer/util/misc.h
@@ -7,3 +7,35 @@ void swap(T& x, T& y)
     x = y;
     y = temp;
 }
+
+template<typename T>
+void sort(T& x, T& y)
+{
+    if (x > y)
+    {
+        swap(x, y);
+    }
+}
+
+template<typename T>
+bool smallest_greater_than_zero(T* result, T x, T y)
+{
+    sort(x, y);
+
+    if (0 < x)
+    {
+        *result = x;
+
+        return true;
+    }
+    else if (0 < y)
+    {
+        *result = y;
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
