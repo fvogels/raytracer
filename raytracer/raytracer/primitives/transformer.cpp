6efa3a4ffcbd83a03c205cde8485e9b4bee19610
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-02 13:49:46 +0200

    primitives/transformer
    bounding_box implemented

--- raytracer/raytracer/primitives/transformer.cpp
+++ raytracer/raytracer/primitives/transformer.cpp
@@ -1,4 +1,5 @@
 #include "primitives/transformer.h"
+#include "math/misc.h"
 #include <algorithm>
 
 using namespace math;
@@ -34,14 +35,38 @@ std::vector<std::shared_ptr<Hit>> raytracer::primitives::_private_::Transformer:
         hit->position = this->transformer.transformation_matrix * hit->position;
         hit->normal = (this->transformer.transformation_matrix * hit->normal).normalized();
     }
-    
+
     return hits;
 }
 
 math::Box raytracer::primitives::_private_::Transformer::bounding_box() const
 {
-    // TODO
-    abort();
+    auto box = transformee->bounding_box();
+
+#define AUX(I, J, K)       Point3D p ## I ## J ## K = box.corner<I, J, K>()
+    AUX(0, 0, 0);
+    AUX(0, 0, 1);
+    AUX(0, 1, 0);
+    AUX(0, 1, 1);
+    AUX(1, 0, 0);
+    AUX(1, 0, 1);
+    AUX(1, 1, 0);
+    AUX(1, 1, 1);
+#undef AUX
+
+    double min_x = math::minimum(p000.x, p001.x, p010.x, p011.x, p100.x, p101.x, p110.x, p111.x);
+    double max_x = math::maximum(p000.x, p001.x, p010.x, p011.x, p100.x, p101.x, p110.x, p111.x);
+
+    double min_y = math::minimum(p000.y, p001.y, p010.y, p011.y, p100.y, p101.y, p110.y, p111.y);
+    double max_y = math::maximum(p000.y, p001.y, p010.y, p011.y, p100.y, p101.y, p110.y, p111.y);
+
+    double min_z = math::minimum(p000.z, p001.z, p010.z, p011.z, p100.z, p101.z, p110.z, p111.z);
+    double max_z = math::maximum(p000.z, p001.z, p010.z, p011.z, p100.z, p101.z, p110.z, p111.z);
+
+    Point3D lower_corner(min_x, min_y, min_z);
+    Point3D upper_corner(max_x, max_y, max_z);
+
+    return Box::from_corners(lower_corner, upper_corner);
 }
 
 Primitive raytracer::primitives::transform(const math::Transformation& transformation, Primitive transformee)
