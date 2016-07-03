718f83fd702c91fdf4a378eb35959ea5a4338cfd
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-03 23:36:42 +0200

    animation/animation

--- raytracer/raytracer/animation/animation.cpp
+++ raytracer/raytracer/animation/animation.cpp
@@ -1,15 +1,25 @@
 #include "animation/animation.h"
+#include <functional>
 
 using namespace animation;
 using namespace math;
 
+//
+//Animation<math::Point3D> animation::circular_xz(Angle start, Angle end, Duration duration)
+//{
+//    std::function<Point3D(TimeStamp)> lambda = [start, end, duration](TimeStamp ts)
+//    {
+//        double 
+//        return Point3D(cos(start + (end - start) * ts.seconds()), 0, sin(start + (end - start) * ts.seconds()));
+//    };
+//
+//    return Animation(from_lambda(lambda), );
+//}
 
-double animation::Animation::duration() const
-{
-    return m_duration;
-}
-
-Animation<Point3D> animation::rotate_around(const Point3D&, double radius, const Interval<math::Angle> angle)
-{
-
-}
+//Animation<math::Point3D> animation::circular_xz(const math::Point3D& around, double radius, Angle start, Angle end, const Duration& duration)
+//{
+//    std::function<Point3D(TimeStamp)> function = [around, radius, start, end, duration](TimeStamp ts)
+//    {
+//        return around + Point3D(radius * cos(a
+//    };
+//}
--- raytracer/raytracer/animation/animation.h
+++ raytracer/raytracer/animation/animation.h
@@ -1,10 +1,38 @@
 #pragma once
 
+#include "math/function.h"
+#include "math/point3d.h"
+#include "math/interval.h"
+#include "math/angle.h"
+#include "animation/duration.h"
+#include "animation/time-stamp.h"
+
+
 namespace animation
 {
+    template<typename T>
     class Animation
     {
     public:
-        virtual void update(double time) = 0;
+        Animation(std::shared_ptr<math::FunctionBody<T, TimeStamp>> body, Duration duration)
+            : m_body(body), m_duration(duration) { }
+
+        T operator ()(TimeStamp t)
+        {
+            return m_body->evaluate(t);
+        }
+
+        Duration duration() const
+        {
+            return m_duration;
+        }
+
+    private:
+        std::shared_ptr<math::FunctionBody<T, TimeStamp>> m_body;
+        Duration m_duration;
     };
+
+    // Animation<math::Point3D> circular_xz(math::Angle, math::Angle);
+
+    // Animation<math::Point3D> circular_xz(const math::Point3D&, double, math::Angle, math::Angle, const Duration&);
 }
