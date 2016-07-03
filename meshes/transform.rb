6c200e93e76aedb28de7c43363fb488b0b29f295
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-03 09:00:32 +0200

    meshes/transform
    Added --center

--- meshes/transform.rb
+++ meshes/transform.rb
@@ -7,29 +7,63 @@ data = load(STDIN)
 $vertices = data[:vertices]
 $triangles = data[:triangles]
 
+def x_coords
+  $vertices.map(&:x)
+end
 
-def resize(factor)
-  $vertices = $vertices.map do |vertex|
-    Vertex.new(vertex.x / factor, vertex.y / factor, vertex.z / factor)
+def y_coords
+  $vertices.map(&:y)
+end
+
+def z_coords
+  $vertices.map(&:z)
+end
+
+def scale(factor)
+  $vertices.map! do |vertex|
+    Vertex.new(vertex.x * factor, vertex.y * factor, vertex.z * factor)
   end
 end
 
+def translate(dx, dy, dz)
+  $vertices.map! do |vertex|
+    Vertex.new(vertex.x + dx, vertex.y + dy, vertex.z + dz)
+  end
+end
 
 OptionParser.new do |opts|
-  opts.on("--resize AXIS", "Scale uniformly so that mesh has size 1 in given dimension") do |axis|
-    case axis.downcase
+  opts.on("--scale ARG", "Scale uniformly") do |arg|
+    case arg.downcase
     when 'x'
     then
-      coords = $vertices.map(&:x)
+      coords = x_coords
+      factor = 1 / (coords.max - coords.min)
     when 'y'
     then
-      coords = $vertices.map(&:y)
+      coords = y_coords
+      factor = 1 / (coords.max - coords.min)
     when 'z'
     then
-      coords = $vertices.map(&:z)
+      coords = z_coords
+      factor = 1 / (coords.max - coords.min)
+    when /^[0-9.]+$/
+    then
+      factor = arg.to_f
     end
 
-    resize(coords.max - coords.min)
+    scale(factor)
+  end
+
+  opts.on("--center", "Center mesh around origin") do |arg|
+    xs = x_coords
+    ys = y_coords
+    zs = z_coords
+
+    dx = (xs.max + xs.min) / 2
+    dy = (ys.max + ys.min) / 2
+    dz = (zs.max + zs.min) / 2
+
+    translate(-dx, -dy, -dz)
   end
 end.parse!
 
