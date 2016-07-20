87d666470c8f600891d286febf9d598416876f53
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-21 01:49:54 +0200

    math/functions/patterns.cpp

--- raytracer/raytracer/math/functions/patterns.cpp
+++ raytracer/raytracer/math/functions/patterns.cpp
@@ -7,8 +7,8 @@ Function<bool, const Point2D&> math::functions::checkered()
 {
     std::function<bool(const Point2D&)> function = [](const Point2D& p)
     {
-        double x = p.x - floor(p.x);
-        double y = p.y - floor(p.y);
+        double x = p.x() - floor(p.x());
+        double y = p.y() - floor(p.y());
 
         return (x < 0.5) != (y < 0.5);
     };
@@ -20,7 +20,7 @@ Function<bool, const Point2D&> math::functions::horizontal_lines(double thicknes
 {
     std::function<bool(const Point2D&)> function = [thickness](const Point2D& p)
     {
-        auto y = p.y;
+        auto y = p.y();
 
         return fabs(y - round(y)) < thickness / 2;
     };
@@ -32,7 +32,7 @@ Function<bool, const Point2D&> math::functions::vertical_lines(double thickness)
 {
     std::function<bool(const Point2D&)> function = [thickness](const Point2D& p)
     {
-        auto x = p.x;
+        auto x = p.x();
 
         return fabs(x - round(x)) < thickness / 2;
     };
@@ -44,8 +44,8 @@ Function<bool, const Point2D&> math::functions::grid(double thickness)
 {
     std::function<bool(const Point2D&)> function = [thickness](const Point2D& p)
     {
-        auto x = p.x;
-        auto y = p.y;
+        auto x = p.x();
+        auto y = p.y();
 
         return fabs(x - round(x)) < thickness / 2 || fabs(y - round(y)) < thickness / 2;
     };
