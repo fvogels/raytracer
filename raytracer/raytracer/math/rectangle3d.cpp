7a5d6e32c436461231ce0086519a3d25dc42d585
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-21 01:18:31 +0200

    math/rectangle3d.cpp

--- raytracer/raytracer/math/rectangle3d.cpp
+++ raytracer/raytracer/math/rectangle3d.cpp
@@ -4,5 +4,5 @@ using namespace math;
 
 Point3D math::Rectangle3D::project(const Point2D& p) const
 {
-    return origin + x_axis * p.x + y_axis * p.y;
+    return origin + x_axis * p.x() + y_axis * p.y();
 }
