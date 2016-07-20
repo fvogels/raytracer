a612c034e0699b10772c8f646ee17e5afe302afc
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-21 01:09:49 +0200

    math/point2d

--- raytracer/raytracer/math/point2d.h
+++ raytracer/raytracer/math/point2d.h
@@ -1,40 +1,16 @@
 #pragma once
 
+#include "math/point.h"
 #include "math/vector2d.h"
 #include "math/approx.h"
 
 
 namespace math
 {
-    struct Point2D final
+    using Point2D = Point<2>;
+
+    Point2D point(double radius, Angle theta)
     {
-        double x, y;
-
-        constexpr Point2D() noexcept
-            : x(0), y(0) { }
-
-        constexpr Point2D(double x, double y) noexcept
-            : x(x), y(y) { }
-
-        constexpr Point2D(const Point2D& v) noexcept = default;
-
-        static Point2D origin()
-        {
-            return Point2D(0, 0);
-        }
-
-        Point2D& operator =(const Point2D&) noexcept = default;
-    };
-
-    Point2D operator +(const Point2D&, const Vector2D&) noexcept;
-    Point2D operator +(const Vector2D&, const Point2D&) noexcept;
-    Vector2D operator -(const Point2D&, const Point2D&) noexcept;
-    Point2D operator -(const Point2D&, const Vector2D&) noexcept;
-
-    bool operator ==(const Point2D&, const Point2D&) noexcept;
-    bool operator !=(const Point2D&, const Point2D&) noexcept;
-
-    double distance(const Point2D&, const Point2D&) noexcept;
-
-    std::ostream& operator <<(std::ostream& out, const Point2D&);
+        return point(radius * cos(theta), radius * sin(theta));
+    }
 }
