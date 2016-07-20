4c0b8da7ff40986511fc8f00f13109a1fcbab8bb
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-21 01:18:30 +0200

    cameras/fisheye-camera.cpp

--- raytracer/raytracer/cameras/fisheye-camera.cpp
+++ raytracer/raytracer/cameras/fisheye-camera.cpp
@@ -21,19 +21,19 @@ void raytracer::cameras::_private_::FisheyeCamera::enumerate_untransformed_rays(
     assert(0 <= point.x && point.x <= 1);
     assert(0 <= point.y && point.y <= 1);
 
-    Point3D eye(0, 0, 0);
+    Point3D eye = math::point(0, 0, 0);
 
     Interval<double> x_interval(0, 1);
     Interval<Angle> azimuth_interval(90_degrees - m_horizontal_view_angle / 2, 90_degrees + m_horizontal_view_angle / 2);
     IntervalMapper<double, Angle> azimuth_mapper(x_interval, azimuth_interval);
-    Angle azimuth = azimuth_mapper[point.x];
+    Angle azimuth = azimuth_mapper[point.x()];
 
     Interval<double> y_interval(0, 1);
     Interval<Angle> altitude_interval(-m_vertical_view_angle / 2, m_vertical_view_angle / 2);
     IntervalMapper<double, Angle> altitude_mapper(y_interval, altitude_interval);
-    Angle altitude = altitude_mapper[point.y];
+    Angle altitude = altitude_mapper[point.y()];
 
-    Point3D through(1.0, azimuth, altitude);
+    Point3D through = math::point(1.0, azimuth, altitude);
 
     callback(Ray(eye, through));
 }
