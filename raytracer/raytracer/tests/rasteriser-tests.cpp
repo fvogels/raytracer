06e3b092ca854ae19297254f19b54103972ced67
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-24 21:58:21 +0200

    tests
    position -> Position

--- raytracer/raytracer/tests/rasteriser-tests.cpp
+++ raytracer/raytracer/tests/rasteriser-tests.cpp
@@ -10,10 +10,10 @@ TEST_CASE("Rasterising [0,1]-[1,0] in 2x2", "[Rasterizer]")
     Rectangle2D rect(point(0, 1), vector(1, 0), vector(0, -1));
     Rasterizer r(rect, 2, 2);
     
-    REQUIRE(r[position(0, 0)] == Rectangle2D(point(0, 1), vector(0.5, 0), vector(0, -0.5)));
-    REQUIRE(r[position(1, 0)] == Rectangle2D(point(0.5, 1), vector(0.5, 0), vector(0, -0.5)));
-    REQUIRE(r[position(0, 1)] == Rectangle2D(point(0, 0.5), vector(0.5, 0), vector(0, -0.5)));
-    REQUIRE(r[position(1, 1)] == Rectangle2D(point(0.5, 0.5), vector(0.5, 0), vector(0, -0.5)));
+    REQUIRE(r[Position(0, 0)] == Rectangle2D(point(0, 1), vector(0.5, 0), vector(0, -0.5)));
+    REQUIRE(r[Position(1, 0)] == Rectangle2D(point(0.5, 1), vector(0.5, 0), vector(0, -0.5)));
+    REQUIRE(r[Position(0, 1)] == Rectangle2D(point(0, 0.5), vector(0.5, 0), vector(0, -0.5)));
+    REQUIRE(r[Position(1, 1)] == Rectangle2D(point(0.5, 0.5), vector(0.5, 0), vector(0, -0.5)));
 }
 
 #endif
