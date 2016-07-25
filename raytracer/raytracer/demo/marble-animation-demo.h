24282ea953b5f28294c130c054b842a62396fa1c
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-25 23:40:33 +0200

    demo/marble-animation-demo
    Uses consumers

--- raytracer/raytracer/demo/marble-animation-demo.h
+++ raytracer/raytracer/demo/marble-animation-demo.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include "bitmap-consumers/bitmap-consumer.h"
+#include "pipeline/consumer.h"
 #include <string>
 
 
 namespace demos
 {
-    void marble(std::shared_ptr<imaging::BitmapConsumer>);
+    void marble(std::shared_ptr<raytracer::pipeline::Consumer<std::shared_ptr<imaging::Bitmap>>>);
 }
