5562996485dab39ecf6379b719243a5311d692a9
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-25 23:03:01 +0200

    math/rasterizer
    Slight refactoring

--- raytracer/raytracer/math/rasterizer.h
+++ raytracer/raytracer/math/rasterizer.h
@@ -9,16 +9,18 @@ namespace math
     {
     private:
         Rectangle2D rect;
-        unsigned horizontal_subdivisions;
-        unsigned vertical_subdivisions;
+        unsigned m_horizontal_subdivisions;
+        unsigned m_vertical_subdivisions;
 
     public:
         Rasterizer(const Rectangle2D& rect, unsigned horizontal_subdivisions, unsigned vertical_subdivisions)
-            : rect(rect), horizontal_subdivisions(horizontal_subdivisions), vertical_subdivisions(vertical_subdivisions) { }
-
+            : rect(rect), m_horizontal_subdivisions(horizontal_subdivisions), m_vertical_subdivisions(vertical_subdivisions) { }
         Rasterizer(const Rasterizer&) = default;
 
         Rasterizer& operator =(const Rasterizer&) = default;
+
+        unsigned horizontal_subdivisions() const { return m_horizontal_subdivisions; }
+        unsigned vertical_subdivisions() const { return m_vertical_subdivisions; }
 
         Rectangle2D operator [](const Position&) const;
     };
