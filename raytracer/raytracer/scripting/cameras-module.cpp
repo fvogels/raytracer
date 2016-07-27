54d81f5166aef571c18d4f61a7c3bca9128e8cbd
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-27 21:53:40 +0200

    scripting/cameras-module
    depth-of-field-perspective

--- raytracer/raytracer/scripting/cameras-module.cpp
+++ raytracer/raytracer/scripting/cameras-module.cpp
@@ -40,6 +40,18 @@ namespace
         {
             return cameras::orthographic(eye, look_at, up, horizontal_angle, vertical_angle);
         }
+
+        Camera depth_of_field_perspective(
+            const math::Point3D& eye,
+            const math::Point3D& look_at,
+            const math::Vector3D& up,
+            double distance,
+            double aspect_ratio,
+            double eye_size,
+            raytracer::Sampler eye_sampler) const
+        {
+            return cameras::depth_of_field_perspective(eye, look_at, up, distance, aspect_ratio, eye_size, eye_sampler);
+        }
     };
 
     Camera perspective(const std::map<std::string, Boxed_Value> argument_map)
@@ -75,12 +87,27 @@ namespace
         return cameras::fisheye(eye, look_at, up, Angle::degrees(horizontal_angle), Angle::degrees(vertical_angle));
     }
 
+    Camera depth_of_field_perspective(const std::map<std::string, Boxed_Value> argument_map)
+    {
+        EXTRACT_ARGUMENT(Point3D, eye);
+        EXTRACT_ARGUMENT(Point3D, look_at);
+        EXTRACT_ARGUMENT(Vector3D, up);
+        EXTRACT_ARGUMENT(double, distance);
+        EXTRACT_ARGUMENT(double, aspect_ratio);
+        EXTRACT_ARGUMENT(double, eye_size);
+        EXTRACT_ARGUMENT(Sampler, eye_sampler);
+
+        return cameras::depth_of_field_perspective(eye, look_at, up, distance, aspect_ratio, eye_size, eye_sampler);
+    }
+
     Camera create_camera(const std::map<std::string, Boxed_Value> argument_map)
     {
         EXTRACT_ARGUMENT(std::string, type);
 
         FACTORY_TYPE_DISPATCH(perspective);
         FACTORY_TYPE_DISPATCH(orthographic);
+        FACTORY_TYPE_DISPATCH(fisheye);
+        FACTORY_TYPE_DISPATCH(depth_of_field_perspective);
         HANDLE_UNKNOWN_TYPE;
     }
 }
