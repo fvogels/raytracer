ab3f80533926541ec088d89405a378e39a77fe6c
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-27 13:30:48 +0200

    math/functions/easing-functions
    Fixed quad_inout

--- raytracer/raytracer/math/functions/easing-functions.cpp
+++ raytracer/raytracer/math/functions/easing-functions.cpp
@@ -45,7 +45,14 @@ EasingFunction math::functions::easing::_private_::quadratic_inout()
     std::function<double(double)> lambda = [](double t) {
         assert(Interval<double>(0, 1).contains(t));
 
-        return (3 - 2 * t) * t * t;
+        if (t < -0.5)
+        {
+            return 2 * pow(t, 2);
+        }
+        else
+        {
+            return -2 * pow(t, 2) + 4 * t - 1;
+        }
     };
 
     return from_lambda(lambda);
