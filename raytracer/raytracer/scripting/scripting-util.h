eb6bac1aecf2496b261461d4f83637d62333c2a0
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-29 00:31:13 +0200

    scripting/scripting-util
    cast_vector_elements

--- raytracer/raytracer/scripting/scripting-util.h
+++ raytracer/raytracer/scripting/scripting-util.h
@@ -3,6 +3,7 @@
 #include <chaiscript/chaiscript.hpp>
 #include <string>
 #include <map>
+#include <algorithm>
 
 #define EXTRACT_ARGUMENT(TYPE, NAME)            auto NAME = raytracer::scripting::util::get_argument<TYPE>(argument_map, #NAME)
 #define FACTORY_TYPE_DISPATCH(TYPE)             if ( type == #TYPE ) return TYPE(argument_map)
@@ -33,6 +34,18 @@ namespace raytracer
                 {
                     return chaiscript::boxed_cast<double>(value);
                 }
+            }
+
+            template<typename T>
+            std::vector<T> cast_vector_elements(const std::vector<chaiscript::Boxed_Value>& boxed_values)
+            {
+                std::vector<T> unboxed_values(boxed_values.size());
+ 
+                std::transform(boxed_values.begin(), boxed_values.end(), unboxed_values.begin(), [](Boxed_Value boxed_value) {
+                    return boxed_cast<T>(boxed_value);
+                });
+
+                return unboxed_values;
             }
         }
     }
