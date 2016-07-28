5f48732e688da34f02eac78e56266f4e50ef7e8d
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-29 01:28:53 +0200

    tests/scripting-tests

--- raytracer/raytracer/tests/scripting-tests.cpp
+++ raytracer/raytracer/tests/scripting-tests.cpp
@@ -5,7 +5,7 @@
 #include "math/vector3d.h"
 #include "math/point3d.h"
 #include "math/approx.h"
-#include "imaging/color.h"
+#include "imaging/Color.h"
 #include "raytracing/scene.h"
 #include <sstream>
 
@@ -49,56 +49,103 @@ using namespace raytracer::scripting;
 //
 //TEST_CASE("[Scripting] colors.black()", "[Scripting]")
 //{
-//    auto result = evaluate<imaging::color>("Colors.black()");
+//    auto result = evaluate<imaging::Color>("Colors.black()");
 //
 //    REQUIRE(result == math::approx(imaging::colors::black()));
 //}
 //
 //TEST_CASE("[Scripting] colors.white()", "[Scripting]")
 //{
-//    auto result = evaluate<imaging::color>("Colors.white()");
+//    auto result = evaluate<imaging::Color>("Colors.white()");
 //
 //    REQUIRE(result == math::approx(imaging::colors::white()));
 //}
 //
 //TEST_CASE("[Scripting] colors.red()", "[Scripting]")
 //{
-//    auto result = evaluate<imaging::color>("Colors.red()");
+//    auto result = evaluate<imaging::Color>("Colors.red()");
 //
 //    REQUIRE(result == math::approx(imaging::colors::red()));
 //}
 //
 //TEST_CASE("[Scripting] colors.blue() * 0.5", "[Scripting]")
 //{
-//    auto result = evaluate<imaging::color>("Colors.blue() * 0.5");
+//    auto result = evaluate<imaging::Color>("Colors.blue() * 0.5");
 //
 //    REQUIRE(result == math::approx(imaging::colors::blue() * 0.5));
 //}
+//
+//TEST_CASE("[Scripting] Creating perspective camera", "[Scripting]")
+//{
+//    auto result = evaluate<raytracer::Camera>(R"(
+//        var camera = Cameras.perspective(pos(0,0,5), pos(0,0,0), vec(0,1,0), 1, 1)
+//        camera
+//    )");
+//
+//    REQUIRE(!!result);
+//}
+//
+//TEST_CASE("[Scripting] Creating perspective camera using argument map", "[Scripting]")
+//{
+//    auto result = evaluate<raytracer::Camera>(R"(
+//        camera(["type": "perspective", "eye": pos(0,0,5), "look_at": pos(0,0,0), "up": vec(0,1,0), "distance": 1, "aspect_ratio": 1])
+//    )");
+//
+//    REQUIRE(!!result);
+//}
+//
+//TEST_CASE("[Scripting] Testing maps and casts", "[Scripting]")
+//{
+//    auto result = evaluate<std::map<std::string, chaiscript::Boxed_Value>>(R"(
+//        ["x": 1.0, "y": "hello"]
+//    )");
+//
+//    REQUIRE(result.size() == 2);
+//
+//    auto first = result["x"];
+//    auto second = result["y"];
+//
+//    REQUIRE(chaiscript::boxed_cast<double>(first) == 1);
+//    REQUIRE(chaiscript::boxed_cast<std::string>(second) == "hello");
+//}
 
-TEST_CASE("[Scripting] Creating perspective camera", "[Scripting]")
+
+
+
+TEST_CASE("[Scripting] Testing pipeline", "[Scripting]")
 {
-    // 
-    auto result = evaluate<raytracer::Camera>(R"(
-        var camera = Cameras.perspective(pos(0,0,5), pos(0,0,0), vec(0,1,0), 1, 1)
-        camera
+    evaluate<void>(R"(
+        var camera   = Cameras.perspective(pos(0,0,5), pos(0,0,0), vec(0,1,0), 1, 1)
+        var material = Materials.uniform( Colors.white() * 0.2,
+                                          Colors.white() * 0.8,
+                                          Colors.white(),
+                                          20,
+                                          0.2,
+                                          0,
+                                          0 )
+        var root     = Primitives.decorate( material, Primitives.sphere() )
+        var lights   = [ Lights.omnidirectional(pos(0,5,0), Colors.white()) ]
+        var scene    = scene(camera, root, lights )
+        pipeline(scene, [ Pipeline.renderer()
+                        , Pipeline.wif("e:/temp/output/test.wif") ])
     )");
-
-    REQUIRE(!!result);
 }
 
-TEST_CASE("[Scripting] Adding light to scene", "[Scripting]")
-{
-    auto result = evaluate<raytracer::Scene>(R"(
-        var camera = Cameras.perspective(pos(0,0,5), pos(0,0,0), vec(0,1,0), 1, 1)
-        var root   = Primitives.sphere()
-        var scene  = Scene(camera, root)
 
-        add_light( scene, Lights.omnidirectional(pos(0,5,0), Colors.white()) )
-
-        scene
-    )");
-
-    REQUIRE(result.light_sources.size() == 1);
-}
+//
+//TEST_CASE("[Scripting] Adding light to scene", "[Scripting]")
+//{
+//    auto result = evaluate<raytracer::Scene>(R"(
+//        var camera = Cameras.perspective(pos(0,0,5), pos(0,0,0), vec(0,1,0), 1, 1)
+//        var root   = sphere()
+//        var scene  = Scene(camera, root)
+//
+//        add_light( scene, Lights.omnidirectional(pos(0,5,0), Colors.white()) )
+//
+//        scene
+//    )");
+//
+//    REQUIRE(result.light_sources.size() == 1);
+//}
 
 #endif
