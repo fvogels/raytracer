1a7948c858c6e8c8a99254819456fb992d3ec5af
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-29 13:18:46 +0200

    scripting/rendering-module
    Added different renderers

--- raytracer/raytracer/scripting/rendering-module.cpp
+++ raytracer/raytracer/scripting/rendering-module.cpp
@@ -3,6 +3,7 @@
 #include "raytracing/ray-tracer.h"
 #include "sampling/samplers.h"
 #include "imaging/wif-format.h"
+#include "util/looper.h"
 
 using namespace chaiscript;
 using namespace raytracer;
@@ -12,24 +13,36 @@ namespace
 {
     struct RendererLibrary
     {
-        Renderer single_threaded(unsigned width, unsigned height, RayTracer ray_tracer) const
+        Renderer standard(unsigned width, unsigned height, Sampler sampler, RayTracer ray_tracer) const
         {
-            return raytracer::rendering::single_threaded(width, height, raytracer::samplers::single(), ray_tracer);
+            return raytracer::rendering::standard(width, height, sampler, ray_tracer, util::loopers::looper(4));
+        }
+
+        Renderer standard2(unsigned width, unsigned height, Sampler sampler, RayTracer ray_tracer, unsigned thread_count) const
+        {
+            return raytracer::rendering::standard(width, height, sampler, ray_tracer, util::loopers::looper(thread_count));
+        }
+
+        Renderer edge(unsigned width, unsigned height, Sampler sampler, RayTracer ray_tracer, double edge_thickness) const
+        {
+            return raytracer::rendering::edge(width, height, sampler, ray_tracer, util::loopers::looper(4), edge_thickness);
+        }
+
+        Renderer edge2(unsigned width, unsigned height, Sampler sampler, RayTracer ray_tracer, double edge_thickness, unsigned thread_count) const
+        {
+            return raytracer::rendering::edge(width, height, sampler, ray_tracer, util::loopers::looper(thread_count), edge_thickness);
+        }
+
+        Renderer cartoon(unsigned width, unsigned height, Sampler sampler, RayTracer ray_tracer, unsigned shade_count, double edge_thickness) const
+        {
+            return raytracer::rendering::cartoon(width, height, sampler, ray_tracer, util::loopers::looper(4), shade_count, edge_thickness);
+        }
+
+        Renderer cartoon2(unsigned width, unsigned height, Sampler sampler, RayTracer ray_tracer, unsigned shade_count, double edge_thickness, unsigned thread_count) const
+        {
+            return raytracer::rendering::cartoon(width, height, sampler, ray_tracer, util::loopers::looper(thread_count), shade_count, edge_thickness);
         }
     };
-
-    std::shared_ptr<imaging::Bitmap> render(unsigned width, unsigned height, RayTracer ray_tracer, const Scene& scene)
-    {
-        auto renderer = raytracer::rendering::single_threaded(width, height, samplers::single(), ray_tracer);
-
-        return renderer->render(scene);
-    }
-
-    void save_as_wif(imaging::Bitmap bitmap, std::string path)
-    {
-        imaging::WIF wif(path);
-        wif.write_frame(bitmap);
-    }
 }
 
 ModulePtr raytracer::scripting::_private_::create_rendering_module()
@@ -39,12 +52,14 @@ ModulePtr raytracer::scripting::_private_::create_rendering_module()
     auto renderer_library = std::make_shared<RendererLibrary>();
     module->add_global_const(chaiscript::const_var(renderer_library), "Renderers");
 
-#define RENDERER(NAME) module->add(fun(&RendererLibrary::NAME), #NAME)
-    RENDERER(single_threaded);
+#define RENDERER(INTERNAL, EXTERNAL) module->add(fun(&RendererLibrary::INTERNAL), #EXTERNAL)
+    RENDERER(standard, standard);
+    RENDERER(standard2, standard);
+    RENDERER(edge, edge);
+    RENDERER(edge2, edge);
+    RENDERER(cartoon, cartoon);
+    RENDERER(cartoon2, cartoon);
 #undef COLOR
-
-    module->add(fun(&render), "render");
-    module->add(fun(&save_as_wif), "save_as_wif");
 
     return module;
 }
