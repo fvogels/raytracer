e13e0f99cbfe9e7f59affa4fbc2d5e0df8ed6d5d
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-29 12:20:53 +0200

    scripting/camera-module
    Rewrote using ArgumentMapParser

--- raytracer/raytracer/scripting/cameras-module.cpp
+++ raytracer/raytracer/scripting/cameras-module.cpp
@@ -23,11 +23,13 @@ namespace
 
         Camera perspective_by_map(const std::map<std::string, Boxed_Value> argument_map) const
         {
-            EXTRACT_ARGUMENT(Point3D, eye);
-            EXTRACT_ARGUMENT(Point3D, look_at);
-            EXTRACT_ARGUMENT(Vector3D, up);
-            EXTRACT_ARGUMENT(double, distance);
-            EXTRACT_ARGUMENT(double, aspect_ratio);
+            START_ARGUMENTS(argument_map);
+            ARGUMENT(Point3D, eye);
+            ARGUMENT(Point3D, look_at);
+            ARGUMENT(Vector3D, up);
+            ARGUMENT(double, distance);
+            ARGUMENT(double, aspect_ratio);
+            END_ARGUMENTS();
 
             return cameras::perspective(eye, look_at, up, distance, aspect_ratio);
         }
@@ -44,11 +46,13 @@ namespace
 
         Camera orthographic_by_map(const std::map<std::string, Boxed_Value> argument_map) const
         {
-            EXTRACT_ARGUMENT(Point3D, eye);
-            EXTRACT_ARGUMENT(Point3D, look_at);
-            EXTRACT_ARGUMENT(Vector3D, up);
-            EXTRACT_ARGUMENT(double, window_width);
-            EXTRACT_ARGUMENT(double, aspect_ratio);
+            START_ARGUMENTS(argument_map);
+            ARGUMENT(Point3D, eye);
+            ARGUMENT(Point3D, look_at);
+            ARGUMENT(Vector3D, up);
+            ARGUMENT(double, window_width);
+            ARGUMENT(double, aspect_ratio);
+            END_ARGUMENTS();
 
             return cameras::orthographic(eye, look_at, up, window_width, aspect_ratio);
         }
@@ -65,11 +69,13 @@ namespace
 
         Camera fisheye_by_map(const std::map<std::string, Boxed_Value> argument_map) const
         {
-            EXTRACT_ARGUMENT(Point3D, eye);
-            EXTRACT_ARGUMENT(Point3D, look_at);
-            EXTRACT_ARGUMENT(Vector3D, up);
-            EXTRACT_ARGUMENT(double, horizontal_angle);
-            EXTRACT_ARGUMENT(double, vertical_angle);
+            START_ARGUMENTS(argument_map);
+            ARGUMENT(Point3D, eye);
+            ARGUMENT(Point3D, look_at);
+            ARGUMENT(Vector3D, up);
+            ARGUMENT(double, horizontal_angle);
+            ARGUMENT(double, vertical_angle);
+            END_ARGUMENTS();
 
             return cameras::fisheye(eye, look_at, up, Angle::degrees(horizontal_angle), Angle::degrees(vertical_angle));
         }
@@ -88,28 +94,19 @@ namespace
 
         Camera depth_of_field_perspective_by_map(const std::map<std::string, Boxed_Value> argument_map) const
         {
-            EXTRACT_ARGUMENT(Point3D, eye);
-            EXTRACT_ARGUMENT(Point3D, look_at);
-            EXTRACT_ARGUMENT(Vector3D, up);
-            EXTRACT_ARGUMENT(double, distance);
-            EXTRACT_ARGUMENT(double, aspect_ratio);
-            EXTRACT_ARGUMENT(double, eye_size);
-            EXTRACT_ARGUMENT(Sampler, eye_sampler);
+            START_ARGUMENTS(argument_map);
+            ARGUMENT(Point3D, eye);
+            ARGUMENT(Point3D, look_at);
+            ARGUMENT(Vector3D, up);
+            ARGUMENT(double, distance);
+            ARGUMENT(double, aspect_ratio);
+            ARGUMENT(double, eye_size);
+            ARGUMENT(Sampler, eye_sampler);
+            END_ARGUMENTS();
 
             return cameras::depth_of_field_perspective(eye, look_at, up, distance, aspect_ratio, eye_size, eye_sampler);
         }
     };
-
-    Camera create_camera(const std::map<std::string, Boxed_Value> argument_map)
-    {
-        EXTRACT_ARGUMENT(std::string, type);
-
-        // FACTORY_TYPE_DISPATCH(perspective);
-        // FACTORY_TYPE_DISPATCH(orthographic);
-        // FACTORY_TYPE_DISPATCH(fisheye);
-        // FACTORY_TYPE_DISPATCH(depth_of_field_perspective);
-        HANDLE_UNKNOWN_TYPE;
-    }
 }
 
 ModulePtr raytracer::scripting::_private_::create_cameras_module()
@@ -125,8 +122,6 @@ ModulePtr raytracer::scripting::_private_::create_cameras_module()
     CAMERA(fisheye);
     CAMERA(depth_of_field_perspective);
 #undef COLOR
-
-    module->add(fun(&create_camera), "camera");
 
     return module;
 }
