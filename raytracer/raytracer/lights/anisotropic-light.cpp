a6c4b1defffa658585ae99bd0204b1b63d5bac4d
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-29 21:08:11 +0200

    light/anisotropic-light
    Made truly anisotropic

--- raytracer/raytracer/lights/anisotropic-light.cpp
+++ raytracer/raytracer/lights/anisotropic-light.cpp
@@ -1,5 +1,6 @@
 #include "lights/anisotropic-light.h"
 #include "math/interval.h"
+#include "math/coordinate-systems.h"
 #include <assert.h>
 
 using namespace math;
@@ -11,10 +12,15 @@ namespace
     class AnisotropicLight : public lights::_private_::PointLightImplementation
     {
     public:
-        AnisotropicLight(const math::Point3D& position, const math::Vector3D& direction, math::Function<imaging::Color, double> light_function)
-            : lights::_private_::PointLightImplementation(position), m_direction(direction), m_light_function(light_function)
+        AnisotropicLight(const math::Point3D& position, const math::Vector3D& direction, const math::Vector3D& up, math::Function<imaging::Color, Angle, Angle> light_function)
+            : lights::_private_::PointLightImplementation(position), m_direction(direction), m_right(direction.cross(up)), m_up(m_right.cross(m_direction)), m_light_function(light_function)
         {
-            assert(direction.is_unit());
+            assert(m_direction.is_unit());
+            assert(m_up.is_unit());
+            assert(m_right.is_unit());
+            assert(m_direction.is_perpendicular_on(m_up));
+            assert(m_direction.is_perpendicular_on(m_right));
+            assert(m_right.is_perpendicular_on(m_up));
             assert(light_function);
         }
 
@@ -24,42 +30,67 @@ namespace
             assert(m_direction.is_unit());
 
             Ray ray(m_position, p);
-            double cos = ray.direction.normalized().dot(m_direction);
+            auto ray_direction = ray.direction.normalized();
 
-            assert(Interval<double>(-1, 1).contains(cos));
+            double x = ray_direction.dot(m_right);
+            double y = ray_direction.dot(m_direction);
+            double z = ray_direction.dot(m_up);
 
-            Color color = m_light_function(cos);
+            assert(Interval<double>(-1, 1).contains(x));
+            assert(Interval<double>(-1, 1).contains(y));
+            assert(Interval<double>(-1, 1).contains(z));
+            assert(x * m_right + y * m_direction + z * m_up == approx(ray_direction));
+            
+            Cartesian3D cartesian{ x,y,z };
+            Spherical spherical = convert_coordinates<Spherical>(cartesian);
+            Color color = m_light_function(spherical.azimuth, spherical.elevation);
 
             return LightRay(ray, color);
         }
 
     private:
         math::Vector3D m_direction;
-        math::Function<imaging::Color, double> m_light_function;
+        math::Vector3D m_right;
+        math::Vector3D m_up; // Must appear last! Important for initialization order
+        math::Function<imaging::Color, Angle, Angle> m_light_function;
     };
 }
 
 
-LightSource raytracer::lights::anisotropic(const math::Point3D& position, const math::Vector3D& direction,  math::Function<imaging::Color, double> light_function)
+LightSource raytracer::lights::anisotropic(const math::Point3D& position, const math::Vector3D& direction, math::Function<imaging::Color, Angle> light_function)
 {
-    return LightSource(std::make_shared<AnisotropicLight>(position, direction, light_function));
-}
+    assert(direction.is_unit());
 
-LightSource raytracer::lights::anisotropic(const math::Point3D& position, const math::Point3D& pointed_at,  math::Function<imaging::Color, double> light_function)
-{
-    return LightSource(std::make_shared<AnisotropicLight>(position, (pointed_at - position).normalized(), light_function));
-}
-
-LightSource raytracer::lights::anisotropic_monochromatic(const math::Point3D& position, const math::Vector3D& direction, math::Function<double, double> brightness_function, imaging::Color color)
-{
-    std::function<Color(double)> lambda = [=](double cos) {
-        return color * brightness_function(cos);
+    std::function<Color(Angle, Angle)> lambda = [=](Angle azimuth, Angle elevation) {
+        return light_function(elevation);
     };
 
-    return LightSource(std::make_shared<AnisotropicLight>(position, direction, from_lambda(lambda)));
+    Vector3D up(0, -direction.z(), direction.y());
+    up.normalize();
+
+    assert(direction.is_perpendicular_on(up));
+    assert(up.is_unit());
+
+    return LightSource(std::make_shared<AnisotropicLight>(position, direction, up, from_lambda(lambda)));
 }
 
-LightSource raytracer::lights::anisotropic_monochromatic(const math::Point3D& position, const math::Point3D& pointed_at, math::Function<double, double> brightness_function, imaging::Color color)
+LightSource raytracer::lights::anisotropic(const math::Point3D& position, const math::Point3D& pointed_at,  math::Function<imaging::Color, Angle> light_function)
 {
-    return anisotropic_monochromatic(position, (pointed_at - position).normalized(), brightness_function, color);
+    auto direction = (pointed_at - position).normalized();
+
+    return anisotropic(position, direction, light_function);
 }
+
+//LightSource raytracer::lights::anisotropic_monochromatic(const math::Point3D& position, const math::Vector3D& direction, math::Function<double, double> brightness_function, imaging::Color color)
+//{
+//    std::function<Color(double, double)> lambda = [=](double azimuth, double elevation) {
+//        return color * brightness_function(elevation);
+//    };
+//
+//    return LightSource(std::make_shared<AnisotropicLight>(position, direction, from_lambda(lambda)));
+//}
+//
+//LightSource raytracer::lights::anisotropic_monochromatic(const math::Point3D& position, const math::Point3D& pointed_at, math::Function<double, double> brightness_function, imaging::Color color)
+//{
+//    return anisotropic_monochromatic(position, (pointed_at - position).normalized(), brightness_function, color);
+//}
--- raytracer/raytracer/lights/anisotropic-light.h
+++ raytracer/raytracer/lights/anisotropic-light.h
@@ -9,14 +9,9 @@ namespace raytracer
 {
     namespace lights
     {
-        namespace _private_
-        {
-            
-        }
-
-        LightSource anisotropic(const math::Point3D&, const math::Vector3D&, math::Function<imaging::Color, double>);
-        LightSource anisotropic(const math::Point3D&, const math::Point3D&, math::Function<imaging::Color, double>);
-        LightSource anisotropic_monochromatic(const math::Point3D&, const math::Vector3D&, math::Function<double, double>, imaging::Color);
-        LightSource anisotropic_monochromatic(const math::Point3D&, const math::Point3D&, math::Function<double, double>, imaging::Color);
+        LightSource anisotropic(const math::Point3D&, const math::Vector3D&, math::Function<imaging::Color, math::Angle>);
+        LightSource anisotropic(const math::Point3D&, const math::Point3D&, math::Function<imaging::Color, math::Angle>);
+        //LightSource anisotropic_monochromatic(const math::Point3D&, const math::Vector3D&, math::Function<double, double>, imaging::Color);
+        //LightSource anisotropic_monochromatic(const math::Point3D&, const math::Point3D&, math::Function<double, double>, imaging::Color);
     }
 }
