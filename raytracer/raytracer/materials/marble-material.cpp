3b99f8ff0a7ef122959bfd4bdc21cfdbe2cae5f7
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-30 10:40:42 +0200

    materials/marble-material
    Removed shine

--- raytracer/raytracer/materials/marble-material.cpp
+++ raytracer/raytracer/materials/marble-material.cpp
@@ -20,7 +20,7 @@ Material raytracer::materials::marble2d(unsigned octaves, double turbulence)
 Material raytracer::materials::marble3d(unsigned octaves, double turbulence)
 {
     std::function<MaterialProperties(double)> converter = [](double t) -> MaterialProperties {
-        MaterialProperties properties(colors::black(), t * colors::white(), colors::white(), 20.0, 0.0, 0.0, 0.0);
+        MaterialProperties properties(colors::black(), t * colors::white(), colors::black(), 0.0, 0.0, 0.0, 0.0);
 
         return properties;
     };
