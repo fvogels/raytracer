efeb03b89e8b287a4ad6d045b3e9d9cec24309d8
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-31 22:54:45 +0200

    primitives/cone
    To cpp

--- raytracer/raytracer/primitives/cone.cpp
+++ raytracer/raytracer/primitives/cone.cpp
@@ -10,87 +10,95 @@ using namespace raytracer;
 using namespace raytracer::primitives;
 
 
-bool raytracer::primitives::_private_::ConeZ::find_hit(const math::Ray& ray, Hit* hit) const
+namespace
 {
-    assert(hit);
-
-    Point2D O2D = point(ray.origin.x(), ray.origin.y());
-    Vector2D D2D = vector(ray.direction.x(), ray.direction.y());
-    Vector2D OP = O2D - point(0, 0);
-
-    double a = D2D.dot(D2D) - pow(ray.direction.z(), 2);
-    double b = 2 * (D2D.dot(OP) - ray.direction.z() * ray.origin.z());
-    double c = OP.dot(OP) - pow(ray.origin.z(), 2);
-
-    QuadraticEquation eq(a, b, c);
-
-    if (eq.has_solutions())
+    class ConeZ : public raytracer::primitives::_private_::PrimitiveImplementation
     {
-        double t1 = eq.x1();
-        double t2 = eq.x2();
-
-        sort(t1, t2);
-
-        double t;
-        if (0 < t1)
+    public:
+        bool find_hit(const math::Ray& ray, Hit* hit) const override
         {
-            t = t1;
-        }
-        else if (0 < t2)
-        {
-            t = t2;
-        }
-        else
-        {
-            return false;
-        }
+            assert(hit);
 
-        if (t < hit->t)
-        {
-            Point3D position = ray.at(t);
+            Point2D O2D = point(ray.origin.x(), ray.origin.y());
+            Vector2D D2D = vector(ray.direction.x(), ray.direction.y());
+            Vector2D OP = O2D - point(0, 0);
 
-            if (position == point(0, 0, 0))
+            double a = D2D.dot(D2D) - pow(ray.direction.z(), 2);
+            double b = 2 * (D2D.dot(OP) - ray.direction.z() * ray.origin.z());
+            double c = OP.dot(OP) - pow(ray.origin.z(), 2);
+
+            QuadraticEquation eq(a, b, c);
+
+            if (eq.has_solutions())
             {
-                return false;
+                double t1 = eq.x1();
+                double t2 = eq.x2();
+
+                sort(t1, t2);
+
+                double t;
+                if (0 < t1)
+                {
+                    t = t1;
+                }
+                else if (0 < t2)
+                {
+                    t = t2;
+                }
+                else
+                {
+                    return false;
+                }
+
+                if (t < hit->t)
+                {
+                    Point3D position = ray.at(t);
+
+                    if (position == point(0, 0, 0))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        Point2D position_on_circle = point(position.x(), position.y());
+                        double height = position.z();
+                        double theta = atan2(position.y(), position.x());
+
+                        hit->t = t;
+                        hit->position = position;
+                        hit->local_position.xyz = position;
+                        hit->local_position.uv = point(theta, height);
+                        hit->normal = vector(position.x(), position.y(), -position.z()).normalized();
+
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
             }
             else
             {
-                Point2D position_on_circle = point(position.x(), position.y());
-                double height = position.z();
-                double theta = atan2(position.y(), position.x());
-
-                hit->t = t;
-                hit->position = position;
-                hit->local_position.xyz = position;
-                hit->local_position.uv = point(theta, height);
-                hit->normal = vector(position.x(), position.y(), -position.z()).normalized();
-
-                return true;
+                return false;
             }
         }
-        else
+
+        std::vector<std::shared_ptr<Hit>> hits(const math::Ray& ray) const override
         {
-            return false;
+            // TODO
+            abort();
         }
-    }
-    else
-    {
-        return false;
-    }
-}
 
-std::vector<std::shared_ptr<Hit>> raytracer::primitives::_private_::ConeZ::hits(const math::Ray& ray) const
-{
-    // TODO
-    abort();
-}
-
-math::Box raytracer::primitives::_private_::ConeZ::bounding_box() const
-{
-    return Box::infinite();
+        math::Box bounding_box() const override
+        {
+            // TODO
+            return Box::infinite();
+        }
+    };
 }
 
 Primitive raytracer::primitives::cone_along_z()
 {
-    return Primitive(std::make_shared<_private_::ConeZ>());
+    return Primitive(std::make_shared<ConeZ>());
 }
