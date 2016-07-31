7f1a7d2490e3a39c14154ba61bdab191118f19cd
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-31 22:28:47 +0200

    lights/spot-light
    Moved class declaration to cpp

--- raytracer/raytracer/lights/spot-light.cpp
+++ raytracer/raytracer/lights/spot-light.cpp
@@ -5,34 +5,46 @@ using namespace math;
 using namespace raytracer;
 using namespace imaging;
 
-
-raytracer::lights::_private_::SpotLight::SpotLight(const math::Point3D& position, const math::Vector3D& direction, math::Angle angle, const imaging::Color& color)
-    : PointLightImplementation(position), m_direction(direction), m_min_cos(cos(angle)), m_color(color)
+namespace
 {
-    assert(direction.is_unit());
-}
-
-LightRay raytracer::lights::_private_::SpotLight::cast_lightray_to(const math::Point3D& p) const
-{
-    Ray ray(m_position, p);
-    double intensity = ray.direction.normalized().dot(m_direction);
-
-    if (intensity > m_min_cos)
+    class SpotLight : public raytracer::lights::_private_::PointLightImplementation
     {
-        return LightRay(ray, m_color);
-    }
-    else
-    {
-        return LightRay(ray, colors::black());
-    }
+    public:
+        SpotLight(const math::Point3D& position, const math::Vector3D& direction, math::Angle angle, const imaging::Color& color)
+            : PointLightImplementation(position), m_direction(direction), m_min_cos(cos(angle)), m_color(color)
+        {
+            assert(direction.is_unit());
+        }
+
+    protected:
+        LightRay cast_lightray_to(const math::Point3D& p) const override
+        {
+            Ray ray(m_position, p);
+            double intensity = ray.direction.normalized().dot(m_direction);
+
+            if (intensity > m_min_cos)
+            {
+                return LightRay(ray, m_color);
+            }
+            else
+            {
+                return LightRay(ray, colors::black());
+            }
+        }
+
+    private:
+        math::Vector3D m_direction;
+        double m_min_cos;
+        imaging::Color m_color;
+    };
 }
 
 LightSource raytracer::lights::spot(const math::Point3D& position, const math::Vector3D& direction, math::Angle angle, const imaging::Color& color)
 {
-    return LightSource(std::make_shared<raytracer::lights::_private_::SpotLight>(position, direction, angle, color));
+    return LightSource(std::make_shared<SpotLight>(position, direction, angle, color));
 }
 
 LightSource raytracer::lights::spot(const math::Point3D& position, const math::Point3D& pointed_at, math::Angle angle, const imaging::Color& color)
 {
-    return LightSource(std::make_shared<raytracer::lights::_private_::SpotLight>(position, (pointed_at - position).normalized(), angle, color));
+    return LightSource(std::make_shared<SpotLight>(position, (pointed_at - position).normalized(), angle, color));
 }
--- raytracer/raytracer/lights/spot-light.h
+++ raytracer/raytracer/lights/spot-light.h
@@ -8,23 +8,6 @@ namespace raytracer
 {
     namespace lights
     {
-        namespace _private_
-        {
-            class SpotLight : public PointLightImplementation
-            {
-            public:
-                SpotLight(const math::Point3D&, const math::Vector3D&, math::Angle, const imaging::Color&);
-
-            protected:
-                LightRay cast_lightray_to(const math::Point3D&) const;
-
-            private:
-                math::Vector3D m_direction;
-                double m_min_cos;
-                imaging::Color m_color;
-            };
-        }
-
         LightSource spot(const math::Point3D&, const math::Vector3D&, math::Angle, const imaging::Color&);
         LightSource spot(const math::Point3D&, const math::Point3D&, math::Angle, const imaging::Color&);
     }
