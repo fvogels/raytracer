71fe67ad67e9a2c4b6f8317f213aa7f5a539361f
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-31 23:53:21 +0200

    primitives/plane
    To cpp

--- raytracer/raytracer/primitives/plane.cpp
+++ raytracer/raytracer/primitives/plane.cpp
@@ -5,145 +5,164 @@ using namespace raytracer::primitives;
 using namespace math;
 
 
-bool raytracer::primitives::_private_::PlaneXY::find_hit(const Ray& ray, Hit* hit) const
+namespace
 {
-    assert(hit != nullptr);
-
-    const Vector3D normal = vector(0, 0, 1);
-    double denom = ray.direction.dot(normal);
-
-    if (denom == approx(0.0))
+    class CoordinatePlane : public raytracer::primitives::_private_::PrimitiveImplementation
     {
-        return false;
-    }
-    else
-    {
-        double numer = -((ray.origin - point(0, 0, 0)).dot(normal));
-        double t = numer / denom;
-
-        if (0 < t && t < hit->t)
+    public:
+        std::vector<std::shared_ptr<Hit>> hits(const math::Ray& ray) const override
         {
-            hit->t = t;
-            hit->position = ray.at(hit->t);
-            hit->local_position.xyz = hit->position;
-            hit->local_position.uv = point(hit->position.x(), hit->position.y());
-            hit->normal = ray.origin.z() > 0 ? normal : -normal;
+            auto hit = std::make_shared<Hit>();
 
-            return true;
+            if (find_hit(ray, hit.get()))
+            {
+                return std::vector<std::shared_ptr<Hit>> { hit };
+            }
+            else
+            {
+                return std::vector<std::shared_ptr<Hit>>();
+            }
         }
-        else
+    };
+
+    class PlaneXY : public CoordinatePlane
+    {
+    public:
+        bool find_hit(const Ray& ray, Hit* hit) const override
         {
-            return false;
+            assert(hit != nullptr);
+
+            const Vector3D normal = vector(0, 0, 1);
+            double denom = ray.direction.dot(normal);
+
+            if (denom == approx(0.0))
+            {
+                return false;
+            }
+            else
+            {
+                double numer = -((ray.origin - point(0, 0, 0)).dot(normal));
+                double t = numer / denom;
+
+                if (0 < t && t < hit->t)
+                {
+                    hit->t = t;
+                    hit->position = ray.at(hit->t);
+                    hit->local_position.xyz = hit->position;
+                    hit->local_position.uv = point(hit->position.x(), hit->position.y());
+                    hit->normal = ray.origin.z() > 0 ? normal : -normal;
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
-    }
-}
 
-bool raytracer::primitives::_private_::PlaneXZ::find_hit(const Ray& ray, Hit* hit) const
-{
-    assert(hit != nullptr);
-
-    const Vector3D normal = vector(0, 1, 0);
-    double denom = ray.direction.dot(normal);
-
-    if (denom == approx(0.0))
-    {
-        return false;
-    }
-    else
-    {
-        double numer = -((ray.origin - point(0, 0, 0)).dot(normal));
-        double t = numer / denom;
-
-        if (0 < t && t < hit->t)
+        math::Box bounding_box() const override
         {
-            hit->t = t;
-            hit->position = ray.at(hit->t);
-            hit->local_position.xyz = hit->position;
-            hit->local_position.uv = point(hit->position.x(), hit->position.z());
-            hit->normal = ray.origin.y() > 0 ? normal : -normal;
-
-            return true;
+            return Box(Interval<double>(-1E10, 1E10), Interval<double>(-1E10, 1E10), Interval<double>(-0.01, 0.01));
         }
-        else
+    };
+
+    class PlaneXZ : public CoordinatePlane
+    {
+    public:
+        bool find_hit(const Ray& ray, Hit* hit) const override
         {
-            return false;
+            assert(hit != nullptr);
+
+            const Vector3D normal = vector(0, 1, 0);
+            double denom = ray.direction.dot(normal);
+
+            if (denom == approx(0.0))
+            {
+                return false;
+            }
+            else
+            {
+                double numer = -((ray.origin - point(0, 0, 0)).dot(normal));
+                double t = numer / denom;
+
+                if (0 < t && t < hit->t)
+                {
+                    hit->t = t;
+                    hit->position = ray.at(hit->t);
+                    hit->local_position.xyz = hit->position;
+                    hit->local_position.uv = point(hit->position.x(), hit->position.z());
+                    hit->normal = ray.origin.y() > 0 ? normal : -normal;
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
-    }
-}
 
-bool raytracer::primitives::_private_::PlaneYZ::find_hit(const Ray& ray, Hit* hit) const
-{
-    assert(hit != nullptr);
-
-    const Vector3D normal = vector(1, 0, 0);
-    double denom = ray.direction.dot(normal);
-
-    if (denom == approx(0.0))
-    {
-        return false;
-    }
-    else
-    {
-        double numer = -((ray.origin - point(0, 0, 0)).dot(normal));
-        double t = numer / denom;
-
-        if (0 < t && t < hit->t)
+        math::Box bounding_box() const override
         {
-            hit->t = t;
-            hit->position = ray.at(hit->t);
-            hit->local_position.xyz = hit->position;
-            hit->local_position.uv = point(hit->position.y(), hit->position.z());
-            hit->normal = ray.origin.x() > 0 ? normal : -normal;
-
-            return true;
+            return Box(Interval<double>(-1E10, 1E10), Interval<double>(-0.01, 0.01), Interval<double>(-1E10, 1E10));
         }
-        else
+    };
+
+    class PlaneYZ : public CoordinatePlane
+    {
+    public:
+        bool find_hit(const Ray& ray, Hit* hit) const override
         {
-            return false;
+            assert(hit != nullptr);
+
+            const Vector3D normal = vector(1, 0, 0);
+            double denom = ray.direction.dot(normal);
+
+            if (denom == approx(0.0))
+            {
+                return false;
+            }
+            else
+            {
+                double numer = -((ray.origin - point(0, 0, 0)).dot(normal));
+                double t = numer / denom;
+
+                if (0 < t && t < hit->t)
+                {
+                    hit->t = t;
+                    hit->position = ray.at(hit->t);
+                    hit->local_position.xyz = hit->position;
+                    hit->local_position.uv = point(hit->position.y(), hit->position.z());
+                    hit->normal = ray.origin.x() > 0 ? normal : -normal;
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
-    }
-}
 
-std::vector<std::shared_ptr<Hit>> raytracer::primitives::_private_::CoordinatePlane::hits(const math::Ray& ray) const
-{
-    auto hit = std::make_shared<Hit>();
-
-    if (find_hit(ray, hit.get()))
-    {
-        return std::vector<std::shared_ptr<Hit>> { hit };
-    }
-    else
-    {
-        return std::vector<std::shared_ptr<Hit>>();
-    }
-}
-
-math::Box raytracer::primitives::_private_::PlaneXY::bounding_box() const
-{
-    return Box(Interval<double>(-1E10, 1E10), Interval<double>(-1E10, 1E10), Interval<double>(-0.01, 0.01));
-}
-
-math::Box raytracer::primitives::_private_::PlaneXZ::bounding_box() const
-{
-    return Box(Interval<double>(-1E10, 1E10), Interval<double>(-0.01, 0.01), Interval<double>(-1E10, 1E10));
-}
-
-math::Box raytracer::primitives::_private_::PlaneYZ::bounding_box() const
-{
-    return Box(Interval<double>(-0.01, 0.01), Interval<double>(-1E10, 1E10), Interval<double>(-1E10, 1E10));
+        math::Box bounding_box() const override
+        {
+            return Box(Interval<double>(-0.01, 0.01), Interval<double>(-1E10, 1E10), Interval<double>(-1E10, 1E10));
+        }
+    };
 }
 
 Primitive raytracer::primitives::xy_plane()
 {
-    return Primitive(std::make_shared<raytracer::primitives::_private_::PlaneXY>());
+    return Primitive(std::make_shared<PlaneXY>());
 }
 
 Primitive raytracer::primitives::xz_plane()
 {
-    return Primitive(std::make_shared<raytracer::primitives::_private_::PlaneXZ>());
+    return Primitive(std::make_shared<PlaneXZ>());
 }
 
 Primitive raytracer::primitives::yz_plane()
 {
-    return Primitive(std::make_shared<raytracer::primitives::_private_::PlaneYZ>());
+    return Primitive(std::make_shared<PlaneYZ>());
 }
--- raytracer/raytracer/primitives/plane.h
+++ raytracer/raytracer/primitives/plane.h
@@ -6,36 +6,6 @@ namespace raytracer
 {
     namespace primitives
     {
-        namespace _private_
-        {
-            class CoordinatePlane : public PrimitiveImplementation
-            {
-            public:
-                std::vector<std::shared_ptr<Hit>> hits(const math::Ray&) const override;
-            };
-
-            class PlaneXY : public CoordinatePlane
-            {
-            public:
-                bool find_hit(const math::Ray&, Hit*) const override;
-                math::Box bounding_box() const override;
-            };
-
-            class PlaneXZ : public CoordinatePlane
-            {
-            public:
-                bool find_hit(const math::Ray&, Hit*) const override;
-                math::Box bounding_box() const override;
-            };
-
-            class PlaneYZ : public CoordinatePlane
-            {
-            public:
-                bool find_hit(const math::Ray&, Hit*) const override;
-                math::Box bounding_box() const override;
-            };
-        }
-
         Primitive xy_plane();
         Primitive xz_plane();
         Primitive yz_plane();
