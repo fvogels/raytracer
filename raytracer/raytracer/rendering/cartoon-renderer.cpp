878e53574e6549a442e6f5e1490ba3a55a2ff583
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-31 17:44:45 +0200

    Removed CartoonRenderer declaration from header file

--- raytracer/raytracer/rendering/cartoon-renderer.cpp
+++ raytracer/raytracer/rendering/cartoon-renderer.cpp
@@ -17,111 +17,118 @@ namespace
 
         return sqrt(dx * dx + dy * dy);
     }
-}
 
-
-raytracer::rendering::_private_::CartoonRenderer::CartoonRenderer(
-    unsigned horizontal_resolution,
-    unsigned vertical_resolution,
-    raytracer::Sampler sampler,
-    RayTracer ray_tracer,
-    std::shared_ptr<util::Looper> looper,
-    unsigned shade_count,
-    double stroke_thickness)
-    : RendererImplementation(horizontal_resolution, vertical_resolution, sampler, ray_tracer, looper)
-    , m_shade_count(shade_count)
-    , m_stroke_thickness(stroke_thickness)
-{
-    // NOP
-}
-
-std::shared_ptr<imaging::Bitmap> raytracer::rendering::_private_::CartoonRenderer::render(const Scene& scene) const
-{
-    auto result = std::make_shared<Bitmap>(m_horizontal_resolution, m_vertical_resolution);
-    Bitmap& bitmap = *result;
-    Rectangle2D window(point(0, 0), vector(1, 0), vector(0, 1));
-    Rasterizer window_rasterizer(window, bitmap.width(), bitmap.height());
-    data::Grid<std::vector<std::pair<unsigned, Point2D>>> group_grid(m_horizontal_resolution, m_vertical_resolution);
-
-    for_each_pixel([&](Position pixel_coordinates) {
-        Position bitmap_coordinates(pixel_coordinates.x, bitmap.height() - pixel_coordinates.y - 1);
-        math::Rectangle2D pixel_rectangle = window_rasterizer[pixel_coordinates];
-        imaging::Color c = imaging::colors::black();
-        unsigned sample_count = 0;
-
-        m_sampler->sample(pixel_rectangle, [&](const Point2D& p) {
-            scene.camera->enumerate_rays(p, [&](const Ray& ray) {
-                TraceResult tr = m_ray_tracer->trace(scene, ray);
-                group_grid[pixel_coordinates].push_back(std::make_pair(tr.group_id, p));
-                c += tr.color.quantized(m_shade_count);
-                ++sample_count;
-            });
-        });
-
-        c /= sample_count;
-
-        bitmap[bitmap_coordinates] = c;
-    });
-
-
-    for (unsigned y = 0; y != bitmap.height(); ++y)
+    class CartoonRenderer : public raytracer::rendering::_private_::RendererImplementation
     {
-        for (unsigned x = 0; x != bitmap.width(); ++x)
+    public:
+        CartoonRenderer(
+            unsigned horizontal_resolution,
+            unsigned vertical_resolution,
+            raytracer::Sampler sampler,
+            RayTracer ray_tracer,
+            std::shared_ptr<util::Looper> looper,
+            unsigned shade_count,
+            double stroke_thickness)
+            : RendererImplementation(horizontal_resolution, vertical_resolution, sampler, ray_tracer, looper)
+            , m_shade_count(shade_count)
+            , m_stroke_thickness(stroke_thickness)
         {
-            Position pixel_coordinates(x, y);
-            Position bitmap_coordinates(x, bitmap.height() - y - 1);
-            unsigned border_count = 0;
+            // NOP
+        }
 
-            for (auto& pair : group_grid[pixel_coordinates])
-            {
-                unsigned current_id = pair.first;
-                Point2D current_xy = pair.second;
-                bool is_border = false;
+        std::shared_ptr<imaging::Bitmap> render(const Scene& scene) const
+        {
+            auto result = std::make_shared<Bitmap>(m_horizontal_resolution, m_vertical_resolution);
+            Bitmap& bitmap = *result;
+            Rectangle2D window(point(0, 0), vector(1, 0), vector(0, 1));
+            Rasterizer window_rasterizer(window, bitmap.width(), bitmap.height());
+            data::Grid<std::vector<std::pair<unsigned, Point2D>>> group_grid(m_horizontal_resolution, m_vertical_resolution);
 
-                group_grid.around(pixel_coordinates, unsigned(ceil(m_stroke_thickness * std::max(m_horizontal_resolution, m_vertical_resolution))), [&](const Position& neighbor_pixel_position) {
-                    for (auto& pair : group_grid[neighbor_pixel_position])
-                    {
-                        unsigned neighbor_id = pair.first;
-                        Point2D neighbor_xy = pair.second;
+            for_each_pixel([&](Position pixel_coordinates) {
+                Position bitmap_coordinates(pixel_coordinates.x, bitmap.height() - pixel_coordinates.y - 1);
+                math::Rectangle2D pixel_rectangle = window_rasterizer[pixel_coordinates];
+                imaging::Color c = imaging::colors::black();
+                unsigned sample_count = 0;
 
-                        if (current_id != neighbor_id)
-                        {
-                            double dist = distance(current_xy, neighbor_xy);
-
-                            if (dist < m_stroke_thickness)
-                            {
-                                is_border = true;
-                            }
-                        }
-                    }
+                m_sampler->sample(pixel_rectangle, [&](const Point2D& p) {
+                    scene.camera->enumerate_rays(p, [&](const Ray& ray) {
+                        TraceResult tr = m_ray_tracer->trace(scene, ray);
+                        group_grid[pixel_coordinates].push_back(std::make_pair(tr.group_id, p));
+                        c += tr.color.quantized(m_shade_count);
+                        ++sample_count;
+                    });
                 });
 
-                if (is_border)
-                {
-                    ++border_count;
-                }
-            }
+                c /= sample_count;
 
-            double border_percentage = double(border_count) / group_grid[pixel_coordinates].size();
-                        
-            if (border_percentage > 0)
+                bitmap[bitmap_coordinates] = c;
+            });
+
+
+            for (unsigned y = 0; y != bitmap.height(); ++y)
             {
-                if (border_percentage < 1)
+                for (unsigned x = 0; x != bitmap.width(); ++x)
                 {
-                    bitmap[bitmap_coordinates] *= (1 - border_percentage);
-                }
-                else
-                {
-                    bitmap[bitmap_coordinates] = colors::black();
+                    Position pixel_coordinates(x, y);
+                    Position bitmap_coordinates(x, bitmap.height() - y - 1);
+                    unsigned border_count = 0;
+
+                    for (auto& pair : group_grid[pixel_coordinates])
+                    {
+                        unsigned current_id = pair.first;
+                        Point2D current_xy = pair.second;
+                        bool is_border = false;
+
+                        group_grid.around(pixel_coordinates, unsigned(ceil(m_stroke_thickness * std::max(m_horizontal_resolution, m_vertical_resolution))), [&](const Position& neighbor_pixel_position) {
+                            for (auto& pair : group_grid[neighbor_pixel_position])
+                            {
+                                unsigned neighbor_id = pair.first;
+                                Point2D neighbor_xy = pair.second;
+
+                                if (current_id != neighbor_id)
+                                {
+                                    double dist = distance(current_xy, neighbor_xy);
+
+                                    if (dist < m_stroke_thickness)
+                                    {
+                                        is_border = true;
+                                    }
+                                }
+                            }
+                        });
+
+                        if (is_border)
+                        {
+                            ++border_count;
+                        }
+                    }
+
+                    double border_percentage = double(border_count) / group_grid[pixel_coordinates].size();
+
+                    if (border_percentage > 0)
+                    {
+                        if (border_percentage < 1)
+                        {
+                            bitmap[bitmap_coordinates] *= (1 - border_percentage);
+                        }
+                        else
+                        {
+                            bitmap[bitmap_coordinates] = colors::black();
+                        }
+                    }
                 }
             }
-        }
-    }
 
-    return result;
+            return result;
+        }
+
+    private:
+        unsigned m_shade_count;
+        double m_stroke_thickness;
+    };
 }
 
 Renderer raytracer::rendering::cartoon(unsigned horizontal_resolution, unsigned vertical_resolution, raytracer::Sampler sampler, RayTracer ray_tracer, std::shared_ptr<util::Looper> looper, unsigned shade_count, double stroke_thickness)
 {
-    return Renderer(std::make_shared<_private_::CartoonRenderer>(horizontal_resolution, vertical_resolution, sampler, ray_tracer, looper, shade_count, stroke_thickness));
+    return Renderer(std::make_shared<CartoonRenderer>(horizontal_resolution, vertical_resolution, sampler, ray_tracer, looper, shade_count, stroke_thickness));
 }
--- raytracer/raytracer/rendering/cartoon-renderer.h
+++ raytracer/raytracer/rendering/cartoon-renderer.h
@@ -7,21 +7,6 @@ namespace raytracer
 {
     namespace rendering
     {
-        namespace _private_
-        {
-            class CartoonRenderer : public RendererImplementation
-            {
-            public:
-                CartoonRenderer(unsigned, unsigned, raytracer::Sampler, RayTracer, std::shared_ptr<util::Looper>, unsigned, double);
-
-                std::shared_ptr<imaging::Bitmap> render(const Scene&) const override;
-
-            private:
-                unsigned m_shade_count;
-                double m_stroke_thickness;
-            };
-        }
-
         Renderer cartoon(unsigned, unsigned, raytracer::Sampler, RayTracer, std::shared_ptr<util::Looper>, unsigned, double);
     }
 }
