2776542094a6f4f0904b60b25628cecbe4ee193e
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-31 23:48:51 +0200

    primitives/intersection
    To cpp

--- raytracer/raytracer/primitives/intersection.cpp
+++ raytracer/raytracer/primitives/intersection.cpp
@@ -4,115 +4,125 @@ using namespace raytracer;
 using namespace raytracer::primitives;
 using namespace math;
 
-
-raytracer::primitives::_private_::Intersection::Intersection(Primitive first, Primitive second)
-    : m_first(first), m_second(second)
+namespace
 {
-    assert(first);
-    assert(second);
-}
-
-bool raytracer::primitives::_private_::Intersection::find_hit(const Ray& ray, Hit* hit) const
-{
-    auto first_hits = m_first->hits(ray);
-    auto second_hits = m_second->hits(ray);
-
-    auto i1 = first_hits.begin();
-    auto i2 = second_hits.begin();
-
-    bool inside_first = false;
-    bool inside_second = false;
-
-    while (i1 != first_hits.end() && i2 != second_hits.end())
+    class Intersection : public raytracer::primitives::_private_::PrimitiveImplementation
     {
-        double t1 = (*i1)->t;
-        double t2 = (*i2)->t;
-
-        if (t1 < t2)
+    public:
+        Intersection(Primitive first, Primitive second)
+            : m_first(first), m_second(second)
         {
-            inside_first = !inside_first;
+            assert(first);
+            assert(second);
+        }
 
-            if (inside_second && t1 > 0 )
+        bool find_hit(const Ray& ray, Hit* hit) const override
+        {
+            auto first_hits = m_first->hits(ray);
+            auto second_hits = m_second->hits(ray);
+
+            auto i1 = first_hits.begin();
+            auto i2 = second_hits.begin();
+
+            bool inside_first = false;
+            bool inside_second = false;
+
+            while (i1 != first_hits.end() && i2 != second_hits.end())
             {
-                *hit = **i1;
-                
-                return true;
+                double t1 = (*i1)->t;
+                double t2 = (*i2)->t;
+
+                if (t1 < t2)
+                {
+                    inside_first = !inside_first;
+
+                    if (inside_second && t1 > 0)
+                    {
+                        *hit = **i1;
+
+                        return true;
+                    }
+
+                    ++i1;
+                }
+                else
+                {
+                    inside_second = !inside_second;
+
+                    if (inside_first && t2 > 0)
+                    {
+                        *hit = **i2;
+
+                        return true;
+                    }
+
+                    ++i2;
+                }
             }
 
-            ++i1;
+            return false;
         }
-        else
-        {
-            inside_second = !inside_second;
-            
-            if (inside_first && t2 > 0)
-            {
-                *hit = **i2;
 
-                return true;
+        std::vector<std::shared_ptr<Hit>> hits(const math::Ray& ray) const override
+        {
+            std::vector<std::shared_ptr<Hit>> result;
+
+            auto first_hits = m_first->hits(ray);
+            auto second_hits = m_second->hits(ray);
+
+            auto i1 = first_hits.begin();
+            auto i2 = second_hits.begin();
+
+            bool inside_first = false;
+            bool inside_second = false;
+            bool inside = false;
+
+            while (i1 != first_hits.end() && i2 != second_hits.end())
+            {
+                std::shared_ptr<Hit> current_hit;
+
+                double t1 = (*i1)->t;
+                double t2 = (*i2)->t;
+
+                if (t1 < t2)
+                {
+                    inside_first = !inside_first;
+                    current_hit = *i1;
+
+                    ++i1;
+                }
+                else
+                {
+                    inside_second = !inside_second;
+                    current_hit = *i2;
+
+                    ++i2;
+                }
+
+                bool new_inside = inside_first && inside_second;
+
+                if (inside != new_inside)
+                {
+                    result.push_back(current_hit);
+                    inside = new_inside;
+                }
             }
 
-            ++i2;
+            return result;
         }
-    }
 
-    return false;
-}
-
-std::vector<std::shared_ptr<Hit>> raytracer::primitives::_private_::Intersection::hits(const math::Ray& ray) const
-{
-    std::vector<std::shared_ptr<Hit>> result;
-
-    auto first_hits = m_first->hits(ray);
-    auto second_hits = m_second->hits(ray);
-
-    auto i1 = first_hits.begin();
-    auto i2 = second_hits.begin();
-
-    bool inside_first = false;
-    bool inside_second = false;
-    bool inside = false;
-
-    while (i1 != first_hits.end() && i2 != second_hits.end())
-    {
-        std::shared_ptr<Hit> current_hit;
-
-        double t1 = (*i1)->t;
-        double t2 = (*i2)->t;
-
-        if (t1 < t2)
+        math::Box bounding_box() const override
         {
-            inside_first = !inside_first;
-            current_hit = *i1;
-
-            ++i1;
-        }
-        else
-        {
-            inside_second = !inside_second;
-            current_hit = *i2;
-
-            ++i2;
+            return m_first->bounding_box().intersect(m_second->bounding_box());
         }
 
-        bool new_inside = inside_first && inside_second;
-
-        if (inside != new_inside)
-        {
-            result.push_back(current_hit);
-            inside = new_inside;
-        }
-    }
-
-    return result;
+    private:
+        Primitive m_first;
+        Primitive m_second;
+    };
 }
 
 Primitive raytracer::primitives::intersection(Primitive first, Primitive second)
 {
-    return Primitive(std::make_shared<_private_::Intersection>(first, second));
-}
-
-math::Box raytracer::primitives::_private_::Intersection::bounding_box() const
-{
-    return m_first->bounding_box().intersect(m_second->bounding_box());
+    return Primitive(std::make_shared<Intersection>(first, second));
 }
--- raytracer/raytracer/primitives/intersection.h
+++ raytracer/raytracer/primitives/intersection.h
@@ -8,23 +8,6 @@ namespace raytracer
 {
     namespace primitives
     {
-        namespace _private_
-        {
-            class Intersection : public PrimitiveImplementation
-            {
-            public:
-                Intersection(Primitive, Primitive);
-
-                bool find_hit(const math::Ray&, Hit*) const override;
-                std::vector<std::shared_ptr<Hit>> hits(const math::Ray&) const override;
-                math::Box bounding_box() const override;
-
-            private:
-                Primitive m_first;
-                Primitive m_second;
-            };
-        }
-
         Primitive intersection(Primitive, Primitive);
     }
 }
