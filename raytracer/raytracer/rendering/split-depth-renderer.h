5d1c9096302ecde5dd7b7734dad58c76ecfe6c9b
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-31 17:47:28 +0200

    rendering/split-depth-renderer
    Clean up

--- raytracer/raytracer/rendering/split-depth-renderer.h
+++ raytracer/raytracer/rendering/split-depth-renderer.h
@@ -7,11 +7,6 @@ namespace raytracer
 {
     namespace rendering
     {
-        namespace _private_
-        {
-            
-        }
-
         Renderer split_depth(unsigned, unsigned, raytracer::Sampler, RayTracer, std::shared_ptr<util::Looper>);
     }
 }
