c57f96ab86368e319d6f8f579ccaf298ae6a7e9a
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-31 17:41:12 +0200

    Removed StandardRenderer declaration from header file

--- raytracer/raytracer/rendering/standard-renderer.cpp
+++ raytracer/raytracer/rendering/standard-renderer.cpp
@@ -6,46 +6,56 @@ using namespace raytracer;
 using namespace raytracer::rendering;
 
 
-raytracer::rendering::_private_::StandardRenderer::StandardRenderer(unsigned horizontal_resolution, unsigned vertical_resolution, raytracer::Sampler sampler, RayTracer ray_tracer, std::shared_ptr<util::Looper> looper)
-    : RendererImplementation(horizontal_resolution, vertical_resolution, sampler, ray_tracer, looper)
+namespace
 {
-    // NOP
-}
+    class StandardRenderer : public raytracer::rendering::_private_::RendererImplementation
+    {
+    public:
+        StandardRenderer(unsigned horizontal_resolution, unsigned vertical_resolution, raytracer::Sampler sampler, RayTracer ray_tracer, std::shared_ptr<util::Looper> looper)
+            : RendererImplementation(horizontal_resolution, vertical_resolution, sampler, ray_tracer, looper)
+        {
+            // NOP
+        }
 
-std::shared_ptr<imaging::Bitmap> raytracer::rendering::_private_::StandardRenderer::render(const Scene& scene) const
-{
-    Rectangle2D window(point(0, 0), vector(1, 0), vector(0, 1));
-    Rasterizer window_rasterizer(window, m_horizontal_resolution, m_vertical_resolution);
-    auto result = std::make_shared<Bitmap>(m_horizontal_resolution, m_vertical_resolution);
-    Bitmap& bitmap = *result;
+        std::shared_ptr<imaging::Bitmap> render(const Scene& scene) const
+        {
+            Rectangle2D window(point(0, 0), vector(1, 0), vector(0, 1));
+            Rasterizer window_rasterizer(window, m_horizontal_resolution, m_vertical_resolution);
+            auto result = std::make_shared<Bitmap>(m_horizontal_resolution, m_vertical_resolution);
+            Bitmap& bitmap = *result;
 
-    for_each_pixel([&](Position pixel_coordinates) {
-        Position bitmap_coordinates(pixel_coordinates.x, bitmap.height() - pixel_coordinates.y - 1);
-        Color c = render_pixel(window_rasterizer, pixel_coordinates, scene);
+            for_each_pixel([&](Position pixel_coordinates) {
+                Position bitmap_coordinates(pixel_coordinates.x, bitmap.height() - pixel_coordinates.y - 1);
+                Color c = render_pixel(window_rasterizer, pixel_coordinates, scene);
 
-        bitmap[bitmap_coordinates] = c;
-    });
+                bitmap[bitmap_coordinates] = c;
+            });
 
-    return result;
-}
+            return result;
+        }
 
-imaging::Color raytracer::rendering::_private_::StandardRenderer::render_pixel(const math::Rasterizer& window_rasterizer, const Position& position, const Scene& scene) const
-{
-    math::Rectangle2D pixel_rectangle = window_rasterizer[position];
-    imaging::Color c = imaging::colors::black();
-    int sample_count = 0;
+    private:
+        Color render_pixel(const math::Rasterizer& window_rasterizer, const Position& position, const Scene& scene) const
+        {
+            math::Rectangle2D pixel_rectangle = window_rasterizer[position];
+            imaging::Color c = imaging::colors::black();
+            int sample_count = 0;
 
-    m_sampler->sample(pixel_rectangle, [this, &c, &sample_count, &scene](const Point2D& p) {
-        scene.camera->enumerate_rays(p, [this, &c, &sample_count, &scene](const Ray& ray) {
-            c += m_ray_tracer->trace(scene, ray).color;
-            ++sample_count;
-        });
-    });
+            m_sampler->sample(pixel_rectangle, [this, &c, &sample_count, &scene](const Point2D& p) {
+                scene.camera->enumerate_rays(p, [this, &c, &sample_count, &scene](const Ray& ray) {
+                    c += m_ray_tracer->trace(scene, ray).color;
+                    ++sample_count;
+                });
+            });
 
-    return c / sample_count;
+            return c / sample_count;
+        }
+
+        unsigned m_thread_count;
+    };
 }
 
 Renderer raytracer::rendering::standard(unsigned horizontal_resolution, unsigned vertical_resolution, raytracer::Sampler sampler, RayTracer ray_tracer, std::shared_ptr<util::Looper> looper)
 {
-    return Renderer(std::make_shared<rendering::_private_::StandardRenderer>(horizontal_resolution, vertical_resolution, sampler, ray_tracer, looper));
+    return Renderer(std::make_shared<StandardRenderer>(horizontal_resolution, vertical_resolution, sampler, ray_tracer, looper));
 }
--- raytracer/raytracer/rendering/standard-renderer.h
+++ raytracer/raytracer/rendering/standard-renderer.h
@@ -9,22 +9,6 @@ namespace raytracer
 {
     namespace rendering
     {
-        namespace _private_
-        {
-            class StandardRenderer : public RendererImplementation
-            {
-            public:
-                StandardRenderer(unsigned, unsigned, raytracer::Sampler, RayTracer, std::shared_ptr<util::Looper>);
-
-                std::shared_ptr<imaging::Bitmap> render(const Scene&) const override;
-
-            private:
-                imaging::Color render_pixel(const math::Rasterizer&, const Position&, const Scene&) const;
-
-                unsigned m_thread_count;
-            };
-        }
-
         Renderer standard(unsigned, unsigned, raytracer::Sampler, RayTracer, std::shared_ptr<util::Looper>);
     }
 }
