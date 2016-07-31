8374f9efe4c05d8547f4d5399018611a857e7854
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-31 17:46:45 +0200

    Removed EdgeRenderer declaration from header file

--- raytracer/raytracer/rendering/edge-renderer.cpp
+++ raytracer/raytracer/rendering/edge-renderer.cpp
@@ -19,105 +19,111 @@ namespace
 
         return sqrt(dx * dx + dy * dy);
     }
-}
 
-
-raytracer::rendering::_private_::EdgeRenderer::EdgeRenderer(
-    unsigned horizontal_resolution,
-    unsigned vertical_resolution,
-    raytracer::Sampler sampler,
-    RayTracer ray_tracer,
-    std::shared_ptr<util::Looper> looper,
-    double stroke_thickness)
-    : RendererImplementation(horizontal_resolution, vertical_resolution, sampler, ray_tracer, looper)
-    , m_stroke_thickness(stroke_thickness)
-{
-    // NOP
-}
-
-std::shared_ptr<imaging::Bitmap> raytracer::rendering::_private_::EdgeRenderer::render(const Scene& scene) const
-{
-    Rectangle2D window(point(0, 0), vector(1, 0), vector(0, 1));
-    Rasterizer window_rasterizer(window, m_horizontal_resolution, m_vertical_resolution);
-    data::Grid<std::vector<std::pair<unsigned, Point2D>>> group_grid(m_horizontal_resolution, m_vertical_resolution);
-
-    for_each_pixel([&](Position pixel_coordinates) {
-        assert(pixel_coordinates.x < m_horizontal_resolution);
-        assert(pixel_coordinates.y < m_vertical_resolution);
-
-        math::Rectangle2D pixel_rectangle = window_rasterizer[pixel_coordinates];
-        std::vector<std::pair<unsigned, Point2D>> data;
-
-        m_sampler->sample(pixel_rectangle, [&](const Point2D& p) {
-            scene.camera->enumerate_rays(p, [&](const Ray& ray) {
-                TraceResult tr = m_ray_tracer->trace(scene, ray);
-
-                data.push_back(std::make_pair(tr.group_id, p));
-            });
-        });
-
-        group_grid[pixel_coordinates] = data;
-    });
-
-    auto result = std::make_shared<Bitmap>(m_horizontal_resolution, m_vertical_resolution);
-    Bitmap& bitmap = *result;
-    bitmap.clear(colors::white());
-
-    LOG(DEBUG) << "Finding edges";
-
-    for_each_pixel([&](Position pixel_coordinates) {
-        Position bitmap_coordinates(pixel_coordinates.x, bitmap.height() - pixel_coordinates.y - 1);
-        unsigned border_count = 0;
-
-        for (auto& pair : group_grid[pixel_coordinates])
+    class EdgeRenderer : public raytracer::rendering::_private_::RendererImplementation
+    {
+    public:
+        EdgeRenderer(
+            unsigned horizontal_resolution,
+            unsigned vertical_resolution,
+            raytracer::Sampler sampler,
+            RayTracer ray_tracer,
+            std::shared_ptr<util::Looper> looper,
+            double stroke_thickness)
+            : RendererImplementation(horizontal_resolution, vertical_resolution, sampler, ray_tracer, looper)
+            , m_stroke_thickness(stroke_thickness)
         {
-            unsigned current_id = pair.first;
-            Point2D current_xy = pair.second;
-            bool is_border = false;
+            // NOP
+        }
 
-            group_grid.around(pixel_coordinates, unsigned(ceil(m_stroke_thickness * std::max(m_horizontal_resolution, m_vertical_resolution))), [&](const Position& neighbor_pixel_coordinates) {
-                for (auto& pair : group_grid[neighbor_pixel_coordinates])
+        std::shared_ptr<imaging::Bitmap> render(const Scene& scene) const
+        {
+            Rectangle2D window(point(0, 0), vector(1, 0), vector(0, 1));
+            Rasterizer window_rasterizer(window, m_horizontal_resolution, m_vertical_resolution);
+            data::Grid<std::vector<std::pair<unsigned, Point2D>>> group_grid(m_horizontal_resolution, m_vertical_resolution);
+
+            for_each_pixel([&](Position pixel_coordinates) {
+                assert(pixel_coordinates.x < m_horizontal_resolution);
+                assert(pixel_coordinates.y < m_vertical_resolution);
+
+                math::Rectangle2D pixel_rectangle = window_rasterizer[pixel_coordinates];
+                std::vector<std::pair<unsigned, Point2D>> data;
+
+                m_sampler->sample(pixel_rectangle, [&](const Point2D& p) {
+                    scene.camera->enumerate_rays(p, [&](const Ray& ray) {
+                        TraceResult tr = m_ray_tracer->trace(scene, ray);
+
+                        data.push_back(std::make_pair(tr.group_id, p));
+                    });
+                });
+
+                group_grid[pixel_coordinates] = data;
+            });
+
+            auto result = std::make_shared<Bitmap>(m_horizontal_resolution, m_vertical_resolution);
+            Bitmap& bitmap = *result;
+            bitmap.clear(colors::white());
+
+            LOG(DEBUG) << "Finding edges";
+
+            for_each_pixel([&](Position pixel_coordinates) {
+                Position bitmap_coordinates(pixel_coordinates.x, bitmap.height() - pixel_coordinates.y - 1);
+                unsigned border_count = 0;
+
+                for (auto& pair : group_grid[pixel_coordinates])
                 {
-                    unsigned neighbor_id = pair.first;
-                    Point2D neighbor_xy = pair.second;
+                    unsigned current_id = pair.first;
+                    Point2D current_xy = pair.second;
+                    bool is_border = false;
 
-                    if (current_id != neighbor_id)
-                    {
-                        double dist = distance(current_xy, neighbor_xy);
-
-                        if (dist < m_stroke_thickness)
+                    group_grid.around(pixel_coordinates, unsigned(ceil(m_stroke_thickness * std::max(m_horizontal_resolution, m_vertical_resolution))), [&](const Position& neighbor_pixel_coordinates) {
+                        for (auto& pair : group_grid[neighbor_pixel_coordinates])
                         {
-                            is_border = true;
+                            unsigned neighbor_id = pair.first;
+                            Point2D neighbor_xy = pair.second;
+
+                            if (current_id != neighbor_id)
+                            {
+                                double dist = distance(current_xy, neighbor_xy);
+
+                                if (dist < m_stroke_thickness)
+                                {
+                                    is_border = true;
+                                }
+                            }
                         }
+                    });
+
+                    if (is_border)
+                    {
+                        ++border_count;
+                    }
+                }
+
+                double border_percentage = double(border_count) / group_grid[pixel_coordinates].size();
+
+                if (border_percentage > 0)
+                {
+                    if (border_percentage < 1)
+                    {
+                        bitmap[bitmap_coordinates] = colors::white() * (1 - border_percentage);
+                    }
+                    else
+                    {
+                        bitmap[bitmap_coordinates] = colors::black();
                     }
                 }
             });
 
-            if (is_border)
-            {
-                ++border_count;
-            }
+            return result;
         }
 
-        double border_percentage = double(border_count) / group_grid[pixel_coordinates].size();
-
-        if (border_percentage > 0)
-        {
-            if (border_percentage < 1)
-            {
-                bitmap[bitmap_coordinates] = colors::white() * (1 - border_percentage);
-            }
-            else
-            {
-                bitmap[bitmap_coordinates] = colors::black();
-            }
-        }
-    });
-
-    return result;
+    private:
+        double m_stroke_thickness;
+    };
 }
 
 Renderer raytracer::rendering::edge(unsigned horizontal_resolution, unsigned vertical_resolution, raytracer::Sampler sampler, RayTracer ray_tracer, std::shared_ptr<util::Looper> looper, double stroke_thickness)
 {
-    return Renderer(std::make_shared<_private_::EdgeRenderer>(horizontal_resolution, vertical_resolution, sampler, ray_tracer, looper, stroke_thickness));
+    return Renderer(std::make_shared<EdgeRenderer>(horizontal_resolution, vertical_resolution, sampler, ray_tracer, looper, stroke_thickness));
 }
--- raytracer/raytracer/rendering/edge-renderer.h
+++ raytracer/raytracer/rendering/edge-renderer.h
@@ -7,20 +7,6 @@ namespace raytracer
 {
     namespace rendering
     {
-        namespace _private_
-        {
-            class EdgeRenderer : public RendererImplementation
-            {
-            public:
-                EdgeRenderer(unsigned, unsigned, raytracer::Sampler, RayTracer, std::shared_ptr<util::Looper>, double);
-
-                std::shared_ptr<imaging::Bitmap> render(const Scene&) const override;
-
-            private:
-                double m_stroke_thickness;
-            };
-        }
-
         Renderer edge(unsigned, unsigned, raytracer::Sampler, RayTracer, std::shared_ptr<util::Looper>, double);
     }
 }
