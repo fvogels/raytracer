cdd07fece269d972ddabb933eef114bbccb12aad
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-31 22:22:24 +0200

    lights/area-light
    Moved class declaration to cpp

--- raytracer/raytracer/lights/area-light.cpp
+++ raytracer/raytracer/lights/area-light.cpp
@@ -6,34 +6,46 @@ using namespace raytracer;
 using namespace imaging;
 
 
-raytracer::lights::_private_::AreaLight::AreaLight(const math::Rectangle3D& area, Sampler sampler, const imaging::Color& color)
-    : m_area(area), m_sampler(sampler), m_color(color)
+namespace
 {
-    assert(sampler);
-}
-
-std::vector<LightRay> raytracer::lights::_private_::AreaLight::lightrays_to(const math::Point3D& illuminated_point) const
-{
-    std::vector<LightRay> light_rays;
-    unsigned count = 0;
-
-    Rectangle2D rect_2d(point(0, 0), vector(1, 0), vector(0, 1));
-    m_sampler->sample(rect_2d, [this, &count, &illuminated_point, &light_rays](const Point2D& p2d) {
-        Point3D p3d = m_area.project(p2d);
-        Ray ray(p3d, illuminated_point);
-        light_rays.push_back(LightRay(ray, m_color));
-        ++count;
-    });
-
-    for (auto& light_ray : light_rays)
+    class AreaLight : public raytracer::lights::_private_::LightSourceImplementation
     {
-        light_ray.color /= count;
-    }
+    public:
+        AreaLight(const math::Rectangle3D& area, Sampler sampler, const imaging::Color& color)
+            : m_area(area), m_sampler(sampler), m_color(color)
+        {
+            assert(sampler);
+        }
 
-    return light_rays;
+        std::vector<LightRay> lightrays_to(const math::Point3D& illuminated_point) const override
+        {
+            std::vector<LightRay> light_rays;
+            unsigned count = 0;
+
+            Rectangle2D rect_2d(point(0, 0), vector(1, 0), vector(0, 1));
+            m_sampler->sample(rect_2d, [this, &count, &illuminated_point, &light_rays](const Point2D& p2d) {
+                Point3D p3d = m_area.project(p2d);
+                Ray ray(p3d, illuminated_point);
+                light_rays.push_back(LightRay(ray, m_color));
+                ++count;
+            });
+
+            for (auto& light_ray : light_rays)
+            {
+                light_ray.color /= count;
+            }
+
+            return light_rays;
+        }
+
+    private:
+        math::Rectangle3D m_area;
+        raytracer::Sampler m_sampler;
+        imaging::Color m_color;
+    };
 }
 
 LightSource raytracer::lights::area(const math::Rectangle3D& area, raytracer::Sampler sampler, const imaging::Color& color)
 {
-    return LightSource(std::make_shared<raytracer::lights::_private_::AreaLight>(area, sampler, color));
+    return LightSource(std::make_shared<AreaLight>(area, sampler, color));
 }
--- raytracer/raytracer/lights/area-light.h
+++ raytracer/raytracer/lights/area-light.h
@@ -12,18 +12,7 @@ namespace raytracer
     {
         namespace _private_
         {
-            class AreaLight : public LightSourceImplementation
-            {
-            public:
-                AreaLight(const math::Rectangle3D&, raytracer::Sampler sampler, const imaging::Color&);
-
-                std::vector<LightRay> lightrays_to(const math::Point3D&) const override;
-
-            private:
-                math::Rectangle3D m_area;
-                raytracer::Sampler m_sampler;
-                imaging::Color m_color;
-            };
+            
         }
 
         LightSource area(const math::Rectangle3D&, raytracer::Sampler, const imaging::Color&);
