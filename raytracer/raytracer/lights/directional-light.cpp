28ac17c7b6ef03c23d972150d166ddfe1e1c0a55
Frederic Vogels <frederic.vogels@gmail.com>
2016-07-31 22:24:15 +0200

    lights/directional-lights
    Moved class declaration to cpp

--- raytracer/raytracer/lights/directional-light.cpp
+++ raytracer/raytracer/lights/directional-light.cpp
@@ -6,21 +6,33 @@ using namespace raytracer;
 using namespace imaging;
 
 
-raytracer::lights::_private_::DirectionalLight::DirectionalLight(const math::Vector3D& direction, const imaging::Color& color)
-    : m_direction(direction), m_color(color)
+namespace
 {
-    assert(direction.is_unit());
+    class DirectionalLight : public raytracer::lights::_private_::LightSourceImplementation
+    {
+    public:
+        DirectionalLight(const math::Vector3D& direction, const imaging::Color& color)
+            : m_direction(direction), m_color(color)
+        {
+            assert(direction.is_unit());
+        }
+
+        std::vector<LightRay> lightrays_to(const math::Point3D& p) const override
+        {
+            Ray ray(p - m_direction * 1000, m_direction * 1000);
+            LightRay light_ray(ray, m_color);
+
+            return std::vector<LightRay> { light_ray };
+        }
+
+    private:
+        math::Vector3D m_direction;
+        imaging::Color m_color;
+    };
 }
 
-std::vector<LightRay> raytracer::lights::_private_::DirectionalLight::lightrays_to(const math::Point3D& p) const
-{
-    Ray ray(p - m_direction * 1000, m_direction * 1000);
-    LightRay light_ray(ray, m_color);
-
-    return std::vector<LightRay> { light_ray };
-}
 
 LightSource raytracer::lights::directional(const math::Vector3D& direction, const imaging::Color& color)
 {
-    return LightSource(std::make_shared<raytracer::lights::_private_::DirectionalLight>(direction, color));
+    return LightSource(std::make_shared<DirectionalLight>(direction, color));
 }
--- raytracer/raytracer/lights/directional-light.h
+++ raytracer/raytracer/lights/directional-light.h
@@ -10,17 +10,7 @@ namespace raytracer
     {
         namespace _private_
         {
-            class DirectionalLight : public LightSourceImplementation
-            {
-            public:
-                DirectionalLight(const math::Vector3D&, const imaging::Color&);
-
-                std::vector<LightRay> lightrays_to(const math::Point3D&) const override;
-
-            private:
-                math::Vector3D m_direction;
-                imaging::Color m_color;
-            };
+            
         }
 
         LightSource directional(const math::Vector3D&, const imaging::Color&);
