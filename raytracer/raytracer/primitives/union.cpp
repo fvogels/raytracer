6f2528044144d8c6435158fc9c001d8ad27d8e2c
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-01 08:35:07 +0200

    primitives/union
    To cpp

--- raytracer/raytracer/primitives/union.cpp
+++ raytracer/raytracer/primitives/union.cpp
@@ -5,59 +5,68 @@ using namespace raytracer;
 using namespace raytracer::primitives;
 using namespace math;
 
-
-raytracer::primitives::_private_::Union::Union(std::vector<Primitive>& children)
-    : children(children)
+namespace
 {
-    // NOP
-}
-
-bool raytracer::primitives::_private_::Union::find_hit(const Ray& ray, Hit* hit) const
-{
-    bool found_hit = false;
-
-    for (const auto& child : this->children)
+    class Union : public raytracer::primitives::_private_::PrimitiveImplementation
     {
-        found_hit = child->find_hit(ray, hit) || found_hit;
-    }
-
-    return found_hit;
-}
-
-std::vector<std::shared_ptr<Hit>> raytracer::primitives::_private_::Union::hits(const math::Ray& ray) const
-{
-    std::vector<std::shared_ptr<Hit>> hits;
-
-    for (const auto& child : this->children)
-    {
-        for (auto hit : child->hits(ray))
+    public:
+        Union(std::vector<Primitive>& children)
+            : children(children)
         {
-            hits.push_back(hit);
+            // NOP
         }
-    }
 
-    std::sort(hits.begin(), hits.end(), [](const std::shared_ptr<Hit>& h1, const std::shared_ptr<Hit>& h2)
-    {
-        return h1->t < h2->t;
-    });
+        bool find_hit(const Ray& ray, Hit* hit) const override
+        {
+            bool found_hit = false;
 
-    return hits;
-}
+            for (const auto& child : this->children)
+            {
+                found_hit = child->find_hit(ray, hit) || found_hit;
+            }
 
-math::Box raytracer::primitives::_private_::Union::bounding_box() const
-{
-    Box result = Box::empty();
+            return found_hit;
+        }
 
-    for (auto child : this->children)
-    {
-        Box child_box = child->bounding_box();
-        result = result.merge(child_box);
-    }
+        std::vector<std::shared_ptr<Hit>> hits(const math::Ray& ray) const override
+        {
+            std::vector<std::shared_ptr<Hit>> hits;
 
-    return result;
+            for (const auto& child : this->children)
+            {
+                for (auto hit : child->hits(ray))
+                {
+                    hits.push_back(hit);
+                }
+            }
+
+            std::sort(hits.begin(), hits.end(), [](const std::shared_ptr<Hit>& h1, const std::shared_ptr<Hit>& h2)
+            {
+                return h1->t < h2->t;
+            });
+
+            return hits;
+        }
+
+        math::Box bounding_box() const override
+        {
+            Box result = Box::empty();
+
+            for (auto child : this->children)
+            {
+                Box child_box = child->bounding_box();
+                result = result.merge(child_box);
+            }
+
+            return result;
+        }
+
+    private:
+        std::vector<Primitive> children;
+    };
 }
 
 Primitive raytracer::primitives::make_union(std::vector<Primitive>& children)
 {
-    return Primitive(std::make_shared<_private_::Union>(children));
+    return Primitive(std::make_shared<Union>(children));
 }
--- raytracer/raytracer/primitives/union.h
+++ raytracer/raytracer/primitives/union.h
@@ -10,18 +10,7 @@ namespace raytracer
     {
         namespace _private_
         {
-            class Union : public PrimitiveImplementation
-            {
-            public:
-                Union(std::vector<Primitive>&);
-
-                bool find_hit(const math::Ray&, Hit*) const override;
-                std::vector<std::shared_ptr<Hit>> hits(const math::Ray&) const override;
-                math::Box bounding_box() const override;
-
-            private:
-                std::vector<Primitive> children;
-            };
+            
         }
 
         Primitive make_union(std::vector<Primitive>&);
