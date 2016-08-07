154b6689afaae371e8a5451bbabd7f028afc355a
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-07 20:14:32 +0200

    scripting/primitives-module
    Moved bindings to top level namespace

--- raytracer/raytracer/scripting/primitives-module.cpp
+++ raytracer/raytracer/scripting/primitives-module.cpp
@@ -8,74 +8,74 @@ using namespace math;
 
 namespace
 {
-    struct PrimitiveLibrary
+    Primitive sphere()
     {
-        Primitive sphere() const
-        {
-            return primitives::sphere(); 
-        }
+        return primitives::sphere(); 
+    }
 
-        Primitive xy_plane() const
-        {
-            return primitives::xy_plane();
-        }
+    Primitive xy_plane()
+    {
+        return primitives::xy_plane();
+    }
 
-        Primitive xz_plane() const
-        {
-            return primitives::xz_plane();
-        }
+    Primitive xz_plane()
+    {
+        return primitives::xz_plane();
+    }
 
-        Primitive yz_plane() const
-        {
-            return primitives::yz_plane();
-        }
+    Primitive yz_plane()
+    {
+        return primitives::yz_plane();
+    }
 
-        Primitive translate(const Vector3D& displacement, Primitive primitive) const 
-        {
-            return primitives::translate(displacement, primitive); 
-        }
+    Primitive translate(const Vector3D& displacement, Primitive primitive) 
+    {
+        return primitives::translate(displacement, primitive); 
+    }
 
-        Primitive make_union(std::vector<Primitive>& children) const
-        {
-            return primitives::make_union(children);
-        }
+    Primitive make_union(const std::vector<chaiscript::Boxed_Value>& boxed_children)
+    {
+        std::vector<Primitive> children(boxed_children.size());
 
-        Primitive decorate(Material material, Primitive decorated) const
-        {
-            return primitives::decorate(material, decorated);
-        }
+        std::transform(boxed_children.begin(), boxed_children.end(), children.begin(), [](chaiscript::Boxed_Value boxed) {
+            return chaiscript::boxed_cast<Primitive>(boxed);
+        });
 
-        Primitive mesh(const std::string& path) const
-        {
-            return primitives::fast_mesh(path);
-        }
+        return primitives::make_union(children);
+    }
 
-        Primitive center(const Point3D& center, Primitive primitive) const
-        {
-            return primitives::center(center, primitive);
-        }
+    Primitive decorate(Material material, Primitive decorated)
+    {
+        return primitives::decorate(material, decorated);
+    }
 
-        Primitive group(unsigned group_id, Primitive primitive) const
-        {
-            return primitives::group(group_id, primitive);
-        }
+    Primitive mesh(const std::string& path)
+    {
+        return primitives::fast_mesh(path);
+    }
 
-        Primitive intersection(Primitive child1, Primitive child2) const
-        {
-            return primitives::intersection(child1, child2);
-        }
-    };
+    Primitive center(const Point3D& center, Primitive primitive)
+    {
+        return primitives::center(center, primitive);
+    }
+
+    Primitive group(unsigned group_id, Primitive primitive)
+    {
+        return primitives::group(group_id, primitive);
+    }
+
+    Primitive intersection(Primitive child1, Primitive child2)
+    {
+        return primitives::intersection(child1, child2);
+    }
 }
 
 ModulePtr raytracer::scripting::_private_::create_primitives_module()
 {
     auto module = std::make_shared<chaiscript::Module>();
 
-    auto primitive_library = std::make_shared<PrimitiveLibrary>();
-    module->add_global_const(chaiscript::const_var(primitive_library), "Primitives");
-
-#define PRIMITIVE(NAME) module->add(fun(&PrimitiveLibrary::NAME), #NAME)
-#define PRIMITIVE_WITH_NAME(FACTORY, NAME) module->add(fun(&PrimitiveLibrary::FACTORY), #NAME)
+#define PRIMITIVE(NAME) module->add(fun(&NAME), #NAME)
+#define PRIMITIVE_WITH_NAME(FACTORY, NAME) module->add(fun(&FACTORY), #NAME)
     PRIMITIVE(sphere);
     PRIMITIVE(xy_plane);
     PRIMITIVE(xz_plane);
