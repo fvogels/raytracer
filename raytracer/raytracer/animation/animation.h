e22a261bf4b020ddfe30efbd03746ab6d9010456
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-08 23:34:58 +0200

    animation/animation
    interval

--- raytracer/raytracer/animation/animation.h
+++ raytracer/raytracer/animation/animation.h
@@ -90,4 +90,22 @@ namespace animation
     Animation<math::Point3D> straight(const math::Point3D&, const math::Point3D&, const Duration&);
     Animation<math::Vector3D> straight(const math::Vector3D&, const math::Vector3D&, const Duration&);
     Animation<math::Point3D> circular(const math::Point3D&, const math::Point3D&, const math::Vector3D&, const math::Interval<math::Angle>&, const Duration&);
+
+    template<typename T>
+    Animation<T> interval(const math::Interval<T> iv, animation::Duration duration)
+    {
+        auto double_animation = straight(0, 1, duration);
+
+        std::function<T(TimeStamp)> lambda = [double_animation, iv](TimeStamp now) {
+            return iv.from_relative(double_animation(now));
+        };
+
+        return make_animation(math::from_lambda(lambda), duration);
+    }
+
+    template<typename T>
+    Animation<T> interval(T from, T to, animation::Duration duration)
+    {
+        return interval(math::Interval<T>(from, to), duration);
+    }
 }
