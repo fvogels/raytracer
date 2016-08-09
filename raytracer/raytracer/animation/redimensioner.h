efd5efa9a62a8c5ff66e5f62cf24214d604baef5
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-09 17:52:12 +0200

    animation/redimensioner

--- raytracer/raytracer/animation/redimensioner.h
+++ raytracer/raytracer/animation/redimensioner.h
@@ -7,32 +7,62 @@
 namespace animation
 {
     template<typename T>
-    math::Function<T(const math::Point2D&)> xyz_to_tyz(math::Function<T(const math::Point3D&)> function, TimeStamp now)
+    animation::Animation<math::Function<T(const math::Point2D&)>> xyz_to_xyt(math::Function<T(const math::Point3D&)> function, Duration duration)
     {
-        std::function<T(const Point2D&)> lambda = [=](const Point2D& p) -> T {
-            return function(Point3D(now.seconds(), p.x(), p.y()));
+        using result_type = math::Function<T(const math::Point2D&)>;
+
+        std::function<result_type(TimeStamp)> lambda = [function](TimeStamp now) -> result_type
+        {
+            std::function<T(const Point2D&)> lambda2 = [now, function](const Point2D& p) -> T
+            {
+                return function(Point3D(p.x(), p.y(), now.seconds()));
+            };
+
+            result_type function = from_lambda(lambda2);
+
+            return function;
         };
 
-        return from_lambda(lambda);
+        return make_animation<result_type>(from_lambda(lambda), duration);
     }
 
     template<typename T>
-    math::Function<T(const math::Point2D&)> xyz_to_xtz(math::Function<T(const math::Point3D&)> function, TimeStamp now)
+    animation::Animation<math::Function<T(const math::Point2D&)>> xyz_to_tyz(math::Function<T(const math::Point3D&)> function, Duration duration)
     {
-        std::function<T(const Point2D&)> lambda = [=](const Point2D& p) -> T {
-            return function(Point3D(p.x(), now.seconds(), p.y()));
+        using result_type = math::Function<T(const math::Point2D&)>;
+
+        std::function<result_type(TimeStamp)> lambda = [function](TimeStamp now) -> result_type
+        {
+            std::function<T(const Point2D&)> lambda2 = [now, function](const Point2D& p) -> T
+            {
+                return function(Point3D(now.seconds(), p.x(), p.y()));
+            };
+
+            result_type function = from_lambda(lambda2);
+
+            return function;
         };
 
-        return from_lambda(lambda);
+        return make_animation<result_type>(from_lambda(lambda), duration);
     }
 
     template<typename T>
-    math::Function<T(const math::Point2D&)> xyz_to_xyt(math::Function<T(const math::Point3D&)> function, TimeStamp now)
+    animation::Animation<math::Function<T(const math::Point2D&)>> xyz_to_xtz(math::Function<T(const math::Point3D&)> function, Duration duration)
     {
-        std::function<T(const Point2D&)> lambda = [=](const Point2D& p) -> T {
-            return function(Point3D(p.x(), p.y(), now.seconds()));
+        using result_type = math::Function<T(const math::Point2D&)>;
+
+        std::function<result_type(TimeStamp)> lambda = [function](TimeStamp now) -> result_type
+        {
+            std::function<T(const Point2D&)> lambda2 = [now, function](const Point2D& p) -> T
+            {
+                return function(Point3D(p.x(), now.seconds(), p.y()));
+            };
+
+            result_type function = from_lambda(lambda2);
+
+            return function;
         };
 
-        return from_lambda(lambda);
+        return make_animation<result_type>(from_lambda(lambda), duration);
     }
 }
