acb192922b213bc061d3bfa54ea1b711cf9551b5
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-09 16:43:36 +0200

    animation/redimensioner

--- raytracer/raytracer/animation/redimensioner.h
+++ raytracer/raytracer/animation/redimensioner.h
@@ -10,7 +10,7 @@ namespace animation
     math::Function<T(const math::Point2D&)> xyz_to_tyz(math::Function<T(const math::Point3D&)> function, TimeStamp now)
     {
         std::function<T(const Point2D&)> lambda = [=](const Point2D& p) -> T {
-            return function(now.seconds(), p.x(), p.y());
+            return function(Point3D(now.seconds(), p.x(), p.y()));
         };
 
         return from_lambda(lambda);
@@ -20,7 +20,7 @@ namespace animation
     math::Function<T(const math::Point2D&)> xyz_to_xtz(math::Function<T(const math::Point3D&)> function, TimeStamp now)
     {
         std::function<T(const Point2D&)> lambda = [=](const Point2D& p) -> T {
-            return function(p.x(), now.seconds(), p.y());
+            return function(Point3D(p.x(), now.seconds(), p.y()));
         };
 
         return from_lambda(lambda);
@@ -30,19 +30,7 @@ namespace animation
     math::Function<T(const math::Point2D&)> xyz_to_xyt(math::Function<T(const math::Point3D&)> function, TimeStamp now)
     {
         std::function<T(const Point2D&)> lambda = [=](const Point2D& p) -> T {
-            return function(p.x(), p.y(), now.seconds());
-        };
-
-        return from_lambda(lambda);
-    }
-
-    template<typename T>
-    math::Function<T(const math::Point3D&)> xyz_to_xyzt(math::Function<T(const math::Point3D&)> function, TimeStamp now)
-    {
-        std::function<T(const Point3D&)> lambda = [=](const Point3D& p) -> T {
-            double delta = now.seconds() + (p.x() + p.y() + p.z()) / 3;
-
-            return function(Point3D(p.x() + delta, p.y() + delta, p.z() + delta));
+            return function(Point3D(p.x(), p.y(), now.seconds()));
         };
 
         return from_lambda(lambda);
