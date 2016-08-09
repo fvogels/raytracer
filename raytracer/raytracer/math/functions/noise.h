55c8af960006908bd85a10e51543761fdf17f882
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-09 16:26:55 +0200

    math/functions/noise
    Noise1D

--- raytracer/raytracer/math/functions/noise.h
+++ raytracer/raytracer/math/functions/noise.h
@@ -9,6 +9,7 @@ namespace math
 {
     namespace functions
     {
+        typedef math::Function<double(double)> Noise1D;
         typedef math::Function<double(const Point2D&)> Noise2D;
         typedef math::Function<double(const Point3D&)> Noise3D;
     }
