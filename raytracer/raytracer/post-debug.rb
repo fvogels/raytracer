0cc9c0721cd3eb56be13787feddf29b2aeba026e
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-10 12:19:29 +0200

    post-debug

---
+++ raytracer/raytracer/post-debug.rb
@@ -0,0 +1,7 @@
+require 'fileutils'
+
+exe_path = ARGV[0]
+output_path = "E:/repos/ucll/shared/scripts/raytracer-debug.exe"
+puts "Post Debug Build Script"
+puts "Copying #{exe_path} to #{output_path}"
+FileUtils.cp(exe_path, output_path)
