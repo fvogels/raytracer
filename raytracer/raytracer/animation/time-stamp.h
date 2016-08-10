cb50c5b63ba41ad650bccc8d36045d37f408d5f8
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-10 14:33:25 +0200

    animation/time-stamp
    Added literals _S and _MS

--- raytracer/raytracer/animation/time-stamp.h
+++ raytracer/raytracer/animation/time-stamp.h
@@ -29,6 +29,26 @@ namespace animation
         Duration m_since_epoch;
     };
 
+    inline TimeStamp operator""_S(long double x) noexcept
+    {
+        return TimeStamp::from_epoch(Duration::from_seconds(double(x)));
+    }
+
+    inline TimeStamp operator""_S(long long unsigned x) noexcept
+    {
+        return TimeStamp::from_epoch(Duration::from_seconds(double(x)));
+    }
+
+    inline TimeStamp operator""_MS(long double x) noexcept
+    {
+        return TimeStamp::from_epoch(Duration::from_milliseconds(double(x)));
+    }
+
+    inline TimeStamp operator""_MS(long long unsigned x) noexcept
+    {
+        return TimeStamp::from_epoch(Duration::from_milliseconds(double(x)));
+    }
+
     TimeStamp operator +(const TimeStamp&, const Duration&);
     TimeStamp operator +(const Duration&, const TimeStamp&);
     TimeStamp operator -(const TimeStamp&, const Duration&);
