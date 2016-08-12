b5bd0abecb251b8fbdbff632357e9ba076a56ab0
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-12 20:38:39 +0200

    scripting/primitives-module
    Added bindings

--- raytracer/raytracer/scripting/primitives-module.cpp
+++ raytracer/raytracer/scripting/primitives-module.cpp
@@ -25,6 +25,17 @@ namespace
         return primitives::make_union(children);
     }
 
+    Primitive make_accelerated_union(const std::vector<chaiscript::Boxed_Value>& boxed_children)
+    {
+        std::vector<Primitive> children(boxed_children.size());
+
+        std::transform(boxed_children.begin(), boxed_children.end(), children.begin(), [](chaiscript::Boxed_Value boxed) {
+            return chaiscript::boxed_cast<Primitive>(boxed);
+        });
+
+        return primitives::accelerated_union(children);
+    }
+
     Primitive mesh(const std::string& path)
     {
         return primitives::fast_mesh(path);
@@ -56,7 +67,12 @@ ModulePtr raytracer::scripting::_private_::create_primitives_module()
     BIND_NS(raytracer::primitives, cylinder_along_z);
     BIND_NS(raytracer::primitives, triangle);
     BIND_NS(raytracer::primitives, translate);
+    BIND_NS(raytracer::primitives, rotate_around_x);
+    BIND_NS(raytracer::primitives, rotate_around_y);
+    BIND_NS(raytracer::primitives, rotate_around_z);
+    BIND_NS(raytracer::primitives, scale);
     BIND_AS(make_union, union);
+    BIND_AS(make_accelerated_union, bbunion);
     BIND_NS(raytracer::primitives, decorate);
     BIND(mesh);
     BIND_NS(raytracer::primitives, center);
