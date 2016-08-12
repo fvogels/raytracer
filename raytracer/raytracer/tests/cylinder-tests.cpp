dd9567dfa4186fe02c978d759d0c5f78a3cc8e30
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-12 13:01:21 +0200

    tests/cylinder

--- raytracer/raytracer/tests/cylinder-tests.cpp
+++ raytracer/raytracer/tests/cylinder-tests.cpp
@@ -29,7 +29,7 @@ TEST_CASE("[Cylinder] First hit (5,0,0)+(1,0,0)*t with Z-cylinder", "[Cylinder]"
     REQUIRE(!c->find_hit(ray, &hit));
 }
 
-TEST_CASE("[cylinder_along_z] First hit (5,0,0)+(-1,0,0)*t with Z-cylinder", "[Cylinder]")
+TEST_CASE("[Cylinder] First hit (5,0,0)+(-1,0,0)*t with Z-cylinder", "[Cylinder]")
 {
     auto c = cylinder_along_z();
     Ray ray(Point3D(5, 0, 0), Vector3D(-1, 0, 0));
@@ -37,6 +37,17 @@ TEST_CASE("[cylinder_along_z] First hit (5,0,0)+(-1,0,0)*t with Z-cylinder", "[C
 
     REQUIRE(c->find_hit(ray, &hit));
     REQUIRE(hit.t == Approx(4));
+    REQUIRE(hit.normal == approx(Vector3D(1, 0, 0)));
+}
+
+TEST_CASE("[Cylinder] First hit (0,0,0)+(-1,0,0)*t with Z-cylinder", "[Cylinder]")
+{
+    auto c = cylinder_along_z();
+    Ray ray(Point3D(0, 0, 0), Vector3D(-1, 0, 0));
+    Hit hit;
+
+    REQUIRE(c->find_hit(ray, &hit));
+    REQUIRE(hit.t == Approx(1));
     REQUIRE(hit.normal == approx(Vector3D(1, 0, 0)));
 }
 
