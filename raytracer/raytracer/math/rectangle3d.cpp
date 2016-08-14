8d0e40f256b41cc4d962636ee023fdfd72e02add
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-14 11:34:11 +0200

    math/rectangle3d
    Docs

--- raytracer/raytracer/math/rectangle3d.cpp
+++ raytracer/raytracer/math/rectangle3d.cpp
@@ -2,6 +2,13 @@
 
 using namespace math;
 
+
+math::Rectangle3D::Rectangle3D(const Point3D& origin, const Vector3D& x_axis, const Vector3D& y_axis)
+    : origin(origin), x_axis(x_axis), y_axis(y_axis) 
+{
+    // NOP
+}
+
 Point3D math::Rectangle3D::project(const Point2D& p) const
 {
     return origin + x_axis * p.x() + y_axis * p.y();
