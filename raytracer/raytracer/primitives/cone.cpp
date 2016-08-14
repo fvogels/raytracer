8765d2ea222041c3ed75b2be13308e256fdded78
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-14 16:24:14 +0200

    primitives/cones
    hits

--- raytracer/raytracer/primitives/cone.cpp
+++ raytracer/raytracer/primitives/cone.cpp
@@ -20,13 +20,13 @@ namespace
         {
             assert(hit);
 
-            Point2D O2D = Point2D(ray.origin.x(), ray.origin.y());
-            Vector2D D2D = Vector2D(ray.direction.x(), ray.direction.y());
-            Vector2D OP = O2D - Point2D(0, 0);
+            Point2D Oxy = Point2D(ray.origin.x(), ray.origin.y());
+            Vector2D Dxy = Vector2D(ray.direction.x(), ray.direction.y());
+            Vector2D Vxy = Oxy - Point2D(0, 0);
 
-            double a = D2D.dot(D2D) - pow(ray.direction.z(), 2);
-            double b = 2 * (D2D.dot(OP) - ray.direction.z() * ray.origin.z());
-            double c = OP.dot(OP) - pow(ray.origin.z(), 2);
+            double a = Dxy.dot(Dxy) - pow(ray.direction.z(), 2);
+            double b = 2 * (Dxy.dot(Vxy) - ray.direction.z() * ray.origin.z());
+            double c = Vxy.dot(Vxy) - pow(ray.origin.z(), 2);
 
             QuadraticEquation eq(a, b, c);
 
@@ -53,26 +53,7 @@ namespace
 
                 if (t < hit->t)
                 {
-                    Point3D position = ray.at(t);
-
-                    if (position == Point3D(0, 0, 0))
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        Point2D position_on_circle(position.x(), position.y());
-                        double height = position.z();
-                        double theta = atan2(position_on_circle.y(), position_on_circle.x());
-
-                        hit->t = t;
-                        hit->position = position;
-                        hit->local_position.xyz = position;
-                        hit->local_position.uv = Point2D(theta, height);
-                        hit->normal = Vector3D(position.x(), position.y(), -position.z()).normalized();
-
-                        return true;
-                    }
+                    return compute_hit_at(ray, t, hit);
                 }
                 else
                 {
@@ -85,17 +66,89 @@ namespace
             }
         }
 
-        std::vector<std::shared_ptr<Hit>> hits(const math::Ray&) const override
+        std::vector<std::shared_ptr<Hit>> hits(const math::Ray& ray) const override
         {
-            LOG(ERROR) << "Not yet implemented: Cone::hits";
+            Point2D Oxy = Point2D(ray.origin.x(), ray.origin.y());
+            Vector2D Dxy = Vector2D(ray.direction.x(), ray.direction.y());
+            Vector2D Vxy = Oxy - Point2D(0, 0);
 
-            // TODO
-            abort();
+            double a = Dxy.dot(Dxy) - pow(ray.direction.z(), 2);
+            double b = 2 * (Dxy.dot(Vxy) - ray.direction.z() * ray.origin.z());
+            double c = Vxy.dot(Vxy) - pow(ray.origin.z(), 2);
+
+            QuadraticEquation quadratic_equation(a, b, c);
+
+            if (quadratic_equation.has_solutions())
+            {
+                std::vector<std::shared_ptr<Hit>> hits;
+
+                double t1 = quadratic_equation.x1();
+                double t2 = quadratic_equation.x2();
+
+                sort(t1, t2);
+
+                auto hit1 = std::make_shared<Hit>();
+                auto hit2 = std::make_shared<Hit>();
+
+                if (compute_hit_at(ray, t1, hit1.get())) { hits.push_back(hit1); }
+                if (compute_hit_at(ray, t2, hit2.get())) { hits.push_back(hit2); }
+
+                return hits;
+            }
+            else
+            {
+                return std::vector<std::shared_ptr<Hit>>();
+            }
         }
 
         math::Box bounding_box() const override
         {
             return Box::infinite();
+        }
+
+    private:
+        /// <summary>
+        /// Computes the hit data. The (0,0,0) point is problematic, so it is not considered to be part of the cone,
+        /// hence the need for a return value.
+        /// </summary>
+        /// <param name="ray">Ray that intersects the cone.</param>
+        /// <param name="t">T-value at which the ray hits the cone.</param>
+        /// <param name="hit">Hit object to be filled in.</param>
+        /// <returns>True if it's a valid hit, false otherwise.</returns>
+        bool compute_hit_at(const Ray& ray, double t, Hit* hit) const
+        {
+            Point3D position = ray.at(t);
+
+            assert(lies_on_cone(position));
+
+            if (position == Point3D(0, 0, 0))
+            {
+                return false;
+            }
+            else
+            {
+                Point2D position_on_circle(position.x(), position.y());
+                double height = position.z();
+                double theta = atan2(position_on_circle.y(), position_on_circle.x());
+
+                hit->t = t;
+                hit->position = position;
+                hit->local_position.xyz = position;
+                hit->local_position.uv = Point2D(theta, height);
+                hit->normal = Vector3D(position.x(), position.y(), -position.z()).normalized();
+
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Checks whether <paramref name="p" /> lies on the cone. (0,0,0) is considered part of the cone by this method.
+        /// </summary>
+        bool lies_on_cone(const Point3D& p) const
+        {
+            double distance_from_z_axis = sqrt(pow(p.x(), 2) + pow(p.y(), 2));
+
+            return distance_from_z_axis == approx(std::abs(p.z()));
         }
     };
 }
