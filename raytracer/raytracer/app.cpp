58c9cc16e08b2ef81f193fcd284038ead5c244ee
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-15 10:22:09 +0200

    app

--- raytracer/raytracer/app.cpp
+++ raytracer/raytracer/app.cpp
@@ -162,6 +162,9 @@ void render()
 
 namespace
 {
+    // Note: these are not used anywhere for now
+    std::map<std::string, std::string> external_variables;
+
     void render_script(const std::string& filename)
     {
         LOG(INFO) << "Rendering " << filename;
@@ -189,6 +192,24 @@ namespace
     {
         ::beep();
     }
+
+    void define_external_variable(const std::string& string)
+    {
+        auto equal_sign_location = string.find('=');
+
+        if (equal_sign_location == std::string::npos)
+        {
+            LOG(ERROR) << "Invalid variable definition:" << string;
+            abort();
+        }
+        else
+        {
+            std::string variable_name = string.substr(0, equal_sign_location);
+            std::string variable_value = string.substr(equal_sign_location + 1);
+
+            external_variables[variable_name] = variable_value;
+        }
+    }
 }
 
 void process_command_line_arguments(int argc, char** argv)
@@ -199,6 +220,7 @@ void process_command_line_arguments(int argc, char** argv)
     processor.register_processor("--quiet", quiet);
     processor.register_processor("--version", show_version);
     processor.register_processor("--beep", emit_beep);
+    processor.register_processor("-d", define_external_variable);
 
     processor.process(argc, argv);
 
