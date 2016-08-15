aacb01c9115157cbb762d9766262815dcc7edbfa
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-15 21:06:16 +0200

    materials/material-properties
    Added parameter name in header file

--- raytracer/raytracer/materials/material-properties.h
+++ raytracer/raytracer/materials/material-properties.h
@@ -7,7 +7,7 @@ namespace raytracer
 {
     struct MaterialProperties
     {
-        MaterialProperties(const imaging::Color&, const imaging::Color&, const imaging::Color&, double, double, double, double);
+        MaterialProperties(const imaging::Color& ambient, const imaging::Color& diffuse, const imaging::Color& specular, double specular_exponent, double reflectivity, double transparency, double refractive_index);
 
         const imaging::Color ambient;
         const imaging::Color diffuse;
