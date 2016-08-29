20fd2e0c6be9bd139fec4b9c4117d7a65379e2fa
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-29 17:20:55 +0200

    animation/animation-inverter
    Fixed bug

--- raytracer/raytracer/animation/animation-inverter.h
+++ raytracer/raytracer/animation/animation-inverter.h
@@ -11,7 +11,7 @@ namespace animation
         std::function<T(TimeStamp)> lambda = [a](TimeStamp now)
         {
             TimeStamp end = TimeStamp::from_epoch(a.duration());
-            TimeStamp inverted_now = end - now.seconds();
+            TimeStamp inverted_now = TimeStamp::from_seconds_since_epoch(end.seconds() - now.seconds());
             
             return a(inverted_now);
         };
