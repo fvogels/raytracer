f32f498a6d0a61a05901acf19215e3e1f079914f
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-30 10:48:31 +0200

    test-generation/matrix-tests

--- raytracer/raytracer/test-generation/matrix-tests.rb
+++ raytracer/raytracer/test-generation/matrix-tests.rb
@@ -34,15 +34,49 @@ test_file 'matrix-tests' do
     end
 
     test_case do |data|
-      data.first = 'identity<4>()'
-      data.second = 'identity<4>()'
+      data.first = 'identity<3>()'
+      data.second = 'identity<3>()'
     end
 
+    test_case do |data|
+      data.first = 'translation(Vector2D(0,0))'
+      data.second = 'identity<3>()'
+    end
+
+    test_case do |data|
+      data.first = 'scaling(1,1)'
+      data.second = 'identity<3>()'
+    end
+
+    test_case do |data|
+      data.first = 'rotation(0_degrees)'
+      data.second = 'identity<3>()'
+    end
+
+    test_case do |data|
+      data.first = 'rotation(0_degrees)'
+      data.second = 'rotation(360_degrees)'
+    end
+
+    test_case do |data|
+      data.first = 'rotation(-90_degrees)'
+      data.second = 'rotation(270_degrees)'
+    end
+
+    test_case do |data|
+      data.first = 'rotation(-180_degrees)'
+      data.second = 'rotation(180_degrees)'
+    end
+
+    test_case do |data|
+      data.first = 'scaling(-1,-1)'
+      data.second = 'rotation(180_degrees)'
+    end
+    
     test_case do |data|
       data.first = 'translation(Vector3D(0,0,0))'
       data.second = 'identity<4>()'
     end
-
 
     test_case do |data|
       data.first = 'scaling(1,1,1)'
@@ -99,7 +133,7 @@ test_file 'matrix-tests' do
   test_suite do
     template do
       <<-END
-        TEST_CASE("[Matrix] Matrix multiplication of #{first} and #{second}", "[Matrix]")
+        TEST_CASE("[Matrix] Matrix multiplication of #{first} and #{second} should give #{expected}", "[Matrix]")
         {
             auto a = #{first};
             auto b = #{second};
@@ -111,6 +145,165 @@ test_file 'matrix-tests' do
       END
     end
 
+    test_case do |data|
+      data.first = 'identity<3>()'
+      data.second = 'identity<3>()'
+      data.expected = 'identity<3>()'
+    end
+
+    [-1, 1, 2].each do |sx|
+      [-1, 1, 2].each do |sy|
+        test_case do |data|
+          data.first = "identity<3>()"
+          data.second = "scaling(#{sx}, #{sy})"
+          data.expected = "scaling(#{sx}, #{sy})"
+        end
+      end
+    end
+
+    [0, 10, 90, 180, 270].each do |angle|
+      test_case do |data|
+        data.first = "identity<3>()"
+        data.second = "rotation(#{angle}_degrees)"
+        data.expected = "rotation(#{angle}_degrees)"
+      end
+    end
+
+    [-1, 0.5, 1, 2].each do |sx1|
+      [-1, 0.5, 1, 2].each do |sy1|
+        [-1, 0.5, 1, 2].each do |sx2|
+          [-1, 0.5, 1, 2].each do |sy2|
+            test_case do |data|
+              data.first = "scaling(#{sx1}, #{sy1})"
+              data.second = "scaling(#{sx2}, #{sy2})"
+              data.expected = "scaling(#{sx1 * sx2}, #{sy1 * sy2})"
+            end
+          end
+        end
+      end
+    end
+
+    [0, -10, 50, 90, 180, 270, 360, 720].each do |angle1|
+      [0, -10, 50, 90, 180, 270, 360, 720].each do |angle2|
+        test_case do |data|
+          data.first = "rotation(#{angle1}_degrees)"
+          data.second = "rotation(#{angle2}_degrees)"
+          data.expected = "rotation(#{angle1 + angle2}_degrees)"
+        end
+      end
+    end
+
+    [1, 5].each do |delta|
+      test_case do |data|
+        data.first = "translation(Vector2D(#{delta}, 0))"
+        data.second = "translation(Vector2D(-#{delta}, 0))"
+        data.expected = "identity<3>()"
+      end
+
+      test_case do |data|
+        data.first = "translation(Vector2D(0, #{delta}))"
+        data.second = "translation(Vector2D(0, -#{delta}))"
+        data.expected = "identity<3>()"
+      end
+    end
+
+    [-1,0,3].each do |dx1|
+      [-2,0,1].each do |dy1|
+        [-5,0,4].each do |dx2|
+          [-1,0,1].each do |dy2|
+            test_case do |data|
+              data.first = "translation(Vector2D(#{dx1}, #{dy1}))"
+              data.second = "translation(Vector2D(#{dx2}, #{dy2}))"
+              data.expected = "translation(Vector2D(#{dx1+dx2}, #{dy1+dy2}))"
+            end
+          end
+        end
+      end
+    end
+
+    [-2,0,5].each do |x|
+      [-1,0,7].each do |y|
+        [-8,0,1].each do |dx|
+          [-5,0,4].each do |dy|
+            test_case do |data|
+              data.first = "translation(Vector2D(#{dx}, #{dy}))"
+              data.second = "Vector2D(#{x}, #{y})"
+              data.expected = "Vector2D(#{x}, #{y})"
+            end
+
+            test_case do |data|
+              data.first = "translation(Vector2D(#{dx}, #{dy}))"
+              data.second = "Point2D(#{x}, #{y})"
+              data.expected = "Point2D(#{x + dx}, #{y + dy})"
+            end
+          end
+        end
+      end
+    end
+
+    [-2,0,5].each do |x|
+      [-1,0,7].each do |y|
+        [-8,0,1].each do |sx|
+          [-5,0,4].each do |sy|
+            test_case do |data|
+              data.first = "scaling(#{sx}, #{sy})"
+              data.second = "Vector2D(#{x}, #{y})"
+              data.expected = "Vector2D(#{sx * x}, #{sy * y})"
+            end
+
+            test_case do |data|
+              data.first = "scaling(#{sx}, #{sy})"
+              data.second = "Point2D(#{x}, #{y})"
+              data.expected = "Point2D(#{sx * x}, #{sy * y})"
+            end
+          end
+        end
+      end
+    end
+
+    [-2,0,4].each do |x|
+      [-3,0,7].each do |y|
+        test_case do |data|
+          data.first = "rotation(180_degrees)"
+          data.second = "Point2D(#{x}, #{y})"
+          data.expected = "Point2D(#{-x}, #{-y})"
+        end
+
+        test_case do |data|
+          data.first = "rotation(180_degrees)"
+          data.second = "Vector2D(#{x}, #{y})"
+          data.expected = "Vector2D(#{-x}, #{-y})"
+        end
+
+        test_case do |data|
+          data.first = "rotation(90_degrees)"
+          data.second = "Point2D(#{x}, #{y})"
+          data.expected = "Point2D(#{-y}, #{x})"
+        end
+
+        test_case do |data|
+          data.first = "rotation(90_degrees)"
+          data.second = "Vector2D(#{x}, #{y})"
+          data.expected = "Vector2D(#{-y}, #{x})"
+        end
+
+        test_case do |data|
+          data.first = "rotation(-90_degrees)"
+          data.second = "Point2D(#{x}, #{y})"
+          data.expected = "Point2D(#{y}, #{-x})"
+        end
+
+        test_case do |data|
+          data.first = "rotation(-90_degrees)"
+          data.second = "Vector2D(#{x}, #{y})"
+          data.expected = "Vector2D(#{y}, #{-x})"
+        end
+      end
+    end
+
+
+
+    
     test_case do |data|
       data.first = 'identity<4>()'
       data.second = 'identity<4>()'
@@ -153,34 +346,22 @@ test_file 'matrix-tests' do
       data.expected = 'rotation_around_z(250_degrees)'
     end
 
-    test_case do |data|
-      data.first = 'scaling(2, 1, 1)'
-      data.second = 'scaling(2, 1, 1)'
-      data.expected = 'scaling(4, 1, 1)'
-    end
-
-    test_case do |data|
-      data.first = 'scaling(2, 1, 1)'
-      data.second = 'scaling(0.5, 1, 1)'
-      data.expected = 'scaling(1, 1, 1)'
-    end
-
-    test_case do |data|
-      data.first = 'scaling(2, 1, 1)'
-      data.second = 'scaling(1, 2, 1)'
-      data.expected = 'scaling(2, 2, 1)'
-    end
-
-    test_case do |data|
-      data.first = 'scaling(2, 1, 1)'
-      data.second = 'scaling(1, 1, 2)'
-      data.expected = 'scaling(2, 1, 2)'
-    end
-
-    test_case do |data|
-      data.first = 'scaling(1, 3, 1)'
-      data.second = 'scaling(1, 1, 2)'
-      data.expected = 'scaling(1, 3, 2)'
+    [-1, 0.5, 1, 2].each do |sx1|
+      [-1, 0.5, 1, 2].each do |sy1|
+        [-1, 0.5, 1, 2].each do |sz1|
+          [-1, 0.5, 1, 2].each do |sx2|
+            [-1, 0.5, 1, 2].each do |sy2|
+              [-1, 0.5, 1, 2].each do |sz2|
+                test_case do |data|
+                  data.first = "scaling(#{sx1}, #{sy1}, #{sz1})"
+                  data.second = "scaling(#{sx2}, #{sy2}, #{sz2})"
+                  data.expected = "scaling(#{sx1 * sx2}, #{sy1 * sy2}, #{sz1 * sz2})"
+                end
+              end
+            end
+          end
+        end
+      end
     end
 
     test_case do |data|
