d5e9a52653b0789c479ac11616dac2be2ad61454
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-31 20:15:22 +0200

    primitives/hit-position
    Added comments

--- raytracer/raytracer/primitives/hit-position.h
+++ raytracer/raytracer/primitives/hit-position.h
@@ -6,9 +6,20 @@
 
 namespace raytracer
 {
+    /// <summary>
+    /// Represents to local position of a hit, i.e. in the primitive's own coordinate system
+    /// instead of the world's.
+    /// </summary>
     struct HitPosition
     {
+        /// <summary>
+        /// 2D uv-coordinates.
+        /// </summary>
         math::Point2D uv;
+
+        /// <summary>
+        /// 3D xyz-coordinates.
+        /// </summary>
         math::Point3D xyz;
     };
 }
