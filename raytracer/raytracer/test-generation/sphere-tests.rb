4adf2283de0dc36eb54e3307152cee55622c789e
Frederic Vogels <frederic.vogels@gmail.com>
2016-08-31 21:33:14 +0200

    test-generation/sphere-tests

--- raytracer/raytracer/test-generation/sphere-tests.rb
+++ raytracer/raytracer/test-generation/sphere-tests.rb
@@ -1,24 +1,28 @@
 require './shared.rb'
 
+file_template = proc do
+  <<-'END'
+    #ifdef TEST_BUILD
 
-test_file 'primitives/sphere' do
+    #include "Catch.h"
+    #include "primitives/primitives.h"
+    #include "math/approx.h"
+
+    using namespace math;
+    using namespace raytracer;
+    using namespace raytracer::primitives;
+
+
+    <<<TESTS>>>
+
+    #endif
+  END
+end  
+
+
+test_file 'primitives/sphere/sphere-no-hits' do
   template do
-    <<-'END'
-      #ifdef TEST_BUILD
-
-      #include "Catch.h"
-      #include "primitives/primitives.h"
-      #include "math/approx.h"
-
-      using namespace math;
-      using namespace raytracer;
-      using namespace raytracer::primitives;
-
-
-      <<<TESTS>>>
-
-      #endif
-    END
+    instance_eval(&file_template)
   end
 
   test_suite do
@@ -40,60 +44,101 @@ test_file 'primitives/sphere' do
       END
     end
 
-    test_case do |data|
-      data.ray_origin = '(0,0,5)'
-      data.ray_direction = '(0,0,1)'
-      data.t = 10
+    (-5..5).each do |a|
+      (-5..5).each do |b|
+        (-5..5).each do |c|
+          [1,2].each do |delta|
+
+            u = a / 5.1
+            v = b / 5.1
+
+            if u * u + v * v > 1 then
+              test_case do |data|
+                data.ray_origin = "(#{u}, #{v}, #{c})"
+                data.ray_direction = "(0, 0, #{delta})"
+                data.t = 10000
+              end
+
+              test_case do |data|
+                data.ray_origin = "(#{u}, #{v}, #{c})"
+                data.ray_direction = "(0, 0, #{-delta})"
+                data.t = 10000
+              end
+
+              test_case do |data|
+                data.ray_origin = "(#{u}, #{c}, #{v})"
+                data.ray_direction = "(0, #{delta}, 0)"
+                data.t = 10000
+              end
+
+              test_case do |data|
+                data.ray_origin = "(#{u}, #{c}, #{v})"
+                data.ray_direction = "(0, #{-delta}, 0)"
+                data.t = 10000
+              end
+
+              test_case do |data|
+                data.ray_origin = "(#{c}, #{u}, #{v})"
+                data.ray_direction = "(#{delta}, 0, 0)"
+                data.t = 10000
+              end
+
+              test_case do |data|
+                data.ray_origin = "(#{c}, #{u}, #{v})"
+                data.ray_direction = "(#{-delta}, 0, 0)"
+                data.t = 10000
+              end
+            end
+          end
+        end
+      end
     end
 
-    test_case do |data|
-      data.ray_origin = '(0,0,-5)'
-      data.ray_direction = '(0,0,-1)'
-      data.t = 10
-    end
+    [2,5,10].each do |a|
+      [1,2].each do |delta|
+        test_case do |data|
+          data.ray_origin = "(0, 0, #{a})"
+          data.ray_direction = "(0, 0, #{-delta})"
+          data.t = (a - 1.0) / delta - 0.01
+        end
 
-    test_case do |data|
-      data.ray_origin = '(0,2,0)'
-      data.ray_direction = '(0,1,0)'
-      data.t = 10
-    end
+        test_case do |data|
+          data.ray_origin = "(0, 0, #{-a})"
+          data.ray_direction = "(0, 0, #{delta})"
+          data.t = (a - 1.0) / delta - 0.01
+        end
 
-    test_case do |data|
-      data.ray_origin = '(0,-2,0)'
-      data.ray_direction = '(0,-1,0)'
-      data.t = 10
-    end
+        test_case do |data|
+          data.ray_origin = "(0, #{a}, 0)"
+          data.ray_direction = "(0, #{-delta}, 0)"
+          data.t = (a - 1.0) / delta - 0.01
+        end
 
-    test_case do |data|
-      data.ray_origin = '(4,0,0)'
-      data.ray_direction = '(1,0,0)'
-      data.t = 10
-    end
+        test_case do |data|
+          data.ray_origin = "(0, #{-a}, 0)"
+          data.ray_direction = "(0, #{delta}, 0)"
+          data.t = (a - 1.0) / delta - 0.01
+        end
+        
+        test_case do |data|
+          data.ray_origin = "(#{a}, 0, 0)"
+          data.ray_direction = "(#{-delta}, 0, 0)"
+          data.t = (a - 1.0) / delta - 0.01
+        end
 
-    test_case do |data|
-      data.ray_origin = '(-4,0,0)'
-      data.ray_direction = '(-1,0,0)'
-      data.t = 10
+        test_case do |data|
+          data.ray_origin = "(#{-a}, 0, 0)"
+          data.ray_direction = "(#{delta}, 0, 0)"
+          data.t = (a - 1.0) / delta - 0.01
+        end
+      end
     end
+  end
+end
 
-    test_case do |data|
-      data.ray_origin = '(4,0,0)'
-      data.ray_direction = '(-1,0,0)'
-      data.t = 2
-    end
-
-    test_case do |data|
-      data.ray_origin = '(40,0,0)'
-      data.ray_direction = '(-1,0,0)'
-      data.t = 10
-    end
-
-    test_case do |data|
-      data.ray_origin = '(40,0,0)'
-      data.ray_direction = '(-2,0,0)'
-      data.t = 10
-    end
-    
+test_file 'primitives/sphere/sphere-full-hit-checks' do
+  template do
+    instance_eval(&file_template)
   end
 
   test_suite do
@@ -112,17 +157,21 @@ test_file 'primitives/sphere' do
             REQUIRE(sphere->find_first_positive_hit(ray, &hit));
             CHECK(hit.t == Approx(#{expected_t}));
             CHECK(hit.position == approx(Point3D#{expected_hit_position}));
-            CHECK(hit.normal == approx(Vector3D#{expected_normal_position}));
+            CHECK(hit.normal == approx(Vector3D#{expected_normal}));
+            CHECK(hit.local_position.uv == approx(Point2D#{expected_uv}));
+            CHECK(hit.local_position.xyz == approx(Point3D#{expected_xyz}));
         }
       END
     end
 
     test_case do |data|
-      data.ray_origin = '(0,0,5)'
-      data.ray_direction = '(0,0,-1)'
+      data.ray_origin = '(0, 0, 5)'
+      data.ray_direction = '(0, 0, -1)'
       data.expected_t = '4'
-      data.expected_hit_position = '(0,0,1)'
-      data.expected_normal_position = data.expected_hit_position
+      data.expected_hit_position = '(0, 0, 1)'
+      data.expected_normal = data.expected_hit_position
+      data.expected_uv = '(0.75, 0.5)'
+      data.expected_xyz = data.expected_hit_position
     end
 
     test_case do |data|
@@ -130,7 +179,9 @@ test_file 'primitives/sphere' do
       data.ray_direction = '(0,0,-1)'
       data.expected_t = '2'
       data.expected_hit_position = '(0,0,1)'
-      data.expected_normal_position = data.expected_hit_position
+      data.expected_normal = data.expected_hit_position
+      data.expected_uv = '(0.75, 0.5)'
+      data.expected_xyz = data.expected_hit_position
     end
 
     test_case do |data|
@@ -138,7 +189,9 @@ test_file 'primitives/sphere' do
       data.ray_direction = '(0,0,+1)'
       data.expected_t = '2'
       data.expected_hit_position = '(0,0,-1)'
-      data.expected_normal_position = data.expected_hit_position
+      data.expected_normal = data.expected_hit_position
+      data.expected_uv = '(0.25, 0.5)'
+      data.expected_xyz = data.expected_hit_position
     end
 
     test_case do |data|
@@ -146,7 +199,9 @@ test_file 'primitives/sphere' do
       data.ray_direction = '(-1,0,0)'
       data.expected_t = '3'
       data.expected_hit_position = '(1,0,0)'
-      data.expected_normal_position = data.expected_hit_position
+      data.expected_normal = data.expected_hit_position
+      data.expected_uv = '(0, 0.5)'
+      data.expected_xyz = data.expected_hit_position
     end
 
     test_case do |data|
@@ -154,7 +209,9 @@ test_file 'primitives/sphere' do
       data.ray_direction = '(1,0,0)'
       data.expected_t = '7'
       data.expected_hit_position = '(-1,0,0)'
-      data.expected_normal_position = data.expected_hit_position
+      data.expected_normal = data.expected_hit_position
+      data.expected_uv = '(0.5, 0.5)'
+      data.expected_xyz = data.expected_hit_position
     end
 
     test_case do |data|
@@ -162,7 +219,9 @@ test_file 'primitives/sphere' do
       data.ray_direction = '(0,-1,0)'
       data.expected_t = '3'
       data.expected_hit_position = '(0,1,0)'
-      data.expected_normal_position = data.expected_hit_position
+      data.expected_normal = data.expected_hit_position
+      data.expected_uv = '(0, 0)'
+      data.expected_xyz = data.expected_hit_position
     end
 
     test_case do |data|
@@ -170,7 +229,9 @@ test_file 'primitives/sphere' do
       data.ray_direction = '(0,1,0)'
       data.expected_t = '1'
       data.expected_hit_position = '(0,-1,0)'
-      data.expected_normal_position = data.expected_hit_position
+      data.expected_normal = data.expected_hit_position
+      data.expected_uv = '(0, 1)'
+      data.expected_xyz = data.expected_hit_position
     end
 
     test_case do |data|
@@ -178,8 +239,16 @@ test_file 'primitives/sphere' do
       data.ray_direction = '(0,1,0)'
       data.expected_t = '1'
       data.expected_hit_position = '(0,1,0)'
-      data.expected_normal_position = '(0,-1,0)'
+      data.expected_normal = '(0,-1,0)'
+      data.expected_uv = '(0, 0)'
+      data.expected_xyz = data.expected_hit_position
     end
+  end
+end
+
+test_file 'primitives/sphere/sphere-all-hits' do  
+  template do
+    instance_eval(&file_template)
   end
 
   test_suite do
