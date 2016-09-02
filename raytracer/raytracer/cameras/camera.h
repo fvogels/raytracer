225bc0b1e7aa3887d2164197f99abccfedb2d014
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-02 21:30:39 +0200

    cameras/cameras
    Comments

--- raytracer/raytracer/cameras/camera.h
+++ raytracer/raytracer/cameras/camera.h
@@ -17,15 +17,38 @@ namespace raytracer
         {
             math::Matrix4x4 create_transformation(const math::Point3D&, const math::Point3D&, const math::Vector3D&);
 
+            /// <summary>
+            /// Actual implementation of cameras.
+            /// </summary>
             class CameraImplementation
             {
             public:
+                /// <summary>
+                /// Does exactly the same as enumerate_rays, but returns the produces rays in the form of a vector.
+                /// It is slightly less efficient, as memory needs to be allocated and freed.
+                /// </summary>
                 virtual std::vector<math::Ray> create_rays(const math::Point2D& p) const;
-                virtual void enumerate_rays(const math::Point2D&, std::function<void(const math::Ray&)>) const = 0;
+
+                /// <summary>
+                /// Enumerates all rays to be cast from position <paramref name="p" />.
+                /// Enumeration happens using a callback: the given function <paramref name="callback" /> gets
+                /// called for each ray. This removes the need for heap allocation and should be more efficient.
+                /// </summary>
+                /// <param name="p">
+                /// Point in [0,1]x[0,1] subspace.
+                /// </param>
+                /// <param name="callback">
+                /// Function that will get called for each ray.
+                /// </param>
+                virtual void enumerate_rays(const math::Point2D& p, std::function<void(const math::Ray&)> callback) const = 0;
             };
         }        
     }
 
+    /// <summary>
+    /// Camera wrapper. To access the actual Camera members, use the -&gt; operator. The members
+    /// are defined in CameraImplementation.
+    /// </summary>
     class Camera
     {
     public:
