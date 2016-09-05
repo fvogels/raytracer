3fd15730d02881c315b9de123e636e934ea7086a
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-05 23:00:30 +0200

    materials/material-properties
    Added default argument values

--- raytracer/raytracer/materials/material-properties.h
+++ raytracer/raytracer/materials/material-properties.h
@@ -7,7 +7,7 @@ namespace raytracer
 {
     struct MaterialProperties
     {
-        MaterialProperties(const imaging::Color& ambient, const imaging::Color& diffuse, const imaging::Color& specular, double specular_exponent, double reflectivity, double transparency, double refractive_index);
+        MaterialProperties(const imaging::Color& ambient, const imaging::Color& diffuse, const imaging::Color& specular, double specular_exponent, double reflectivity = 0, double transparency = 0, double refractive_index = 0);
 
         const imaging::Color ambient;
         const imaging::Color diffuse;
