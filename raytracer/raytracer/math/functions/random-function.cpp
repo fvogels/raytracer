7209f0d558f05a088ddcb797dd401a3ffd80fea1
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-08 23:59:22 +0200

    math/functions/random-functions
    Specialized random functions
    Relying on Mersenne Twister

--- raytracer/raytracer/math/functions/random-function.cpp
+++ raytracer/raytracer/math/functions/random-function.cpp
@@ -1,4 +1,5 @@
 #include "math/functions/random-function.h"
+#include <random>
 #include <array>
 
 using namespace math;
@@ -45,4 +46,53 @@ math::Function<unsigned(unsigned)> math::functions::random_function(unsigned par
     };
 
     return from_lambda<unsigned, unsigned>(lambda);
+}
+
+math::Function<double(unsigned)> math::functions::random::unsigned_to_double(unsigned seed)
+{
+    constexpr unsigned size = 1024;
+    std::mt19937_64 generator(seed);
+    std::uniform_real_distribution<> dist(0, 1);
+    std::shared_ptr<std::vector<double>> table = std::make_shared<std::vector<double>>(size);
+
+    for (unsigned i = 0; i != table->size(); ++i)
+    {
+        (*table)[i] = dist(generator);
+    }
+
+    std::function<double(unsigned)> lambda = [table](unsigned i) -> double {
+        return (*table)[i % table->size()];
+    };
+
+    return from_lambda(lambda);
+}
+
+math::Function<Vector2D(const Position&)> math::functions::random::position_to_vector2d(unsigned seed)
+{
+    auto helper = unsigned_to_double(seed);
+
+    std::function<Vector2D(const Position&)> lambda = [helper](const Position& p) -> Vector2D {
+        auto theta = helper(p.x * 73246 + p.y * 46976) * 360;
+
+        return Vector2D::polar(1, Angle::degrees(theta));
+    };
+
+    return from_lambda(lambda);
+}
+
+math::Function<Vector3D(const Position3D&)> math::functions::random::position3d_to_vector3d(unsigned seed)
+{
+    auto helper = unsigned_to_double(seed);
+
+    std::function<Vector3D(const Position3D&)> lambda = [helper](const Position3D& p) -> Vector3D {
+        auto azimuth = helper((p.x * 0x1234) ^ (p.y * 0x5678) ^ (p.z * 0x9ABC)) * 360;
+        auto elevation = helper((p.x * 0x7410) ^ (p.y * 0x8529) ^ (p.z * 0x63FE)) * 180 - 90;
+
+        assert(interval(0.0, 360.0).contains(azimuth));
+        assert(interval(-90.0, 90.0).contains(elevation));
+
+        return Vector3D::spherical(1, Angle::degrees(azimuth), Angle::degrees(elevation));
+    };
+
+    return from_lambda(lambda);
 }
