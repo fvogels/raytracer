f73f1010229bd10fe0ecc658f47953972093a49c
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-11 16:12:20 +0200

    imaging/wif-format
    Comments

--- raytracer/raytracer/imaging/wif-format.h
+++ raytracer/raytracer/imaging/wif-format.h
@@ -7,12 +7,26 @@
 
 namespace imaging
 {
+    /// <summary>
+    /// Class used to write WIF files.
+    /// </summary>
     class WIF
     {
     public:
-        WIF(const std::string&);
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="path">Path of WIF file</param>
+        WIF(const std::string& path);
+
+        /// <summary>
+        /// Destructor.
+        /// </summary>
         ~WIF();
 
+        /// <summary>
+        /// Writes a new frame to file.
+        /// </summary>
         void write_frame(const Bitmap& bitmap);
 
     private:
