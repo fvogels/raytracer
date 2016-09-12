53b02858892a9b1801c8aee0057e2c871f72e8f2
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-12 18:50:04 +0200

    demo/terrain-demo
    Uses demo

--- raytracer/raytracer/demo/terrain-demo.cpp
+++ raytracer/raytracer/demo/terrain-demo.cpp
@@ -1,96 +1,58 @@
 #include "demo/terrain-demo.h"
-#include "materials/materials.h"
-#include "cameras/cameras.h"
-#include "imaging/wif-format.h"
-#include "raytracers/ray-tracers.h"
-#include "renderers/renderers.h"
-#include "samplers/samplers.h"
-#include "raytracers/scene.h"
-#include "math/functions.h"
-#include "animation/animation.h"
-#include "pipeline/pipelines.h"
-#include "loopers/loopers.h"
+#include "demo/demo.h"
 
 using namespace raytracer;
 using namespace animation;
 using namespace math;
 using namespace imaging;
+using namespace demos;
 
 
 namespace
 {
-    constexpr unsigned ANTIALIASING = 2;
-    constexpr unsigned FPS = 1;
-    constexpr unsigned HPIXELS = 500;
-    constexpr unsigned VPIXELS = 500;
-    constexpr unsigned N_THREADS = 4;
-
-    raytracer::Primitive create_root(TimeStamp)
+    class TerrainDemo : public Demo
     {
-        using namespace raytracer::primitives;
-        using namespace raytracer::materials;
+    public:
+        using Demo::Demo;
 
-        MaterialProperties terrain_material_properties(colors::white() * 0.1, colors::green() * 0.8, colors::black(), 0);
-        MaterialProperties water_material_properties(colors::white() * 0.1, colors::blue() * 0.8, colors::white() * 0.2, 10, 0.5);
-        auto terrain_material = uniform(terrain_material_properties);
-        auto water_material = uniform(water_material_properties);
-        auto landscape = decorate(terrain_material, terrain());
+    protected:
+        raytracer::Primitive create_root(TimeStamp) override
+        {
+            using namespace raytracer::primitives;
+            using namespace raytracer::materials;
 
-        auto sea_level = landscape->bounding_box().y().from_relative(0.40);
-        auto water = decorate(water_material, crop_by_box(translate(Vector3D(0, sea_level, 0), xz_plane()), landscape->bounding_box()));
+            MaterialProperties terrain_material_properties(colors::white() * 0.1, colors::green() * 0.8, colors::black(), 0);
+            MaterialProperties water_material_properties(colors::white() * 0.1, colors::blue() * 0.8, colors::white() * 0.2, 10, 0.5);
+            auto terrain_material = uniform(terrain_material_properties);
+            auto water_material = uniform(water_material_properties);
+            auto landscape = decorate(terrain_material, terrain());
 
-        std::vector<Primitive> primitives = { landscape, water };
+            auto sea_level = landscape->bounding_box().y().from_relative(0.40);
+            auto water = decorate(water_material, crop_by_box(translate(Vector3D(0, sea_level, 0), xz_plane()), landscape->bounding_box()));
 
-        return make_union(primitives);
-    }
+            std::vector<Primitive> primitives = { landscape, water };
 
-    std::vector<raytracer::LightSource> create_light_sources(TimeStamp)
-    {
-        using namespace raytracer::lights;
+            return make_union(primitives);
+        }
 
-        std::vector<LightSource> light_sources;
-        light_sources.push_back(lights::omnidirectional(Point3D(5, 8, 5), colors::white()));
+        std::vector<raytracer::LightSource> create_light_sources(TimeStamp) override
+        {
+            using namespace raytracer::lights;
 
-        return light_sources;
-    }
+            std::vector<LightSource> light_sources;
+            light_sources.push_back(lights::omnidirectional(Point3D(5, 8, 5), colors::white()));
 
-    raytracer::Camera create_camera(TimeStamp)
-    {
-        return raytracer::cameras::perspective(Point3D(-2, 10, -2), Point3D(10, 0, 10), Vector3D(0, 1, 0), 1, 1);
-    }
+            return light_sources;
+        }
 
-    Animation<std::shared_ptr<Scene>> create_scene_animation()
-    {
-        std::function<std::shared_ptr<Scene>(TimeStamp)> lambda = [](TimeStamp now) {
-            auto camera = create_camera(now);
-            auto root = create_root(now);
-            auto light_sources = create_light_sources(now);
-            auto scene = std::make_shared<Scene>(camera, root, light_sources);
-
-            return scene;
-        };
-
-        auto function = from_lambda(lambda);
-
-        return make_animation<std::shared_ptr<Scene>>(function, Duration::from_seconds(1));
-    }
-
-    void render(std::shared_ptr<pipeline::Consumer<std::shared_ptr<Bitmap>>> output)
-    {
-        auto scene_animation = create_scene_animation();
-        auto ray_tracer = raytracer::raytracers::v6();
-
-        auto sampler = raytracer::samplers::multi_jittered(ANTIALIASING);
-        auto renderer = raytracer::renderers::standard(HPIXELS, VPIXELS, sampler, ray_tracer, loopers::smart_looper(N_THREADS));
-
-        pipeline::start(create_scene_animation())
-            >> pipeline::animation(FPS)
-            >> pipeline::renderer(renderer)
-            >> output;
-    }
+        raytracer::Camera create_camera(TimeStamp) override
+        {
+            return raytracer::cameras::perspective(Point3D(-2, 10, -2), Point3D(10, 0, 10), Vector3D(0, 1, 0), 1, 1);
+        }
+    };
 }
 
 void demos::terrain(std::shared_ptr<pipeline::Consumer<std::shared_ptr<Bitmap>>> output)
 {
-    render(output);
+    TerrainDemo(500, 1_s, 1, 2).render(output);
 }
