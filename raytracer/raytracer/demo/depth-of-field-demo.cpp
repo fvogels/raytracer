5f7ea538f78bc7e023765b288a9d522da203eeb0
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-12 17:13:02 +0200

    demo/fisheye-demo
    Restored to pre-Demo and fixed bugs

--- raytracer/raytracer/demo/depth-of-field-demo.cpp
+++ raytracer/raytracer/demo/depth-of-field-demo.cpp
@@ -1,69 +1,107 @@
 #include "demo/depth-of-field-demo.h"
-#include "demo/demo.h"
+#include "materials/materials.h"
+#include "cameras/cameras.h"
+#include "imaging/wif-format.h"
+#include "raytracers/ray-tracers.h"
+#include "renderers/renderers.h"
+#include "samplers/samplers.h"
+#include "raytracers/scene.h"
+#include "math/function.h"
+#include "animation/animations.h"
+#include "pipeline/pipelines.h"
+#include "loopers/loopers.h"
+#include "loopers/loopers.h"
 #include "easylogging++.h"
 
 using namespace raytracer;
 using namespace animation;
 using namespace math;
 using namespace imaging;
-using namespace demos;
 
 
 namespace
 {
-    class DepthOfFieldDemo : public Demo
+    constexpr unsigned ANTIALIASING = 2;
+    constexpr unsigned FPS = 30;
+    constexpr unsigned HPIXELS = 500;
+    constexpr unsigned VPIXELS = 500;
+    constexpr unsigned N_THREADS = 4;
+
+    raytracer::Primitive create_root(TimeStamp)
     {
-    public:
-        using Demo::Demo;
+        using namespace raytracer::primitives;
+        using namespace raytracer::materials;
 
-    protected:
-        raytracer::Primitive create_root(TimeStamp) override
+        std::vector<Primitive> spheres;
+        for (double z = 0; z <= 24; z += 3)
         {
-            using namespace raytracer::primitives;
-            using namespace raytracer::materials;
-
-            std::vector<Primitive> spheres;
-            for (double z = 0; z <= 24; z += 3)
-            {
-                spheres.push_back(translate(Vector3D(2, 0, z), sphere()));
-                spheres.push_back(translate(Vector3D(-2, 0, z), sphere()));
-            }
-            auto sphere_union = primitives::accelerated_union(spheres);
-
-            MaterialProperties material_properties(colors::white() * 0.1, colors::white() * 0.8, colors::white(), 20, 0.2, 0, 0);
-            auto decorated_spheres = decorate(uniform(material_properties), sphere_union);
-
-            MaterialProperties white(colors::white() * 0.1, colors::white() * 0.8, colors::white(), 20, 0.5, 0, 0);
-            MaterialProperties black(colors::black() * 0.1, colors::black() * 0.8, colors::white(), 20, 0.5, 0, 0);
-            auto plane = decorate(checkered(uniform(white), uniform(black)),
-                translate(Vector3D(0, -1, 0), xz_plane()));
-
-            std::vector<Primitive> children = { plane, decorated_spheres };
-            return make_union(children);
+            spheres.push_back(translate(Vector3D(2, 0, z), sphere()));
+            spheres.push_back(translate(Vector3D(-2, 0, z), sphere()));
         }
+        auto sphere_union = primitives::accelerated_union(spheres);
 
-        std::vector<raytracer::LightSource> create_light_sources(TimeStamp) override
-        {
-            using namespace raytracer::lights;
+        MaterialProperties material_properties(colors::white() * 0.1, colors::white() * 0.8, colors::white(), 20, 0.2, 0, 0);
+        auto decorated_spheres = decorate(uniform(material_properties), sphere_union);
 
-            std::vector<LightSource> light_sources;
-            light_sources.push_back(omnidirectional(Point3D(0, 5, 5), colors::white()));
+        MaterialProperties white(colors::white() * 0.1, colors::white() * 0.8, colors::white(), 20, 0.5, 0, 0);
+        MaterialProperties black(colors::black() * 0.1, colors::black() * 0.8, colors::white(), 20, 0.5, 0, 0);
+        auto plane = decorate(checkered(uniform(white), uniform(black)),
+            translate(Vector3D(0, -1, 0), xz_plane()));
 
-            return light_sources;
-        }
+        std::vector<Primitive> children = { plane, decorated_spheres };
+        return make_union( children );
+    }
 
-        raytracer::Camera create_camera(TimeStamp now) override
-        {
-            auto sampler = samplers::multi_jittered(3);
+    std::vector<raytracer::LightSource> create_light_sources(TimeStamp)
+    {
+        using namespace raytracer::lights;
 
-            auto lookat_animation = animation::animate(Point3D(0, 0, 1), Point3D(0, 0, 7), Duration::from_seconds(1));
+        std::vector<LightSource> light_sources;
+        light_sources.push_back(omnidirectional(Point3D(0, 5, 5), colors::white()));
 
-            return raytracer::cameras::depth_of_field_perspective(Point3D(0, 0, 0), lookat_animation(now), Vector3D(0, 1, 0), 1, 1, 0.05, sampler);
-        }
-    };
+        return light_sources;
+    }
+
+    raytracer::Camera create_camera(TimeStamp now)
+    {
+        auto sampler = samplers::multi_jittered(3);
+
+        auto lookat_animation = animation::animate(Point3D(0, 0, 1), Point3D(0, 0, 7), Duration::from_seconds(1));
+
+        return raytracer::cameras::depth_of_field_perspective(Point3D(0, 0, 0), lookat_animation(now), Vector3D(0, 1, 0), 1, 1, 0.05, sampler);
+    }
+
+    Animation<std::shared_ptr<Scene>> create_scene_animation()
+    {
+        std::function<std::shared_ptr<Scene>(TimeStamp)> lambda = [](TimeStamp now) {
+            auto camera = create_camera(now);
+            auto root = create_root(now);
+            auto light_sources = create_light_sources(now);
+            auto scene = std::make_shared<Scene>(camera, root, light_sources);
+
+            return scene;
+        };
+
+        auto function = from_lambda(lambda);
+
+        return make_animation<std::shared_ptr<Scene>>(function, Duration::from_seconds(3));
+    }
+
+    void render(std::shared_ptr<raytracer::pipeline::Consumer<std::shared_ptr<imaging::Bitmap>>> output)
+    {
+        auto scene_animation = create_scene_animation();
+        auto ray_tracer = raytracer::raytracers::v6();
+        auto sampler = raytracer::samplers::multi_jittered(ANTIALIASING);
+        auto renderer = raytracer::renderers::standard(HPIXELS, VPIXELS, sampler, ray_tracer, loopers::smart_looper(N_THREADS));
+
+        pipeline::start(create_scene_animation())
+            >> pipeline::animation(30)
+            >> pipeline::renderer(renderer)
+            >> output;
+    }
 }
 
 void demos::depth_of_field(std::shared_ptr<raytracer::pipeline::Consumer<std::shared_ptr<imaging::Bitmap>>> output)
 {
-    DepthOfFieldDemo(500, 3_s, 30, 2);
+    render(output);
 }
--- raytracer/raytracer/demo/fisheye-demo.cpp
+++ raytracer/raytracer/demo/fisheye-demo.cpp
@@ -1,66 +1,103 @@
 #include "demo/fisheye-demo.h"
-#include "demo/demo.h"
+#include "materials/materials.h"
+#include "cameras/cameras.h"
+#include "imaging/wif-format.h"
+#include "raytracers/ray-tracers.h"
+#include "renderers/renderers.h"
+#include "samplers/samplers.h"
+#include "raytracers/scene.h"
+#include "math/function.h"
+#include "animation/animations.h"
+#include "pipeline/pipelines.h"
+#include "loopers/loopers.h"
 #include "easylogging++.h"
 
 using namespace raytracer;
 using namespace animation;
 using namespace math;
 using namespace imaging;
-using namespace demos;
 
 
 namespace
 {
-    class FishEyeDemo : public Demo
+    constexpr unsigned ANTIALIASING = 2;
+    constexpr unsigned FPS = 30;
+    constexpr unsigned HPIXELS = 500;
+    constexpr unsigned VPIXELS = 500;
+    constexpr unsigned N_THREADS = 4;
+
+    raytracer::Primitive create_root(TimeStamp)
     {
-    public:
-        using Demo::Demo;
+        using namespace raytracer::primitives;
+        using namespace raytracer::materials;
 
-    protected:
-        Primitive create_root(TimeStamp) override
+        std::vector<Primitive> sphere_list;
+        for (double z = 0; z <= 24; z += 3)
         {
-            using namespace raytracer::primitives;
-            using namespace raytracer::materials;
-
-            std::vector<Primitive> primitives;
-            for (double z = 0; z <= 24; z += 3)
-            {
-                primitives.push_back(translate(Vector3D(2, 0, -z), sphere()));
-                primitives.push_back(translate(Vector3D(-2, 0, -z), sphere()));
-            }
-
-            auto white = uniform(MaterialProperties(colors::white() * 0.1, colors::white() * 0.8, colors::white(), 10, 0.5, 0, 0));
-            auto black = uniform(MaterialProperties(colors::black() * 0.1, colors::black() * 0.8, colors::white(), 10, 0.5, 0, 0));
-            auto plane_material = checkered(white, black);
-            auto plane = decorate(plane_material, translate(Vector3D(0, -1, 0), xz_plane()));
-            primitives.push_back(plane);
-
-            auto primitive_union = primitives::accelerated_union(primitives);
-
-            MaterialProperties material_properties(colors::white() * 0.1, colors::white() * 0.8, colors::white(), 20, 0.2, 0, 0);
-            return decorate(uniform(material_properties), primitive_union);
+            sphere_list.push_back(translate(Vector3D(2, 0, -z), sphere()));
+            sphere_list.push_back(translate(Vector3D(-2, 0, -z), sphere()));
         }
 
-        std::vector<raytracer::LightSource> create_light_sources(TimeStamp) override
-        {
-            using namespace raytracer::lights;
+        MaterialProperties sphere_material_properties(colors::white() * 0.1, colors::white() * 0.8, colors::white(), 20, 0.2, 0, 0);
+        Material sphere_material = uniform(sphere_material_properties);
+        auto spheres = decorate(sphere_material, accelerated_mesh(sphere_list));
 
-            std::vector<LightSource> light_sources;
-            light_sources.push_back(omnidirectional(Point3D(0, 5, 5), colors::white()));
+        auto white = uniform(MaterialProperties(colors::white() * 0.1, colors::white() * 0.8, colors::white(), 10, 0.5, 0, 0));
+        auto black = uniform(MaterialProperties(colors::black() * 0.1, colors::black() * 0.8, colors::white(), 10, 0.5, 0, 0));
+        auto plane_material = checkered(white, black);
+        auto plane = decorate(plane_material, translate(Vector3D(0, -1, 0), xz_plane()));
 
-            return light_sources;
-        }
+        std::vector<Primitive> primitives { spheres, plane };
+        return make_union(primitives);
+    }
 
-        raytracer::Camera create_camera(TimeStamp now) override
-        {
-            auto angle_animation = animation::animate(90_degrees, 270_degrees, 1_s);
+    std::vector<raytracer::LightSource> create_light_sources(TimeStamp)
+    {
+        using namespace raytracer::lights;
 
-            return raytracer::cameras::fisheye(Point3D(0, 0, 5), Point3D(0, 0, 0), Vector3D(0, 1, 0), 90_degrees + 180_degrees * now.seconds(), 180_degrees);
-        }
-    };
+        std::vector<LightSource> light_sources;
+        light_sources.push_back(omnidirectional(Point3D(0, 5, 5), colors::white()));
+
+        return light_sources;
+    }
+
+    raytracer::Camera create_camera(TimeStamp now)
+    {
+        auto angle_animation = animation::animate(90_degrees, 270_degrees, 1_s);
+
+        return raytracer::cameras::fisheye(Point3D(0, 0, 5), Point3D(0, 0, 0), Vector3D(0, 1, 0), 90_degrees + 180_degrees * now.seconds(), 180_degrees);
+    }
+
+    Animation<std::shared_ptr<Scene>> create_scene_animation()
+    {
+        std::function<std::shared_ptr<Scene>(TimeStamp)> lambda = [](TimeStamp now) {
+            auto camera = create_camera(now);
+            auto root = create_root(now);
+            auto light_sources = create_light_sources(now);
+            auto scene = std::make_shared<Scene>(camera, root, light_sources);
+
+            return scene;
+        };
+
+        auto function = from_lambda(lambda);
+
+        return make_animation<std::shared_ptr<Scene>>(function, Duration::from_seconds(1));
+    }
+
+    void render(std::shared_ptr<pipeline::Consumer<std::shared_ptr<Bitmap>>> output)
+    {
+        auto scene_animation = create_scene_animation();
+        auto ray_tracer = raytracer::raytracers::v6();
+        auto renderer = raytracer::renderers::standard(HPIXELS, VPIXELS, raytracer::samplers::multi_jittered(ANTIALIASING), ray_tracer, loopers::smart_looper(N_THREADS));
+
+        pipeline::start(create_scene_animation())
+            >> pipeline::animation(FPS)
+            >> pipeline::renderer(renderer)
+            >> output;
+    }
 }
 
 void demos::fisheye(std::shared_ptr<pipeline::Consumer<std::shared_ptr<Bitmap>>> output)
 {
-    FishEyeDemo(500, 1_s, 30, 2).render(output);
+    render(output);
 }
