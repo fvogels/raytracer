19b95ec07876d2fec2060030746633269497bfa3
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-12 10:07:14 +0200

    demos/demo

---
+++ raytracer/raytracer/demo/demo.cpp
@@ -0,0 +1,45 @@
+#include "demo/demo.h"
+#include "loopers/loopers.h"
+
+using namespace raytracer;
+using namespace animation;
+using namespace math;
+using namespace imaging;
+
+
+demos::Demo::Demo(unsigned antialias, unsigned fps, unsigned bitmap_size)
+    : m_antialias(antialias), m_fps(fps), m_bitmap_size(bitmap_size)
+{
+    // NOP
+}
+
+void demos::Demo::render(std::shared_ptr<raytracer::pipeline::Consumer<std::shared_ptr<imaging::Bitmap>>> consumer)
+{
+    auto scene_animation = create_scene_animation();
+    auto renderer = create_renderer();
+
+    pipeline::start(create_scene_animation())
+        >> pipeline::animation(m_fps)
+        >> pipeline::renderer(renderer)
+        >> consumer;
+}
+
+RayTracer demos::Demo::create_ray_tracer()
+{
+    return raytracer::raytracers::v6();
+}
+
+Renderer demos::Demo::create_renderer()
+{
+    return raytracer::renderers::standard(m_bitmap_size, m_bitmap_size, create_sampler(), create_ray_tracer(), create_looper());
+}
+
+Sampler demos::Demo::create_sampler()
+{
+    return raytracer::samplers::multi_jittered(m_antialias);
+}
+
+std::shared_ptr<loopers::Looper> demos::Demo::create_looper()
+{
+    return loopers::smart_looper(4);
+}
---
+++ raytracer/raytracer/demo/demo.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "demo/fisheye-demo.h"
+#include "materials/materials.h"
+#include "cameras/cameras.h"
+#include "raytracers/ray-tracers.h"
+#include "renderers/renderers.h"
+#include "samplers/samplers.h"
+#include "raytracers/scene.h"
+#include "math/function.h"
+#include "animation/animations.h"
+#include "pipeline/pipelines.h"
+#include "loopers/looper.h"
+#include <memory>
+
+
+namespace demos
+{
+    class Demo
+    {
+    public:
+        Demo(unsigned antialias, unsigned fps, unsigned bitmap_size);
+
+    protected:
+        virtual raytracer::Primitive create_root(animation::TimeStamp) = 0;
+        virtual std::vector<raytracer::LightSource> create_light_sources(animation::TimeStamp) = 0;
+        virtual raytracer::Camera create_camera(animation::TimeStamp) = 0;
+        virtual animation::Animation<std::shared_ptr<raytracer::Scene>> create_scene_animation() = 0;
+        virtual raytracer::RayTracer create_ray_tracer();
+        virtual raytracer::Renderer create_renderer();
+        virtual raytracer::Sampler create_sampler();
+        virtual std::shared_ptr<loopers::Looper> create_looper();
+
+    public:
+        virtual void render(std::shared_ptr<raytracer::pipeline::Consumer<std::shared_ptr<imaging::Bitmap>>>);
+
+    private:
+        unsigned m_antialias;
+        unsigned m_fps;
+        unsigned m_bitmap_size;
+    };
+}
