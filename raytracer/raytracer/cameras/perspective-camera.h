30cbeb873cb71b8d4b958b8bf6a9691072629808
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-13 18:37:43 +0200

    cameras/perspective-camera
    Comments

--- raytracer/raytracer/cameras/perspective-camera.h
+++ raytracer/raytracer/cameras/perspective-camera.h
@@ -27,6 +27,13 @@ namespace raytracer
             };
         }
 
+        /// <summary>
+        /// Creates a perspective camera.
+        /// </summary>
+        /// <param name="eye">The camera's location</param>
+        /// <param name="look_at">The point the camera is directed at</param>
+        /// <param name="up">Specifies how the camera is tilted sideways. (0,1,0) is standard.</param>
+        /// <param name="distance">Distance between eye and view plane.</param>
         Camera perspective(
             const math::Point3D& eye,
             const math::Point3D& look_at,
--- raytracer/raytracer/version.h
+++ raytracer/raytracer/version.h
@@ -1,1 +1,1 @@
-#define BUILD_NUMBER 670
+#define BUILD_NUMBER 671
