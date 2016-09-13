3f0ff5a563686377330f0867f43534e86e17b14c
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-13 13:20:33 +0200

    tasks

---
+++ raytracer/raytracer/tasks/task-helpers.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "tasks/lambda-task.h"
+#include <vector>
+
+
+namespace tasks
+{
+    template<typename T>
+    std::vector<std::shared_ptr<Task>> range(T from, T to, std::function<void(T)> lambda)
+    {
+        std::vector<std::shared_ptr<Task>> tasks;
+
+        for (auto i = from; i < to; ++i)
+        {
+            std::function<void()> specialized_lambda = std::bind(lambda, i);
+
+            tasks.push_back(specialized_lambda);
+        }
+
+        return tasks;
+    }
+}
