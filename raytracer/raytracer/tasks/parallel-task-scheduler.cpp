35dd4ff9206798a856af6577282b99ee1d10e84c
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-13 12:55:34 +0200

    tasks

---
+++ raytracer/raytracer/tasks/parallel-task-scheduler.cpp
@@ -0,0 +1,53 @@
+#include "tasks/parallel-task-scheduler.h"
+#include <thread>
+#include <atomic>
+
+using namespace tasks;
+
+
+namespace
+{
+    /// <summary>
+    /// Performs tasks in parallel.
+    /// </summary>
+    class ParallelTaskScheduler : public tasks::schedulers::_private_::TaskSchedulerImplementation
+    {
+    public:
+        ParallelTaskScheduler(unsigned thread_count)
+            : m_thread_count(thread_count)
+        {
+            // NOP
+        }
+
+        void perform(std::vector<std::shared_ptr<Task>> tasks) const
+        {
+            std::atomic<unsigned> task_index(0);
+            std::vector<std::thread> threads;
+
+            for (unsigned k = 0; k != m_thread_count; ++k)
+            {
+                threads.push_back(std::thread([&]() {
+                    unsigned current;
+
+                    while ((current = task_index++) < tasks.size())
+                    {
+                        tasks[current]->perform();
+                    }
+                }));
+            }
+
+            for (auto& thread : threads)
+            {
+                thread.join();
+            }
+        }
+
+    private:
+        unsigned m_thread_count;
+    };
+}
+
+TaskScheduler tasks::schedulers::parallel(unsigned thread_count)
+{
+    return TaskScheduler(std::make_shared<ParallelTaskScheduler>(thread_count));
+}
