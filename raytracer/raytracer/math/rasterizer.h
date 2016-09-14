c5527933fbbf84ab9976e85a473137f98846de44
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-14 17:19:25 +0200

    mast/rasterizer
    Rearranged code

--- raytracer/raytracer/math/rasterizer.h
+++ raytracer/raytracer/math/rasterizer.h
@@ -5,6 +5,9 @@
 
 namespace math
 {
+    /// <summary>
+    /// A rasterizer takes a 2D rectangle and divides it into smaller rectangles in a grid-like fashion.
+    /// </summary>
     class Rasterizer
     {
     private:
@@ -13,15 +16,35 @@ namespace math
         unsigned m_vertical_subdivisions;
 
     public:
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="rect">Rectangle to be subdivided into a grid.</param>
+        /// <param name="horizontal_subdivisions">Number of columns.</param>
+        /// <param name="vertical_subdivisions">Number of rows.</param>
         Rasterizer(const Rectangle2D& rect, unsigned horizontal_subdivisions, unsigned vertical_subdivisions)
             : rect(rect), m_horizontal_subdivisions(horizontal_subdivisions), m_vertical_subdivisions(vertical_subdivisions) { }
+
+        /// <summary>
+        /// Copy constructor.
+        /// </summary>
         Rasterizer(const Rasterizer&) = default;
 
         Rasterizer& operator =(const Rasterizer&) = default;
 
-        unsigned horizontal_subdivisions() const { return m_horizontal_subdivisions; }
-        unsigned vertical_subdivisions() const { return m_vertical_subdivisions; }
+        /// <summary>
+        /// Returns the number of columns.
+        /// </summary>
+        unsigned horizontal_subdivisions() const;
 
-        Rectangle2D operator [](const Position2D&) const;
+        /// <summary>
+        /// Returns the number of rows.
+        /// </summary>
+        unsigned vertical_subdivisions() const;
+
+        /// <summary>
+        /// Returns the subrectangle at the given <paramref name="position" />.
+        /// </summary>
+        Rectangle2D operator [](const Position2D& position) const;
     };
 }
