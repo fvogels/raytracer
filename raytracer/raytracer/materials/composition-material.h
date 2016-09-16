f2cc1cd443321dd64609adc256f60499ba7a12a3
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-16 08:47:27 +0200

    materials/composition-material
    composition3d

--- raytracer/raytracer/materials/composition-material.h
+++ raytracer/raytracer/materials/composition-material.h
@@ -8,5 +8,6 @@ namespace raytracer
     namespace materials
     {
         Material composite(math::Function<Material(const math::Point2D&)> function);
+        Material composite(math::Function<Material(const math::Point3D&)> function);
     }
 }
