557bbb8cd9261d4be058ca22436e2e4813c3ee92
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-16 17:12:10 +0200

    util/misc
    Comments

--- raytracer/raytracer/util/misc.h
+++ raytracer/raytracer/util/misc.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include <string>
+#include <assert.h>
 
 
+/// <summary>
+/// Swaps values of <paramref name="x" /> and <paramref name="y" />.
+/// </summary>
 template<typename T>
 void swap(T& x, T& y)
 {
@@ -11,6 +15,9 @@ void swap(T& x, T& y)
     y = temp;
 }
 
+/// <summary>
+/// Swaps values of <paramref name="x" /> and <paramref name="y" /> if <paramref name="x" /> is greater than <paramref name="y" />.
+/// </summary>
 template<typename T>
 void sort(T& x, T& y)
 {
@@ -20,10 +27,17 @@ void sort(T& x, T& y)
     }
 }
 
+/// <summary>
+/// Given two values <paramref name="x" /> and <paramref name="y" />,
+/// find the smallest positive one. If this value exists, it is written to <paramref name="result" /> and true is returned.
+/// If both values are negative, false is returned.
+/// </summary>
 template<typename T>
 bool smallest_greater_than_zero(T x, T y, T* result)
 {
     sort(x, y);
+
+    assert(x <= y);
 
     if (0 < x)
     {
