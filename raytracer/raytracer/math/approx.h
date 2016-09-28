8b5beff4db160e4d401c4e6c641da0f42fe87568
Frederic Vogels <frederic.vogels@gmail.com>
2016-09-28 22:15:55 +0200

    math/approx
    Fixed bug

--- raytracer/raytracer/math/approx.h
+++ raytracer/raytracer/math/approx.h
@@ -61,7 +61,7 @@ namespace math
     }
 
     template<typename T>
-    bool operator !=(double x, const approximately<T>& y)
+    bool operator !=(T x, const approximately<T>& y)
     {
         return !(x == y);
     }
