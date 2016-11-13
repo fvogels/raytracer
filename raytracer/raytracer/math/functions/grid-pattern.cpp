3255765e163ad9dbb9e7466c053f8bd059a23239
Frederic Vogels <frederic.vogels@gmail.com>
2016-11-13 18:56:04 +0100

    math/functions/grid-pattern
    Added grid3d

--- raytracer/raytracer/math/functions/grid-pattern.cpp
+++ raytracer/raytracer/math/functions/grid-pattern.cpp
@@ -17,3 +17,17 @@ Function<bool(const Point2D&)> math::functions::grid2d(double thickness)
 
     return from_lambda<bool, const Point2D&>(function);
 }
+
+Function<bool(const Point3D&)> math::functions::grid3d(double thickness)
+{
+    std::function<bool(const Point3D&)> function = [thickness](const Point3D& p)
+    {
+        auto x = p.x();
+        auto y = p.y();
+        auto z = p.z();
+
+        return std::abs(x - round(x)) < thickness / 2 || std::abs(y - round(y))  < thickness / 2 || std::abs(z - round(z)) < thickness / 2;
+    };
+
+    return from_lambda<bool, const Point3D&>(function);
+}
--- raytracer/raytracer/math/functions/grid-pattern.h
+++ raytracer/raytracer/math/functions/grid-pattern.h
@@ -9,5 +9,6 @@ namespace math
     namespace functions
     {
         math::Function<bool(const Point2D&)> grid2d(double thickness = 0.1);
+        math::Function<bool(const Point3D&)> grid3d(double thickness = 0.1);
     }
 }
