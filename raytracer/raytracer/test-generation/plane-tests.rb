0fd7d5d484c429a0531fc7e85397c8e648e2c615
Frederic Vogels <frederic.vogels@gmail.com>
2017-10-31 18:07:05 +0100

    plane tests

--- raytracer/raytracer/test-generation/plane-tests.rb
+++ raytracer/raytracer/test-generation/plane-tests.rb
@@ -566,3 +566,283 @@ test_file 'primitives/plane/xz-plane/xz-plane-uv' do
 end
 
 
+test_file 'primitives/plane/yz-plane/yz-plane-t' do
+  template do
+    <<-'END'
+      #ifdef TEST_BUILD
+
+      #include "Catch.h"
+      #include "primitives/primitives.h"
+      #include "math/approx.h"
+
+      using namespace math;
+      using namespace raytracer;
+      using namespace raytracer::primitives;
+
+
+      <<<TESTS>>>
+
+      #endif
+    END
+  end
+
+  test_suite do
+    template do
+      <<-END
+        TEST_CASE("[YZ Plane] t-value of hits with Ray #{ray_origin} + #{ray_direction} * t", "[YZ Plane]")
+        {
+            Point3D ray_origin#{ray_origin};
+            Vector3D ray_direction#{ray_direction};
+
+            auto primitive = raytracer::primitives::yz_plane();
+            Ray ray(ray_origin, ray_direction);
+
+            auto hits = primitive->find_all_hits(ray);
+
+            REQUIRE(hits.size() == 1);
+            CHECK(hits[0]->t == Approx(#{expected_t}));
+        }
+      END
+    end
+
+    [-2, 0, 3.1].each do |z|
+      [-2.7, 0, 8.6].each do |y|
+        [-7, 1, 5, 10].each do |x|
+          [-5.78, 1.0, 2.0, 6.1].each do |dx|
+            test_case do |data|
+              data.ray_origin = "(#{x}, #{y}, #{z})"
+              data.ray_direction = "(#{dx}, 0, 0)"
+              data.expected_t = -x / dx
+            end
+          end         
+        end
+      end
+    end
+  end
+end
+
+test_file 'primitives/plane/yz-plane/yz-plane-n' do
+  template do
+    <<-'END'
+      #ifdef TEST_BUILD
+
+      #include "Catch.h"
+      #include "primitives/primitives.h"
+      #include "math/approx.h"
+
+      using namespace math;
+      using namespace raytracer;
+      using namespace raytracer::primitives;
+
+
+      <<<TESTS>>>
+
+      #endif
+    END
+  end
+
+  test_suite do
+    template do
+      <<-END
+        TEST_CASE("[YZ Plane] Normal of hits with Ray #{ray_origin} + #{ray_direction} * t", "[YZ Plane]")
+        {
+            Point3D ray_origin#{ray_origin};
+            Vector3D ray_direction#{ray_direction};
+
+            auto primitive = raytracer::primitives::yz_plane();
+            Ray ray(ray_origin, ray_direction);
+
+            auto hits = primitive->find_all_hits(ray);
+
+            REQUIRE(hits.size() == 1);
+            CHECK(hits[0]->normal == approx(Vector3D#{expected_normal}));
+        }
+      END
+    end
+
+    [-2, 0, 2].each do |z|
+      [-2, 0, 2].each do |y|
+        [1, 5, 10].each do |x|
+          [-4.5, 1.0, 2.0].each do |dx|
+            test_case do |data|
+              data.ray_origin = "(#{x}, #{y}, #{z})"
+              data.ray_direction = "(#{dx}, 0, 0)"
+              data.expected_normal = "(1, 0, 0)"
+            end
+
+            test_case do |data|
+              data.ray_origin = "(-#{x}, #{y}, #{z})"
+              data.ray_direction = "(#{dx}, 0, 0)"
+              data.expected_normal = "(-1, 0, 0)"
+            end
+          end         
+        end
+      end
+    end
+  end
+end
+
+test_file 'primitives/plane/yz-plane/yz-plane-position' do
+  template do
+    <<-'END'
+      #ifdef TEST_BUILD
+
+      #include "Catch.h"
+      #include "primitives/primitives.h"
+      #include "math/approx.h"
+
+      using namespace math;
+      using namespace raytracer;
+      using namespace raytracer::primitives;
+
+
+      <<<TESTS>>>
+
+      #endif
+    END
+  end
+
+  test_suite do
+    template do
+      <<-END
+        TEST_CASE("[YZ Plane] Position of hits with Ray #{ray_origin} + #{ray_direction} * t", "[YZ Plane]")
+        {
+            Point3D ray_origin#{ray_origin};
+            Vector3D ray_direction#{ray_direction};
+
+            auto primitive = raytracer::primitives::yz_plane();
+            Ray ray(ray_origin, ray_direction);
+
+            auto hits = primitive->find_all_hits(ray);
+
+            REQUIRE(hits.size() == 1);
+            CHECK(hits[0]->position == approx(Point3D#{expected_position}));
+        }
+      END
+    end
+
+    [-5.7, 0, 9.1].each do |z|
+      [-7.7, 0, 2.6].each do |y|
+        [-7.1, 1.6, 5.4, 10.1].each do |x|
+          [-4.5, 1.0, 2.0].each do |dx|
+            test_case do |data|
+              data.ray_origin = "(#{x}, #{y}, #{z})"
+              data.ray_direction = "(#{dx}, 0, 0)"
+              data.expected_position = "(0, #{y}, #{z})"
+            end
+          end         
+        end
+      end
+    end
+  end
+end
+
+test_file 'primitives/plane/yz-plane/yz-plane-xyz' do
+  template do
+    <<-'END'
+      #ifdef TEST_BUILD
+
+      #include "Catch.h"
+      #include "primitives/primitives.h"
+      #include "math/approx.h"
+
+      using namespace math;
+      using namespace raytracer;
+      using namespace raytracer::primitives;
+
+
+      <<<TESTS>>>
+
+      #endif
+    END
+  end
+
+  test_suite do
+    template do
+      <<-END
+        TEST_CASE("[YZ Plane] Local position xyz of hits with Ray #{ray_origin} + #{ray_direction} * t", "[YZ Plane]")
+        {
+            Point3D ray_origin#{ray_origin};
+            Vector3D ray_direction#{ray_direction};
+
+            auto primitive = raytracer::primitives::yz_plane();
+            Ray ray(ray_origin, ray_direction);
+
+            auto hits = primitive->find_all_hits(ray);
+
+            REQUIRE(hits.size() == 1);
+            CHECK(hits[0]->local_position.xyz == approx(Point3D#{expected_xyz}));
+        }
+      END
+    end
+
+    [-5.7, 0, 9.1].each do |z|
+      [-7.7, 0, 2.6].each do |y|
+        [-7.1, 1.6, 5.4, 10.1].each do |x|
+          [-4.5, 1.0, 2.0].each do |dx|
+            test_case do |data|
+              data.ray_origin = "(#{x}, #{y}, #{z})"
+              data.ray_direction = "(#{dx}, 0, 0)"
+              data.expected_xyz = "(0, #{y}, #{z})"
+            end
+          end         
+        end
+      end
+    end
+  end
+end
+
+test_file 'primitives/plane/yz-plane/yz-plane-uv' do
+  template do
+    <<-'END'
+      #ifdef TEST_BUILD
+
+      #include "Catch.h"
+      #include "primitives/primitives.h"
+      #include "math/approx.h"
+
+      using namespace math;
+      using namespace raytracer;
+      using namespace raytracer::primitives;
+
+
+      <<<TESTS>>>
+
+      #endif
+    END
+  end
+
+  test_suite do
+    template do
+      <<-END
+        TEST_CASE("[YZ Plane] Local position uv of hits with Ray #{ray_origin} + #{ray_direction} * t", "[YZ Plane]")
+        {
+            Point3D ray_origin#{ray_origin};
+            Vector3D ray_direction#{ray_direction};
+
+            auto primitive = raytracer::primitives::yz_plane();
+            Ray ray(ray_origin, ray_direction);
+
+            auto hits = primitive->find_all_hits(ray);
+
+            REQUIRE(hits.size() == 1);
+            CHECK(hits[0]->local_position.uv == approx(Point2D#{expected_uv}));
+        }
+      END
+    end
+
+    [-5.7, 0, 9.1].each do |z|
+      [-7.7, 0, 2.6].each do |y|
+        [-7.1, 1.6, 5.4, 10.1].each do |x|
+          [-4.5, 1.0, 2.0].each do |dx|
+            test_case do |data|
+              data.ray_origin = "(#{x}, #{y}, #{z})"
+              data.ray_direction = "(#{dx}, 0, 0)"
+              data.expected_uv = "(#{y}, #{z})"
+            end
+          end         
+        end
+      end
+    end
+  end
+end
