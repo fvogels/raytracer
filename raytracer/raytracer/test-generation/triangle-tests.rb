4e2d2658de819bdd4c2561eb259a988d6b97592d
Frederic Vogels <frederic.vogels@gmail.com>
2017-11-01 12:57:50 +0100

    triangle tests

--- raytracer/raytracer/test-generation/triangle-tests.rb
+++ raytracer/raytracer/test-generation/triangle-tests.rb
@@ -220,3 +220,126 @@ test_file 'primitives/triangle/triangle-t' do
     end
   end
 end
+
+test_file 'primitives/triangle/triangle-normal' do
+  template(&file_template)
+
+  test_suite do
+    template do
+      <<-END
+        TEST_CASE("[Triangle] Expecting normal #{expected_n} for for hit between Triangle #{a} #{b} #{c} and Ray O=#{ray_origin} through u=#{u} v=#{v}", "[Triangle]")
+        {
+            Point3D ray_origin#{ray_origin};
+            Point3D a#{a};
+            Point3D b#{b};
+            Point3D c#{c};
+            Vector3D ab = b - a;
+            Vector3D ac = c - a;
+            double u = #{u};
+            double v = #{v};
+            Point3D p = a + ab * u + ac * v;
+            Vector3D ray_direction = p - ray_origin;
+
+            auto primitive = raytracer::primitives::triangle(a, b, c);
+            Ray ray(ray_origin, ray_direction);
+
+            Hit hit;
+            bool is_hit = primitive->find_first_positive_hit(ray, &hit);
+
+            REQUIRE(is_hit);
+            REQUIRE(hit.normal == approx(Vector3D#{expected_n}));
+        }
+      END
+    end
+
+    [0.01, 0.3, 0.4, 0.6, 0.99].each do |u|
+      [0.01, 0.3, 0.4, 0.6, 0.99].each do |v|
+        [0.5, 1, 15].each do |x|
+          [0.2, 1, 3].each do |y|
+            if u + v < 1
+              test_case do |data|
+                data.a = "(0, 0, 0)"
+                data.b = "(#{x}, 0, 0)"
+                data.c = "(0, #{y}, 0)"
+                data.ray_origin = "(5, 1, 1)"
+                data.u = u
+                data.v = v
+                data.expected_n = '(0, 0, 1)'
+              end
+
+              test_case do |data|
+                data.a = "(0, 0, 0)"
+                data.b = "(#{x}, 0, 0)"
+                data.c = "(0, #{y}, 0)"
+                data.ray_origin = "(5, 7, -2)"
+                data.u = u
+                data.v = v
+                data.expected_n = '(0, 0, -1)'
+              end
+            end
+          end
+        end
+      end
+    end
+
+    [0.01, 0.3, 0.4, 0.6, 0.99].each do |u|
+      [0.01, 0.3, 0.4, 0.6, 0.99].each do |v|
+        [0.5, 1, 15].each do |x|
+          [0.2, 1, 3].each do |z|
+            if u + v < 1
+              test_case do |data|
+                data.a = "(0, 0, 0)"
+                data.b = "(#{x}, 0, 0)"
+                data.c = "(0, 0, #{z})"
+                data.ray_origin = "(7, 3, 8)"
+                data.u = u
+                data.v = v
+                data.expected_n = '(0, 1, 0)'
+              end
+
+              test_case do |data|
+                data.a = "(0, 0, 0)"
+                data.b = "(#{x}, 0, 0)"
+                data.c = "(0, 0, #{z})"
+                data.ray_origin = "(2, -3, 6)"
+                data.u = u
+                data.v = v
+                data.expected_n = '(0, -1, 0)'
+              end
+            end
+          end
+        end
+      end
+    end
+
+    [0.01, 0.3, 0.4, 0.6, 0.99].each do |u|
+      [0.01, 0.3, 0.4, 0.6, 0.99].each do |v|
+        [0.5, 1, 15].each do |y|
+          [0.2, 1, 3].each do |z|
+            if u + v < 1
+              test_case do |data|
+                data.a = "(0, 0, 0)"
+                data.b = "(0, #{y}, 0)"
+                data.c = "(0, 0, #{z})"
+                data.ray_origin = "(1, 7, 6)"
+                data.u = u
+                data.v = v
+                data.expected_n = '(1, 0, 0)'
+              end
+
+              test_case do |data|
+                data.a = "(0, 0, 0)"
+                data.b = "(0, #{y}, 0)"
+                data.c = "(0, 0, #{z})"
+                data.ray_origin = "(-5, 5, 5)"
+                data.u = u
+                data.v = v
+                data.expected_n = '(-1, 0, 0)'
+              end
+            end
+          end
+        end
+      end
+    end        
+  end
+end
