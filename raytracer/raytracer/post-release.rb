d1a3add5f27c268090109b4d36b81e8e20cd1416
Frederic Vogels <frederic.vogels@ucll.be>
2019-11-24 20:58:19 +0100

    neutralized build steps

--- raytracer/raytracer/post-release.rb
+++ raytracer/raytracer/post-release.rb
@@ -1,7 +1,7 @@
 require 'fileutils'
 
-exe_path = ARGV[0]
-output_path = "E:/repos/ucll/shared/scripts/"
-puts "Post Release Build Script"
-puts "Copying #{exe_path} to #{output_path}"
-FileUtils.cp(exe_path, output_path)
+# exe_path = ARGV[0]
+# output_path = "E:/repos/ucll/shared/scripts/"
+# puts "Post Release Build Script"
+# puts "Copying #{exe_path} to #{output_path}"
+# FileUtils.cp(exe_path, output_path)
