7d8e0c0e55148db0f63f99f3b6d3110363d32cd1
Frederic Vogels <frederic.vogels@ucll.be>
2020-12-03 23:43:21 +0100

    simplified grid pattern implementation

--- raytracer/raytracer/math/functions/grid-pattern.cpp
+++ raytracer/raytracer/math/functions/grid-pattern.cpp
@@ -1,4 +1,6 @@
 #include "math/functions/grid-pattern.h"
+#include "math/functions/vertical-lines-pattern.h"
+#include "math/functions/horizontal-lines-pattern.h"
 #include "math/functions.h"
 #include <cmath>
 
@@ -7,15 +9,7 @@ using namespace math;
 
 Function<bool(const Point2D&)> math::functions::grid2d(double thickness)
 {
-    std::function<bool(const Point2D&)> function = [thickness](const Point2D& p)
-    {
-        auto x = p.x();
-        auto y = p.y();
-
-        return std::abs(x - round(x)) < thickness / 2 || std::abs(y - round(y)) < thickness / 2;
-    };
-
-    return from_lambda<bool, const Point2D&>(function);
+    return horizontal_lines(thickness) || vertical_lines(thickness);
 }
 
 Function<bool(const Point3D&)> math::functions::grid3d(double thickness)
