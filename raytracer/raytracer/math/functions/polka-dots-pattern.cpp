93bb4d456263b481a1727cdce7c63b9e3aa852e8
Frederic Vogels <frederic.vogels@ucll.be>
2020-12-03 23:43:33 +0100

    rewrote polka dots

--- raytracer/raytracer/math/functions/polka-dots-pattern.cpp
+++ raytracer/raytracer/math/functions/polka-dots-pattern.cpp
@@ -5,27 +5,23 @@
 using namespace math;
 
 
-Function<bool(const Point2D&)> math::functions::polka(double radius1, double radius2)
+Function<bool(const Point2D&)> math::functions::polka(double radius)
 {
+    auto radius_sqr = radius * radius;
+
     std::function<bool(const Point2D&)> function = [=](const Point2D& p)
     {
         auto x = p.x();
         auto y = p.y();
 
-        auto fx = x - floor(x);
-        auto fy = y - floor(y);
+        auto cx = floor(x) + 0.5;
+        auto cy = floor(y) + 0.5;
+        Point2D fp(cx, cy);
 
-        Point2D fp(fx, fy);
+        auto d_sqr = distance_sqr(p, fp);
 
-#       define CHECK(X, Y, R)   distance(fp, Point2D(X, Y)) < R
-        return CHECK(0.5, 0.5, radius1) || CHECK(0, 0, radius2) || CHECK(1, 0, radius2) || CHECK(0, 1, radius2) || CHECK(1, 1, radius2);
-#       undef CHECK
+        return d_sqr < radius_sqr;
     };
 
     return from_lambda<bool, const Point2D&>(function);
-}
-
-Function<bool(const Point2D&)> math::functions::polka(double radius)
-{
-    return polka(radius, radius);
 }
