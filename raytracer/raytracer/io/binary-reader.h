cfe5336bf310c303503ac6a8a0407e239fc26ed9
Frederic Vogels <frederic.vogels@ucll.be>
2020-12-06 22:34:09 +0100

    binary-reader

---
+++ raytracer/raytracer/io/binary-reader.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <cstdint>
+
+
+namespace io
+{
+    class BinaryReader
+    {
+    public:
+        virtual void read(unsigned byte_count, uint8_t* buffer) = 0;
+
+        template<typename T>
+        void read(T* p, size_t n = 1)
+        {
+            this->read(sizeof(T) * n, reinterpret_cast<uint8_t*>(p));
+        }
+
+        template<typename T>
+        T read()
+        {
+            T value;
+
+            this->read<T>(&value);
+
+            return value;
+        }
+    };
+}
