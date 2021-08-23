00f2bc1982ab172bcea53f28e449ef1bc4b39370
Frederic Vogels <frederic.vogels@ucll.be>
2021-08-23 19:06:57 +0200

    animation inverter - better variable names

--- raytracer/raytracer/animation/animation-inverter.h
+++ raytracer/raytracer/animation/animation-inverter.h
@@ -6,16 +6,16 @@
 namespace animation
 {
     template<typename T>
-    Animation<T> invert(Animation<T> a)
+    Animation<T> invert(Animation<T> animation)
     {
-        std::function<T(TimeStamp)> lambda = [a](TimeStamp now)
+        std::function<T(TimeStamp)> lambda = [animation](TimeStamp now)
         {
-            TimeStamp end = TimeStamp::from_epoch(a.duration());
+            TimeStamp end = TimeStamp::from_epoch(animation.duration());
             TimeStamp inverted_now = TimeStamp::from_seconds_since_epoch(end.seconds() - now.seconds());
             
             return a(inverted_now);
         };
 
-        return make_animation(math::from_lambda(lambda), a.duration());
+        return make_animation(math::from_lambda(lambda), animation.duration());
     }
 }
