e5eed5646604ae65c88b84a48867589c217b0e6b
Frederic Vogels <frederic.vogels@ucll.be>
2021-08-29 10:35:37 +0200

    added comments to mesh receiver

--- raytracer/raytracer/primitives/mesh/mesh-receiver.h
+++ raytracer/raytracer/primitives/mesh/mesh-receiver.h
@@ -11,9 +11,34 @@ namespace raytracer
         class MeshReceiver
         {
         public:
+            /// <summary>
+            /// Called for each vertex in the mesh.
+            /// </summary>
+            /// <param name="x">X-coordinate</param>
+            /// <param name="y">Y-coordinate</param>
+            /// <param name="z">Z-coordinate</param>
             virtual void vertex(double x, double y, double z) = 0;
+
+            /// <summary>
+            /// Called for each normal in the mesh.
+            /// </summary>
+            /// <param name="x">X-coordinate</param>
+            /// <param name="y">Y-coordinate</param>
+            /// <param name="z">Z-coordinate</param>
             virtual void normal(double x, double y, double z) = 0;
+
+            /// <summary>
+            /// Called for each triangle in the mesh.
+            /// </summary>
+            /// <param name="vertex1">Index of first vertex</param>
+            /// <param name="vertex2">Index of second vertex</param>
+            /// <param name="vertex3">Index of third vertex</param>
             virtual void triangle(unsigned vertex1, unsigned vertex2, unsigned vertex3) = 0;
+
+            /// <summary>
+            /// Called for each box in the mesh.
+            /// </summary>
+            /// <param name="n_children">Number of children in the box</param>
             virtual void box(unsigned n_children) = 0;
         };
     }
