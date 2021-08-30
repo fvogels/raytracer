33cbe2fc8d53bc506f492e319c5a09a56ffd40ab
Frederic Vogels <frederic.vogels@ucll.be>
2021-08-30 14:24:28 +0200

    progress shown during mesh optimization

--- meshes-v3/scripts/optimization.rb
+++ meshes-v3/scripts/optimization.rb
@@ -1,134 +1,140 @@
-XYZ = Struct.new :x, :y, :z do
-  def to_a
-    [x, y, z]
-  end
-end
+require_relative './progress'
 
-Mesh = Struct.new :vertices, :normals, :contents do
-  def to_s
-    "<Mesh>"
-  end
 
-  def inspect
-    "<Mesh>"
-  end
-end
-
-class Triangle
-  def initialize(mesh, i, j, k)
-    abort "Invalid vertex index #{i}, mesh has #{mesh.vertices.size} vertices}" unless i < mesh.vertices.size
-    abort "Invalid vertex index #{j}, mesh has #{mesh.vertices.size} vertices}" unless j < mesh.vertices.size
-    abort "Invalid vertex index #{k}, mesh has #{mesh.vertices.size} vertices}" unless k < mesh.vertices.size
-
-    @i = i
-    @j = j
-    @k = k
-
-    @vertices = [i, j, k].map { |index| mesh.vertices[index] }
-    @min = Hash[[:x, :y, :z].map { |coordinate| [coordinate, @vertices.map(&coordinate).min ] }]
-    @max = Hash[[:x, :y, :z].map { |coordinate| [coordinate, @vertices.map(&coordinate).max ] }]
+module Optimization
+  XYZ = Struct.new :x, :y, :z do
+    def to_a
+      [x, y, z]
+    end
   end
 
-  attr_reader :mesh, :i, :j, :k, :vertices
+  Mesh = Struct.new :vertices, :normals, :contents do
+    def to_s
+      "<Mesh>"
+    end
 
-  def indices
-    [i, j, k]
+    def inspect
+      "<Mesh>"
+    end
   end
 
-  def min(coordinate)
-    @min[coordinate]
+  class Triangle
+    def initialize(mesh, i, j, k)
+      abort "Invalid vertex index #{i}, mesh has #{mesh.vertices.size} vertices}" unless i < mesh.vertices.size
+      abort "Invalid vertex index #{j}, mesh has #{mesh.vertices.size} vertices}" unless j < mesh.vertices.size
+      abort "Invalid vertex index #{k}, mesh has #{mesh.vertices.size} vertices}" unless k < mesh.vertices.size
+
+      @i = i
+      @j = j
+      @k = k
+
+      @vertices = [i, j, k].map { |index| mesh.vertices[index] }
+      @min = Hash[[:x, :y, :z].map { |coordinate| [coordinate, @vertices.map(&coordinate).min ] }]
+      @max = Hash[[:x, :y, :z].map { |coordinate| [coordinate, @vertices.map(&coordinate).max ] }]
+    end
+
+    attr_reader :mesh, :i, :j, :k, :vertices
+
+    def indices
+      [i, j, k]
+    end
+
+    def min(coordinate)
+      @min[coordinate]
+    end
+
+    def max(coordinate)
+      @max[coordinate]
+    end
   end
 
-  def max(coordinate)
-    @max[coordinate]
-  end
-end
-
-Box = Struct.new :children
+  Box = Struct.new :children
 
 
-def read_mesh(pathname)
-  pathname.open do |file|
-    n_vertices, n_normals = file.readline.strip.split.map(&:to_i)
-    vertices = (1..n_vertices).map { XYZ.new(*file.readline.split.map(&:to_f)) }
-    normals = (1..n_normals).map { XYZ.new(*file.readline.split.map(&:to_f)) }
-    contents = []
-    mesh = Mesh.new vertices, normals, contents
+  def self.read_mesh(pathname)
+    pathname.open do |file|
+      n_vertices, n_normals = file.readline.strip.split.map(&:to_i)
+      vertices = (1..n_vertices).map { XYZ.new(*file.readline.split.map(&:to_f)) }
+      normals = (1..n_normals).map { XYZ.new(*file.readline.split.map(&:to_f)) }
+      contents = []
+      mesh = Mesh.new vertices, normals, contents
 
-    while (line = file.readline.strip) != 'end'
-      label, *args = line.strip.split
+      while (line = file.readline.strip) != 'end'
+        label, *args = line.strip.split
 
-      if label == 't'
-        abort "Expected 3 arguments for triangle" unless args.size == 3
+        if label == 't'
+          abort "Expected 3 arguments for triangle" unless args.size == 3
 
-        triangle = Triangle.new(mesh, *args.map(&:to_i))
-        contents << triangle
+          triangle = Triangle.new(mesh, *args.map(&:to_i))
+          contents << triangle
+        end
       end
+
+      mesh
     end
-
-    mesh
-  end
-end
-
-def build_hierarchy(triangles)
-  return triangles[0] if triangles.size == 1
-  return Box.new(triangles) if triangles.size < 5
-
-  split_axis = [ :x, :y, :z ].max_by do |axis|
-    min = triangles.map { |t| t.min(axis) }.min
-    max = triangles.map { |t| t.max(axis) }.max
-    max - min
   end
 
-  triangles.sort_by! { |t| t.min(split_axis) }
-
-  left = triangles[0...triangles.size / 2]
-  right = triangles[triangles.size / 2..-1]
-
-  left_hierarchy = build_hierarchy(left)
-  right_hierarchy = build_hierarchy(right)
-
-  Box.new [left_hierarchy, right_hierarchy]
-end
-
-def write_hierarchy(hierarchy, file)
-  case hierarchy
-  when Box
-    box = hierarchy
-    box.children.each do |child|
-      write_hierarchy(child, file)
+  def self.build_hierarchy(triangles, progress)
+    if triangles.size < 5
+      progress.step(triangles.size)
+      return Box.new(triangles)
     end
-    file.puts "b #{box.children.size}"
 
-  when Triangle
-    triangle = hierarchy
-    file.puts "t #{triangle.i} #{triangle.j} #{triangle.k}"
+    split_axis = [ :x, :y, :z ].max_by do |axis|
+      min = triangles.map { |t| t.min(axis) }.min
+      max = triangles.map { |t| t.max(axis) }.max
+      max - min
+    end
 
-  else
-    abort 'Eh?'
+    triangles.sort_by! { |t| t.min(split_axis) }
+
+    left = triangles[0...triangles.size / 2]
+    right = triangles[triangles.size / 2..-1]
+
+    left_hierarchy = build_hierarchy(left, progress)
+    right_hierarchy = build_hierarchy(right, progress)
+
+    Box.new [left_hierarchy, right_hierarchy]
+  end
+
+  def self.write_hierarchy(hierarchy, file)
+    case hierarchy
+    when Box
+      box = hierarchy
+      box.children.each do |child|
+        write_hierarchy(child, file)
+      end
+      file.puts "b #{box.children.size}"
+
+    when Triangle
+      triangle = hierarchy
+      file.puts "t #{triangle.i} #{triangle.j} #{triangle.k}"
+
+    else
+      abort 'Eh?'
+    end
+  end
+
+  def self.write_mesh(mesh, output_pathname)
+    output_pathname.open('w') do |file|
+      file.puts "#{mesh.vertices.size} #{mesh.normals.size}"
+      mesh.vertices.each do |vertex|
+        file.puts "#{vertex.x} #{vertex.y} #{vertex.z}"
+      end
+      mesh.normals.each do |normal|
+        file.puts "#{normal.x} #{normal.y} #{normal.z}"
+      end
+      mesh.contents.each do |hierarchy|
+        write_hierarchy(hierarchy, file)
+      end
+      file.puts 'end'
+    end
   end
 end
-
-def write_mesh(mesh, output_pathname)
-  output_pathname.open('w') do |file|
-    file.puts "#{mesh.vertices.size} #{mesh.normals.size}"
-    mesh.vertices.each do |vertex|
-      file.puts "#{vertex.x} #{vertex.y} #{vertex.z}"
-    end
-    mesh.normals.each do |normal|
-      file.puts "#{normal.x} #{normal.y} #{normal.z}"
-    end
-    mesh.contents.each do |hierarchy|
-      write_hierarchy(hierarchy, file)
-    end
-    file.puts 'end'
-  end
-end
-
 
 def optimize(input_pathname, output_pathname)
-  mesh = read_mesh input_pathname
-  hierarchy = build_hierarchy(mesh.contents.dup)
-  optimized_mesh = Mesh.new(mesh.vertices, mesh.normals, [hierarchy])
-  write_mesh(optimized_mesh, output_pathname)
+  mesh = Optimization.read_mesh input_pathname
+  hierarchy = Optimization.build_hierarchy(mesh.contents.dup, Progress.new(mesh.contents.size))
+  optimized_mesh = Optimization::Mesh.new(mesh.vertices, mesh.normals, [hierarchy])
+  Optimization.write_mesh(optimized_mesh, output_pathname)
 end
---
+++ meshes-v3/scripts/progress.rb
@@ -0,0 +1,32 @@
+class Progress
+  def initialize(total)
+    @total = total
+    @done = 0
+    @last_percentage = -1
+    @start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  end
+
+  def step(n = 1)
+    @done += n
+    report
+  end
+
+  def report
+    p = percentage_done
+
+    if @last_percentage != p
+      @last_percentage = p
+
+      now = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      elapsed_seconds = now - @start_time
+      total_estimated = elapsed_seconds / p * 100
+      left_estimated = total_estimated * (100 - p) / 100
+
+      STDERR.puts "#{p}% done [passed=#{elapsed_seconds.floor}, total=#{total_estimated.ceil}, left=#{left_estimated.ceil}]"
+    end
+  end
+
+  def percentage_done
+    (@done * 100.0 / @total).floor
+  end
+end
