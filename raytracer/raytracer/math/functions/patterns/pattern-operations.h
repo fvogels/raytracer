ed30d2fff2dbba684818b09799ea872943fcdc5f
Frederic Vogels <frederic.vogels@ucll.be>
2021-08-31 13:04:37 +0200

    pattern-operations

---
+++ raytracer/raytracer/math/functions/patterns/pattern-operations.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "math/functions/pattern.h"
+#include "math/point.h"
+#include "math/transformation2d.h"
+#include "math/transformation3d.h"
+#include <memory>
+
+
+namespace math
+{
+    namespace functions
+    {
+        namespace patterns
+        {
+            Pattern2D transform(const math::Transformation2D& transformation, Pattern2D pattern);
+
+            Pattern2D translate(math::Vector2D displacement, Pattern2D pattern);
+            Pattern2D rotate(math::Angle angle, Pattern2D pattern);
+            Pattern2D scale(double sx, double sy, Pattern2D pattern);
+        }
+    }
+}
