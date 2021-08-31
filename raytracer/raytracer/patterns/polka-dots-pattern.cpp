4c795c5021f0eeb8383cc187ef7d5ce2a59ee791
Frederic Vogels <frederic.vogels@ucll.be>
2021-08-31 20:33:12 +0200

    simplified polka implementation

--- raytracer/raytracer/patterns/polka-dots-pattern.cpp
+++ raytracer/raytracer/patterns/polka-dots-pattern.cpp
@@ -11,17 +11,11 @@ using namespace patterns;
 Pattern2D patterns::polka2d(double radius, double separation)
 {
     auto radius_sqr = radius * radius;
-    auto center = separation / 2;
+    Point2D center(separation / 2, separation / 2);
 
-    std::function<bool(const Point2D&)> function = [=](const Point2D& p)
+    std::function<bool(const Point2D&)> function = [=](const Point2D& point)
     {
-        auto x = p.x();
-        auto y = p.y();
-
-        auto dx = x - center;
-        auto dy = y - center;
-
-        return dx * dx + dy * dy < radius_sqr;
+        return distance_sqr(center, point) < radius_sqr;
     };
 
     return tessellate(make_pattern(function), separation, separation);
@@ -30,19 +24,11 @@ Pattern2D patterns::polka2d(double radius, double separation)
 Pattern3D patterns::polka3d(double radius, double separation)
 {
     auto radius_sqr = radius * radius;
-    auto center = separation / 2;
+    Point3D center(separation / 2, separation / 2, separation / 2);
 
-    std::function<bool(const Point3D&)> function = [=](const Point3D& p)
+    std::function<bool(const Point3D&)> function = [=](const Point3D& point)
     {
-        auto x = p.x();
-        auto y = p.y();
-        auto z = p.z();
-
-        auto dx = x - center;
-        auto dy = y - center;
-        auto dz = z - center;
-
-        return dx * dx + dy * dy + dz * dz < radius_sqr;
+        return distance_sqr(center, point) < radius_sqr;
     };
 
     return tessellate(make_pattern(function), separation, separation, separation);
