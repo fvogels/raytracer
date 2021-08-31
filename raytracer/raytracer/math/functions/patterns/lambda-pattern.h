c3cc0d5d971515be78ea18b0a6c61a7ce501d7e9
Frederic Vogels <frederic.vogels@ucll.be>
2021-08-31 13:04:44 +0200

    lambda pattern

---
+++ raytracer/raytracer/math/functions/patterns/lambda-pattern.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "math/functions/pattern.h"
+#include <functional>
+
+
+namespace math
+{
+    namespace functions
+    {
+        namespace patterns
+        {
+            namespace _private_
+            {
+                class LambdaPattern2DImplementation : public Pattern2DImplementation
+                {
+                public:
+                    LambdaPattern2DImplementation(std::function<bool(const Point2D&)> function)
+                        : m_function(function)
+                    {
+                        // NOP
+                    }
+
+                    bool at(const Point2D& point) const override
+                    {
+                        return m_function(point);
+                    }
+
+                private:
+                    std::function<bool(const Point2D&)> m_function;
+                };
+
+                class LambdaPattern3DImplementation : public Pattern3DImplementation
+                {
+                public:
+                    LambdaPattern3DImplementation(std::function<bool(const Point3D&)> function)
+                        : m_function(function)
+                    {
+                        // NOP
+                    }
+
+                    bool at(const Point3D& point) const override
+                    {
+                        return m_function(point);
+                    }
+
+                private:
+                    std::function<bool(const Point3D&)> m_function;
+                };
+            }
+
+            inline Pattern2D make_pattern(std::function<bool(const Point2D&)> function)
+            {
+                return Pattern2D(std::make_shared<_private_::LambdaPattern2DImplementation>(function));
+            }
+        }
+    }
+}
