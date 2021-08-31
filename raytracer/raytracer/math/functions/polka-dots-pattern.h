ea112cf29d1476aaedb3b81dd0aaf84c9a13bd73
Frederic Vogels <frederic.vogels@ucll.be>
2021-08-31 13:59:58 +0200

    removed redundant includes from polka dot pattern

--- raytracer/raytracer/math/functions/polka-dots-pattern.h
+++ raytracer/raytracer/math/functions/polka-dots-pattern.h
@@ -3,9 +3,6 @@
 #include "math/function.h"
 #include "math/point.h"
 #include "math/functions/checkered-pattern.h"
-#include "math/functions/horizontal-lines-pattern.h"
-#include "math/functions/vertical-lines-pattern.h"
-#include "math/functions/grid-pattern.h"
 
 
 namespace math
