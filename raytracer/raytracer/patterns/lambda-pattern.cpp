34dd140b14a10a69712f72db7bf9077ae81754b3
Frederic Vogels <frederic.vogels@ucll.be>
2021-10-28 11:25:57 +0200

    cleanup lambda patterns

--- raytracer/raytracer/patterns/lambda-pattern.cpp
+++ raytracer/raytracer/patterns/lambda-pattern.cpp
@@ -1,34 +1,51 @@
 #include "patterns/lambda-pattern.h"
 
 
-patterns::_private_::LambdaPattern2DImplementation::LambdaPattern2DImplementation(std::function<bool(const math::Point2D&)> function)
-    : m_function(function)
+namespace
 {
-    // NOP
-}
+    class LambdaPattern2DImplementation : public patterns::Pattern2DImplementation
+    {
+    public:
+        LambdaPattern2DImplementation(std::function<bool(const math::Point2D&)> function)
+            : m_function(function)
+        {
+            // NOP
+        }
 
-bool patterns::_private_::LambdaPattern2DImplementation::at(const math::Point2D& point) const
-{
-    return m_function(point);
-}
+        bool at(const math::Point2D& point) const override
+        {
+            return m_function(point);
+        }
 
-patterns::_private_::LambdaPattern3DImplementation::LambdaPattern3DImplementation(std::function<bool(const math::Point3D&)> function)
-    : m_function(function)
-{
-    // NOP
-}
+    private:
+        std::function<bool(const math::Point2D&)> m_function;
+    };
 
-bool patterns::_private_::LambdaPattern3DImplementation::at(const math::Point3D& point) const
-{
-    return m_function(point);
+    class LambdaPattern3DImplementation : public patterns::Pattern3DImplementation
+    {
+    public:
+        LambdaPattern3DImplementation(std::function<bool(const math::Point3D&)> function)
+            : m_function(function)
+        {
+            // NOP
+        }
+
+        bool at(const math::Point3D& point) const override
+        {
+            return m_function(point);
+        }
+
+    private:
+        std::function<bool(const math::Point3D&)> m_function;
+    };
 }
 
 patterns::Pattern2D patterns::make_pattern(std::function<bool(const math::Point2D&)> function)
 {
-    return Pattern2D(std::make_shared<_private_::LambdaPattern2DImplementation>(function));
+    return Pattern2D(std::make_shared<LambdaPattern2DImplementation>(function));
 }
 
 patterns::Pattern3D patterns::make_pattern(std::function<bool(const math::Point3D&)> function)
 {
-    return Pattern3D(std::make_shared<_private_::LambdaPattern3DImplementation>(function));
+    return Pattern3D(std::make_shared<LambdaPattern3DImplementation>(function));
 }
--- raytracer/raytracer/patterns/lambda-pattern.h
+++ raytracer/raytracer/patterns/lambda-pattern.h
@@ -8,27 +8,7 @@ namespace patterns
 {
     namespace _private_
     {
-        class LambdaPattern2DImplementation : public Pattern2DImplementation
-        {
-        public:
-            LambdaPattern2DImplementation(std::function<bool(const math::Point2D&)> function);
-
-            bool at(const math::Point2D& point) const override;
-
-        private:
-            std::function<bool(const math::Point2D&)> m_function;
-        };
-
-        class LambdaPattern3DImplementation : public Pattern3DImplementation
-        {
-        public:
-            LambdaPattern3DImplementation(std::function<bool(const math::Point3D&)> function);
-
-            bool at(const math::Point3D& point) const override;
-
-        private:
-            std::function<bool(const math::Point3D&)> m_function;
-        };
+        
     }
 
     Pattern2D make_pattern(std::function<bool(const math::Point2D&)> function);
